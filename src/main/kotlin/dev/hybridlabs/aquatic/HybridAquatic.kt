98b92ac9e0430bcb8e74d8b529eb8601fb0b1ca4
Daniel Orr <orrdaniel73@gmail.com>
2023-08-06 05:44:54 +0100

    Implement wandering trader trades for Blahaj Plushies

--- src/main/kotlin/dev/hybridlabs/aquatic/HybridAquatic.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/HybridAquatic.kt
@@ -1,11 +1,16 @@
 package dev.hybridlabs.aquatic
 
+import dev.hybridlabs.aquatic.block.BlahajPlushieBlock
 import dev.hybridlabs.aquatic.block.HybridAquaticBlocks
 import dev.hybridlabs.aquatic.block.entity.HybridAquaticBlockEntityTypes
 import dev.hybridlabs.aquatic.entity.HybridAquaticEntityTypes
 import dev.hybridlabs.aquatic.item.HybridAquaticItemGroups
 import dev.hybridlabs.aquatic.item.HybridAquaticItems
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.`object`.builder.v1.trade.TradeOfferHelper
+import net.minecraft.item.BlockItem
+import net.minecraft.registry.Registries
+import net.minecraft.village.TradeOffers.SellItemFactory
 import org.slf4j.LoggerFactory
 
 object HybridAquatic : ModInitializer {
@@ -22,5 +27,18 @@ object HybridAquatic : ModInitializer {
 
         HybridAquaticItems
         HybridAquaticItemGroups
+
+        registerWanderingTraderTrades()
+    }
+
+    private fun registerWanderingTraderTrades() {
+        // blahaj plushies
+        Registries.ITEM
+            .filter { it is BlockItem && it.block is BlahajPlushieBlock }
+            .forEach { block ->
+                TradeOfferHelper.registerWanderingTraderOffers(2) { list ->
+                    list.add(SellItemFactory(block, 8, 1, 2, 2))
+                }
+            }
     }
 }
