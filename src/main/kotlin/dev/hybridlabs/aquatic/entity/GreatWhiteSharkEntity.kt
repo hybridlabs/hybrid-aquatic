34400992063542b991be88b33a7064f11690ac1e
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2023-08-11 03:47:40 +0300

    great white shark health

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/GreatWhiteSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/GreatWhiteSharkEntity.kt
@@ -17,7 +17,7 @@ class GreatWhiteSharkEntity(entityType: EntityType<out GreatWhiteSharkEntity>, w
         val PREY_TAG = TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier(MOD_ID, "prey/great_white_shark"))!!
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, HybridAquaticConfig.CLOWNFISH_HEALTH.toDouble())
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 60.0)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.4)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 8.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 20.0)
