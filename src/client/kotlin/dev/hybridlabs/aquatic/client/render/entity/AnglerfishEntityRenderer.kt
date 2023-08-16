d49a9b96c92c5c550fa319d601e830f4b4bd4971
Ivan Kh <47220198+Ivan-Khar@users.noreply.github.com>
2023-08-16 22:54:42 +0300

    Fix emissive layers not rendering properly + some typos

--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/AnglerfishEntityRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/AnglerfishEntityRenderer.kt
@@ -1,12 +1,10 @@
 package dev.hybridlabs.aquatic.client.render.entity
 
 import dev.hybridlabs.aquatic.client.model.entity.AnglerfishEntityModel
-import dev.hybridlabs.aquatic.entity.HybridAquaticEntityTypes
 import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
 import net.minecraft.client.render.entity.EntityRendererFactory.Context
 import software.bernie.geckolib.renderer.GeoEntityRenderer
 import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer
-import software.bernie.geckolib.renderer.layer.GeoRenderLayer
 
 class AnglerfishEntityRenderer(context: Context) : GeoEntityRenderer<HybridAquaticFishEntity>(context, AnglerfishEntityModel()) {
     init {
