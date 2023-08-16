373d261689f2e5c6c187fa89031df27a2410d703
Ivan Kh <47220198+Ivan-Khar@users.noreply.github.com>
2023-08-16 22:53:57 +0300

    Fix emissive layers not rendering properly + some typos

--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/BarreleyeEntityRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/BarreleyeEntityRenderer.kt
@@ -4,5 +4,10 @@ import dev.hybridlabs.aquatic.client.model.entity.BarreleyeEntityModel
 import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
 import net.minecraft.client.render.entity.EntityRendererFactory.Context
 import software.bernie.geckolib.renderer.GeoEntityRenderer
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer
 
-class BarreleyeEntityRenderer(context: Context) : GeoEntityRenderer<HybridAquaticFishEntity>(context, BarreleyeEntityModel())
+class BarreleyeEntityRenderer(context: Context) : GeoEntityRenderer<HybridAquaticFishEntity>(context, BarreleyeEntityModel()) {
+    init {
+        addRenderLayer(AutoGlowingGeoLayer(this))
+    }
+}
--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/WhaleSharkEntityRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/WhaleSharkEntityRenderer.kt
@@ -4,6 +4,10 @@ import dev.hybridlabs.aquatic.client.model.entity.WhaleSharkEntityModel
 import dev.hybridlabs.aquatic.entity.shark.WhaleSharkEntity
 import net.minecraft.client.render.entity.EntityRendererFactory.Context
 import software.bernie.geckolib.renderer.GeoEntityRenderer
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer
 
 class WhaleSharkEntityRenderer(context: Context) : GeoEntityRenderer<WhaleSharkEntity>(context, WhaleSharkEntityModel()) {
+    init {
+        addRenderLayer(AutoGlowingGeoLayer(this))
+    }
 }
