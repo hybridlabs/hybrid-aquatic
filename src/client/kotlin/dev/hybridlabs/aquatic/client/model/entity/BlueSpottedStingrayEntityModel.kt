d0d6379d01fd0eab420674d6b31b5b354f12c7ed
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2023-08-17 07:31:16 +0300

    stingray related typo

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/BlueSpottedStingrayEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/BlueSpottedStingrayEntityModel.kt
@@ -2,4 +2,4 @@ package dev.hybridlabs.aquatic.client.model.entity
 
 import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
 
-class BlueSpottedStingrayEntityModel : HybridAquaticFishEntityModel<HybridAquaticFishEntity>("BLUE_SPOTTED_STINGRAY")
+class BlueSpottedStingrayEntityModel : HybridAquaticFishEntityModel<HybridAquaticFishEntity>("blue_spotted_stingray")
