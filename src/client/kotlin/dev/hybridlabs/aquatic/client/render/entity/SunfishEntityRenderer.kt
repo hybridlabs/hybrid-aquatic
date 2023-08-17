bcbfc20f43488867d4d015fc47791544b6f7c08d
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2023-08-17 05:08:52 +0300

    Sunfish 2

--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/SunfishEntityRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/SunfishEntityRenderer.kt
@@ -1,6 +1,6 @@
 package dev.hybridlabs.aquatic.client.render.entity
 
-import dev.hybridlabs.aquatic.client.model.entity.OpahEntityModel
+import dev.hybridlabs.aquatic.client.model.entity.SunfishEntityModel
 import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
 import net.minecraft.client.render.entity.EntityRendererFactory.Context
 import software.bernie.geckolib.renderer.GeoEntityRenderer
