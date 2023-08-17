7f4fd806ba2c9166c15e1f45f203b8c1708e54fa
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2023-08-17 04:35:17 +0300

    I hate you, Dragon

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/VampireSquidEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/VampireSquidEntityModel.kt
@@ -2,4 +2,4 @@ package dev.hybridlabs.aquatic.client.model.entity
 
 import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
 
-class VampireSquidEntityModel : HybridAquaticFishEntityModel<HybridAquaticFishEntity>("vampire_squid")
+class VampireSquidEntityModel : HybridAquaticFishEntityModel<HybridAquaticFishEntity>("vampiresquid")
