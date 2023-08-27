7156e8651466e2f8491e136791c80807e6bb656e
Daniel Orr <orrdaniel73@gmail.com>
2023-08-27 02:54:58 +0100

    Anemone silk touch or shears

--- src/main/kotlin/dev/hybridlabs/aquatic/data/server/BlockLootTableProvider.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/data/server/BlockLootTableProvider.kt
@@ -4,11 +4,23 @@ import dev.hybridlabs.aquatic.HybridAquatic
 import dev.hybridlabs.aquatic.block.HybridAquaticBlocks
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
+import net.minecraft.loot.LootPool
 import net.minecraft.loot.LootTable
+import net.minecraft.loot.entry.ItemEntry
 import net.minecraft.registry.Registries
 
 class BlockLootTableProvider(output: FabricDataOutput) : FabricBlockLootTableProvider(output) {
     override fun generate() {
+        // anemone
+        addDrop(HybridAquaticBlocks.ANEMONE) { block ->
+            LootTable.builder()
+                .pool(
+                    LootPool.builder()
+                        .with(ItemEntry.builder(block))
+                        .conditionally(WITH_SILK_TOUCH_OR_SHEARS)
+                )
+        }
+
         // message in a bottle
         addDrop(HybridAquaticBlocks.MESSAGE_IN_A_BOTTLE, LootTable.builder())
 
