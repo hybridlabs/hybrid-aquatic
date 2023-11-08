c0ceac904b6116d4bfc1828c74c733d461ec7845
Daniel Orr <orrdaniel73@gmail.com>
2023-11-08 08:03:26 +0000

    Make message in a bottles placeable in water

--- src/main/kotlin/dev/hybridlabs/aquatic/item/MessageInABottleItem.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/item/MessageInABottleItem.kt
@@ -4,14 +4,13 @@ import dev.hybridlabs.aquatic.block.HybridAquaticBlocks
 import dev.hybridlabs.aquatic.block.MessageInABottleBlock
 import dev.hybridlabs.aquatic.block.MessageInABottleBlock.Variant
 import dev.hybridlabs.aquatic.block.entity.MessageInABottleBlockEntity
-import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemStack
 
 /**
  * Represents the Message in a Bottle block item.
  * @see MessageInABottleBlock
  */
-class MessageInABottleItem(settings: Settings) : BlockItem(HybridAquaticBlocks.MESSAGE_IN_A_BOTTLE, settings) {
+class MessageInABottleItem(settings: Settings) : PlaceableInWaterItem(HybridAquaticBlocks.MESSAGE_IN_A_BOTTLE, settings) {
     override fun getTranslationKey(stack: ItemStack): String {
         // custom variant translation keys
         val id = stack.getSubNbt(BLOCK_ENTITY_TAG_KEY)?.getString(MessageInABottleBlockEntity.VARIANT_KEY) ?: ""
--- src/main/kotlin/dev/hybridlabs/aquatic/item/PlaceableInWaterItem.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/item/PlaceableInWaterItem.kt
@@ -3,24 +3,22 @@ package dev.hybridlabs.aquatic.item
 import net.minecraft.block.Block
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.BlockItem
-import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.ItemUsageContext
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
-import net.minecraft.world.RaycastContext
+import net.minecraft.world.RaycastContext.FluidHandling
 import net.minecraft.world.World
 
-class PlaceableInWaterItem(block: Block, settings: Settings): BlockItem(block, settings) {
-    override fun useOnBlock(context: ItemUsageContext?): ActionResult {
+open class PlaceableInWaterItem(block: Block, settings: Settings): BlockItem(block, settings) {
+    override fun useOnBlock(context: ItemUsageContext): ActionResult {
         return ActionResult.PASS
     }
 
-    override fun use(world: World?, user: PlayerEntity, hand: Hand?): TypedActionResult<ItemStack> {
-        val blockHitResult = Item.raycast(world, user, RaycastContext.FluidHandling.SOURCE_ONLY)
+    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        val blockHitResult = raycast(world, user, FluidHandling.SOURCE_ONLY)
         val actionResult = super.useOnBlock(ItemUsageContext(user, hand, blockHitResult))
-
         return TypedActionResult(actionResult, user.getStackInHand(hand))
     }
 }
