eab2a3a913d38a02f5287a657f2de199eb399f45
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2023-11-17 19:15:00 +0200

    Added second hermit crab model Added new behavior to sea urchins

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/critter/SeaUrchinEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/critter/SeaUrchinEntity.kt
@@ -1,29 +1,35 @@
 package dev.hybridlabs.aquatic.entity.critter
 
+import net.minecraft.block.Blocks
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.damage.DamageSource
 import net.minecraft.entity.damage.DamageTypes
-import net.minecraft.entity.mob.WaterCreatureEntity
+import net.minecraft.entity.mob.PathAwareEntity
 import net.minecraft.registry.tag.DamageTypeTags
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
+import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
-class SeaUrchinEntity(entityType: EntityType<out SeaUrchinEntity>, world: World) : HybridAquaticCritterEntity(entityType, world, 4) {
+class SeaUrchinEntity(entityType: EntityType<out SeaUrchinEntity>, world: World) :
+    HybridAquaticCritterEntity(entityType, world, 4) {
+
+    private var timeUntilNextBreak = 0
+
     companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
-            return WaterCreatureEntity.createMobAttributes()
+            return PathAwareEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 3.0)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.2)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 20.0)
                 .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 100.0)
-
         }
     }
+
     override fun getHurtSound(source: DamageSource): SoundEvent {
         return SoundEvents.ENTITY_SLIME_HURT_SMALL
     }
@@ -43,29 +49,47 @@ class SeaUrchinEntity(entityType: EntityType<out SeaUrchinEntity>, world: World)
     override fun getSwimSound(): SoundEvent {
         return SoundEvents.ENTITY_SLIME_SQUISH_SMALL
     }
+
     override fun damage(source: DamageSource, amount: Float): Boolean {
         return if (world.isClient) {
             false
         } else {
-            if (!source.isIn(DamageTypeTags.AVOIDS_GUARDIAN_THORNS) && !source.isOf(
-                    DamageTypes.THORNS
-                )
-            ) {
-                val var4 = source.source
-                if (var4 is LivingEntity) {
-                    var4.damage(this.damageSources.thorns(this), 2.0f)
+            if (!source.isIn(DamageTypeTags.AVOIDS_GUARDIAN_THORNS) && !source.isOf(DamageTypes.THORNS)) {
+                val attacker = source.source
+                if (attacker is LivingEntity) {
+                    attacker.damage(this.damageSources.thorns(this), 2.0f)
                 }
             }
             super.damage(source, amount)
         }
     }
+
     override fun tick() {
         super.tick()
-        if (!isWet) {
-            this.speed = 0.01F
+
+        if (world.isClient) {
+            return
+        }
+
+        if (timeUntilNextBreak > 0) {
+            timeUntilNextBreak--
+            return
+        }
+
+        if (world.random.nextInt(6000) < 300) {
+            breakKelpUnderneath()
+            timeUntilNextBreak = 2400 + world.random.nextInt(1200)
         }
     }
-    override fun getMaxSize() : Int {
+
+    private fun breakKelpUnderneath() {
+        val posUnderneath = BlockPos(this.x.toInt(), (this.y+1).toInt(), this.z.toInt())
+        if (world.getBlockState(posUnderneath).isOf(Blocks.KELP_PLANT)) {
+            world.setBlockState(posUnderneath, Blocks.AIR.defaultState)
+        }
+    }
+
+    override fun getMaxSize(): Int {
         return 5
     }
 
