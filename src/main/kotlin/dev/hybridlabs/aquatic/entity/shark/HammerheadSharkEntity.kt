cf17dd9ba0a0c8dc6dd20fe3109c488856b4c269
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-20 13:32:46 -0300

    Update HammerheadSharkEntity.kt

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/HammerheadSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/HammerheadSharkEntity.kt
@@ -25,10 +25,11 @@ class HammerheadSharkEntity(entityType: EntityType<out HammerheadSharkEntity>, w
     companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 30.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.8)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 45.0)
+		.add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 9.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.6)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 16.0)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
         }
     }
 
