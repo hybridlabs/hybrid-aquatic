d851f9374b7f06b27c1f7a1a18c95dc342ae70c8
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-20 13:33:50 -0300

    Update TigerSharkEntity.kt

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/TigerSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/TigerSharkEntity.kt
@@ -25,10 +25,11 @@ class TigerSharkEntity(entityType: EntityType<out TigerSharkEntity>, world: Worl
         companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 36.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.8)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 54.0)
+		.add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 9.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.5)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 8.0)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 16.0)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
                 .add(EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, 100.0)
         }
     }
