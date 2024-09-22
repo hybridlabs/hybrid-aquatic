58c1af2a86454cfe12d09359c9e726da7f91b552
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-22 12:52:06 -0300

    Update GreatWhiteSharkEntity.kt removed armor toughness

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/GreatWhiteSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/GreatWhiteSharkEntity.kt
@@ -26,7 +26,6 @@ class GreatWhiteSharkEntity(entityType: EntityType<out GreatWhiteSharkEntity>, w
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 54.0)
-		.add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 9.0)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.6)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 8.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
