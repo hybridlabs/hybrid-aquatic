1511a5e3258e15ed619b43db9b9869971aeaf3e7
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-22 12:52:38 -0300

    Update ThresherSharkEntity.kt removed armor toughness

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/ThresherSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/ThresherSharkEntity.kt
@@ -26,7 +26,6 @@ class ThresherSharkEntity(entityType: EntityType<out ThresherSharkEntity>, world
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 36.0)
-		.add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 9.0)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.8)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
