782085dc5d89c1cfa886812b708599fb359072e2
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-22 12:52:21 -0300

    Update HammerheadSharkEntity.kt removed armor toughness

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/HammerheadSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/HammerheadSharkEntity.kt
@@ -26,7 +26,6 @@ class HammerheadSharkEntity(entityType: EntityType<out HammerheadSharkEntity>, w
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 45.0)
-		.add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 9.0)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.6)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 4.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
