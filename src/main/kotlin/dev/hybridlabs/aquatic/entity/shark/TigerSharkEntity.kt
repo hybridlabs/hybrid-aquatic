0b11b4ea143c059c2b247a807f2b7ae0149699a7
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-22 12:52:53 -0300

    Update TigerSharkEntity.kt removed armor toughness

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/TigerSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/TigerSharkEntity.kt
@@ -26,7 +26,6 @@ class TigerSharkEntity(entityType: EntityType<out TigerSharkEntity>, world: Worl
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return WaterCreatureEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 54.0)
-		.add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 9.0)
                 .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 1.5)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 8.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 26.0)
