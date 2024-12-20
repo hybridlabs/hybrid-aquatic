7f1bd58775ff164a1a7a25127bee7b76bea13e65
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2024-12-20 17:33:49 +0200

    Edited buoy hitbox again

--- src/main/kotlin/dev/hybridlabs/aquatic/block/BuoyBlock.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/block/BuoyBlock.kt
@@ -98,7 +98,7 @@ open class BuoyBlock(settings: Settings): Block(settings), BlockEntityProvider, 
         val FACING: DirectionProperty = HorizontalFacingBlock.FACING
 
         private val CUBE_SHAPE: VoxelShape = createCuboidShape(0.5, 3.0, 0.5, 15.5, 16.0, 15.5)
-        private val POLE_SHAPE: VoxelShape = createCuboidShape(4.0, 0.0, 3.0, 12.0, 2.0, 13.0)
+        private val POLE_SHAPE: VoxelShape = createCuboidShape(4.0, 16.0, 6.0, 12.0, 48.0, 10.0)
 
         private val SHAPE: VoxelShape = VoxelShapes.union(CUBE_SHAPE, POLE_SHAPE)
         private val COLLISION_SHAPE: VoxelShape = VoxelShapes.union(CUBE_SHAPE, POLE_SHAPE)
