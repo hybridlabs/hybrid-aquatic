b08aba8ac1d0266cebe7a8908baa56820ab005a8
dragoncommands <dragoncommands>
2025-02-05 15:07:19 -0500

    Added throwing star item type: Added:  - ThrowingStarItem  - Throwing star item Changed:  - Throwing stars to inherit from thrown item

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/miscellaneous/ThrowingStarEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/miscellaneous/ThrowingStarEntity.kt
@@ -2,7 +2,8 @@ package dev.hybridlabs.aquatic.entity.miscellaneous
 
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
-import net.minecraft.entity.projectile.ProjectileEntity
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.world.World
@@ -10,24 +11,27 @@ import net.minecraft.world.World
 /**
  *
  */
-class ThrowingStarEntity(entityType: EntityType<out ProjectileEntity>, world: World?) :
-    ProjectileEntity(
-                entityType,
-                world
-    ) {
+class ThrowingStarEntity(entityType: EntityType<out ThrownItemEntity>?, world: World?) :
+        ThrownItemEntity(entityType, world) {
 
     var DisplayItem : ItemStack = Items.COOKED_COD.defaultStack;
 
     override fun initDataTracker() {}
 
     override fun tick() {
-
         super.tick()
     }
 
-    companion object {
-        fun spawnThrowingStar(player : Entity, magnitude: Float) {
+    override fun isCollidable(): Boolean {
+        return super.isCollidable()
+    }
 
+    override fun getDefaultItem(): Item {
+        return Items.COOKED_COD;
+    }
+
+    companion object {
+        fun applyVelocity(player : Entity, magnitude: Float) {
         }
     }
 }
