a447e5ddff08e8e8977adc67c9e4c31aa917a62b
dragoncommands <dragoncommands>
2025-02-09 17:30:20 -0500

    new Throwing star renderer Added:  - Rotation within throwing star animation Changed:  - Throwing star has been transitioned into the new Lodged item class   - movement to be reimplemented soon

--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/misc/ThrowingStarEntityRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/misc/ThrowingStarEntityRenderer.kt
@@ -12,20 +12,50 @@ import net.minecraft.client.render.model.json.ModelTransformationMode
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.item.Items
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.Direction
+import net.minecraft.util.math.MathHelper
+import net.minecraft.util.math.RotationAxis
+import net.minecraft.util.math.Vec3d
 import software.bernie.geckolib.renderer.GeoEntityRenderer
+import java.lang.Math.pow
+import kotlin.math.absoluteValue
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class ThrowingStarEntityRenderer(renderManager: EntityRendererFactory.Context?) : EntityRenderer<ThrowingStarEntity>(renderManager) {
 
     private val itemRenderer: ItemRenderer = renderManager!!.itemRenderer;
 
     override fun render(entity: ThrowingStarEntity?, yaw: Float, tickDelta: Float, matrices: MatrixStack?, vertexConsumers: VertexConsumerProvider?, light: Int) {
-        val stack = Items.STONE.defaultStack;
+        val stack = entity!!.displayItem;
 
-        val bakedModel = itemRenderer.getModel(stack, entity!!.world, null, 0)
+        val worldTime = entity.world.time;
+//        val lerpedRotationSpeed = MathHelper.lerp(1 / normalizeXZ(entity.velocity), 0.0f, MAX_SPEED)
+
+        matrices!!.multiply(RotationAxis.NEGATIVE_X.rotationDegrees(90f));
+        matrices.multiply(RotationAxis.NEGATIVE_Z.rotationDegrees((worldTime * tickDelta) * 5))
+
+
+        val bakedModel = itemRenderer.getModel(stack, entity.world, null, 0)
         itemRenderer.renderItem(stack, ModelTransformationMode.FIXED, false, matrices, vertexConsumers, light, OverlayTexture.DEFAULT_UV, bakedModel);
     }
 
     override fun getTexture(entity: ThrowingStarEntity?): Identifier? {
         return null;
+    }
+
+    companion object {
+
+        val MAX_SPEED = 5.0f;
+
+        /**
+         * Combine X and Z motion into one vector
+         */
+        fun normalizeXZ(pos : Vec3d) : Float {
+            val a = pos.x.toFloat();
+            val b = pos.z.toFloat();
+
+            return sqrt((a * a) + (b * b)).absoluteValue;
+        }
     }
 }
--- src/main/kotlin/dev/hybridlabs/aquatic/entity/miscellaneous/ThrowingStarEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/miscellaneous/ThrowingStarEntity.kt
@@ -1,36 +1,77 @@
 package dev.hybridlabs.aquatic.entity.miscellaneous
 
-import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
-import net.minecraft.entity.projectile.thrown.SnowballEntity
+import net.minecraft.entity.FlyingItemEntity
+import net.minecraft.entity.data.DataTracker
+import net.minecraft.entity.data.TrackedData
+import net.minecraft.entity.data.TrackedDataHandlerRegistry
+import net.minecraft.entity.projectile.ArrowEntity
+import net.minecraft.entity.projectile.PersistentProjectileEntity
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.nbt.NbtCompound
 import net.minecraft.world.World
 
 /**
  *
  */
-class ThrowingStarEntity(entityType: EntityType<out ThrownItemEntity>?, world: World?) :
-        ThrownItemEntity(entityType, world) {
+class ThrowingStarEntity(entityType: EntityType<out PersistentProjectileEntity>?, world: World?) :
+        PersistentProjectileEntity(entityType, world), FlyingItemEntity {
 
-    var DisplayItem : ItemStack = Items.COOKED_COD.defaultStack;
+    var displayItem : ItemStack = Items.COOKED_COD.defaultStack;
+
+    // Create a basic expiration date for the projectile. This will be renewed upon landing.
+    private var expirationDate : Long = world!!.time + INITIAL_LIFETIME;
+
+    override fun initDataTracker() {
+        super.initDataTracker()
+
+        dataTracker.startTracking(INITIAL_MOVEMENT_VEC, 0.0f)
+        dataTracker.startTracking(EXPIRATION_DATE, 0)
+    }
 
     override fun tick() {
         super.tick()
+
+        if (world.time > expirationDate) {
+            this.remove(RemovalReason.DISCARDED)
+        }
+    }
+
+    override fun writeCustomDataToNbt(nbt: NbtCompound?) {
+        super.writeCustomDataToNbt(nbt!!)
+        nbt.putLong("expirationDate", expirationDate);
+    }
+
+    override fun readCustomDataFromNbt(nbt: NbtCompound?) {
+        super.readCustomDataFromNbt(nbt!!)
+        nbt.getLong("expirationDate")
     }
 
     override fun isCollidable(): Boolean {
         return super.isCollidable()
     }
 
-    override fun getDefaultItem(): Item {
-        return Items.COOKED_COD;
+    override fun asItemStack(): ItemStack {
+        TODO("Not yet implemented")
     }
 
     companion object {
-        fun applyVelocity(player : Entity, magnitude: Float) {
-        }
+        val INITIAL_MOVEMENT_VEC : TrackedData<Float> =
+                DataTracker.registerData(ThrowingStarEntity::class.java, TrackedDataHandlerRegistry.FLOAT)
+
+        val EXPIRATION_DATE : TrackedData<Long> =
+                DataTracker.registerData(ThrowingStarEntity::class.java, TrackedDataHandlerRegistry.LONG)
+
+        const val INITIAL_LIFETIME = (20 * 10)  // Lives for 10 seconds in the air
+        const val EMBEDDED_LIFETIME = (20 * 20)  // Lives for 20 seconds upon landing
+
     }
+
+    override fun getStack(): ItemStack {
+        return displayItem
+    }
+
 }
