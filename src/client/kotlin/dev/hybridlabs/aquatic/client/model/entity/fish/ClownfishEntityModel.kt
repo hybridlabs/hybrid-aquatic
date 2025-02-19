fd46baa51b1d41050c48c486438bb375fc2ba538
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-02-19 23:50:59 +0200

    Cleanup

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/ClownfishEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/ClownfishEntityModel.kt
@@ -2,5 +2,4 @@ package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
 
-
 class ClownfishEntityModel : HybridAquaticFishEntityModel<HybridAquaticFishEntity>("clownfish")
