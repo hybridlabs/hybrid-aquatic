d819a909ff97b169653b6c8b56373471e3ece9b9
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-02-19 03:45:19 +0200

    Removed training entity used for Madi

--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/fish/GayFishEntityRenderer.kt
+++
@@ -1,8 +0,0 @@
-package dev.hybridlabs.aquatic.client.render.entity.fish
-
-import dev.hybridlabs.aquatic.client.model.entity.fish.GayFishEntityModel
-import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
-import net.minecraft.client.render.entity.EntityRendererFactory.Context
-
-class GayFishEntityRenderer(context: Context) : HybridAquaticFishEntityRenderer<HybridAquaticFishEntity>(context,
-   GayFishEntityModel(), false, false)
