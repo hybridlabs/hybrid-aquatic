1db44f1dd4420d6a1e8210126df0507dfd72f3e2
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-02-20 03:27:26 +0200

    Just testing something

--- src/main/kotlin/dev/hybridlabs/aquatic/config/HybridAquaticConfig.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/config/HybridAquaticConfig.kt
@@ -8,7 +8,7 @@ data class HybridAquaticConfig(
      * The version of the data stored.
      * Increase when the config needs to be reset, i.e. when new entity spawn configs are added.
      */
-    val dataVersion: Int = 2,
+    val dataVersion: Int = 3,
 
     val entitySpawnConfig: List<EntitySpawnConfig> = EntitySpawnConfigGenerator.generate(),
 ) {
