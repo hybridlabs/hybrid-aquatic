00ab152ec8edc7d035a556b2a2bcd64aa39ba857
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-06-25 09:15:50 +0300

    Slight cleanup

--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/shark/WhaleSharkEntityRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/shark/WhaleSharkEntityRenderer.kt
@@ -1,7 +1,7 @@
 package dev.hybridlabs.aquatic.client.render.entity.shark
 
 import dev.hybridlabs.aquatic.client.model.entity.shark.WhaleSharkEntityModel
-import dev.hybridlabs.aquatic.entity.shark.HybridAquaticSharkEntity
+import dev.hybridlabs.aquatic.entity.shark.WhaleSharkEntity
 import net.minecraft.client.render.entity.EntityRendererFactory.Context
 
-class WhaleSharkEntityRenderer(context: Context) : HybridAquaticSharkEntityRenderer<HybridAquaticSharkEntity>(context, WhaleSharkEntityModel(), true)
+class WhaleSharkEntityRenderer(context: Context) : HybridAquaticSharkEntityRenderer<WhaleSharkEntity>(context, WhaleSharkEntityModel(), true)
