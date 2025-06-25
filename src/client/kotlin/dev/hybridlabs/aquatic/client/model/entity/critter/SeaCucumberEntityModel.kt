97f116ce2d14c467c0e7f803b74e6296f1a39827
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-06-26 02:59:33 +0300

    Sea cucumber variants redone + added 5 new variants

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/critter/SeaCucumberEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/critter/SeaCucumberEntityModel.kt
@@ -1,7 +1,38 @@
 package dev.hybridlabs.aquatic.client.model.entity.critter
 
-import dev.hybridlabs.aquatic.entity.critter.HybridAquaticCritterEntity
+import dev.hybridlabs.aquatic.entity.critter.SeaCucumberEntity
 import net.minecraft.util.Identifier
 
-class SeaCucumberEntityModel : HybridAquaticCritterEntityModel<HybridAquaticCritterEntity>("sea_cucumber")
+class SeaCucumberEntityModel : HybridAquaticCritterEntityModel<SeaCucumberEntity>("sea_cucumber") {
+
+    private val BLACK_TEATFISH_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/critter/sea_cucumber/sea_cucumber_black_teatfish.png")
+    private val WHITE_TEATFISH_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/critter/sea_cucumber/sea_cucumber_white_teatfish.png")
+    private val GREENFISH_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/critter/sea_cucumber/sea_cucumber_greenfish.png")
+    private val PRICKLY_REDFISH_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/critter/sea_cucumber/sea_cucumber_prickly_redfish.png")
+    private val CURRYFISH_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/critter/sea_cucumber/sea_cucumber_curryfish.png")
+    private val SANDFISH_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/critter/sea_cucumber/sea_cucumber_sandfish.png")
+    private val SEA_PIG_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/critter/sea_cucumber/sea_cucumber_sea_pig.png")
+
+    private val SEA_PIG_MODEL = Identifier("hybrid-aquatic", "geo/critter/sea_cucumber/sea_cucumber_sea_pig.geo.json")
+    private val SEA_CUCUMBER_MODEL = Identifier("hybrid-aquatic", "geo/critter/sea_cucumber/sea_cucumber.geo.json")
+
+    override fun getTextureResource(animatable: SeaCucumberEntity): Identifier {
+        return when (animatable.variant) {
+            SeaCucumberEntity.Type.BLACK_TEATFISH -> BLACK_TEATFISH_TEXTURE
+            SeaCucumberEntity.Type.WHITE_TEATFISH -> WHITE_TEATFISH_TEXTURE
+            SeaCucumberEntity.Type.GREENFISH -> GREENFISH_TEXTURE
+            SeaCucumberEntity.Type.PRICKLY_REDFISH -> PRICKLY_REDFISH_TEXTURE
+            SeaCucumberEntity.Type.CURRYFISH -> CURRYFISH_TEXTURE
+            SeaCucumberEntity.Type.SANDFISH -> SANDFISH_TEXTURE
+            SeaCucumberEntity.Type.SEA_PIG -> SEA_PIG_TEXTURE
+        }
+    }
+
+    override fun getModelResource(animatable: SeaCucumberEntity): Identifier {
+        return when (animatable.variant) {
+            SeaCucumberEntity.Type.SEA_PIG -> SEA_PIG_MODEL
+            else -> SEA_CUCUMBER_MODEL
+        }
+    }
+}
 
--- src/main/kotlin/dev/hybridlabs/aquatic/entity/critter/SeaCucumberEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/critter/SeaCucumberEntity.kt
@@ -1,16 +1,34 @@
 package dev.hybridlabs.aquatic.entity.critter
 
 import dev.hybridlabs.aquatic.entity.HybridAquaticEntityTypes
+import net.minecraft.entity.EntityData
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnReason
+import net.minecraft.entity.VariantHolder
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.data.DataTracker
+import net.minecraft.entity.data.TrackedData
+import net.minecraft.entity.data.TrackedDataHandlerRegistry
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.registry.tag.BiomeTags
+import net.minecraft.util.StringIdentifiable
+import net.minecraft.util.function.ValueLists
+import net.minecraft.world.LocalDifficulty
+import net.minecraft.world.ServerWorldAccess
 import net.minecraft.world.World
+import net.minecraft.world.biome.Biome
 import software.bernie.geckolib.core.animatable.GeoAnimatable
 import software.bernie.geckolib.core.animation.AnimationState
 import software.bernie.geckolib.core.`object`.PlayState
+import java.util.function.IntFunction
+import kotlin.random.Random
 
+@Suppress("DEPRECATION")
 class SeaCucumberEntity(entityType: EntityType<out SeaCucumberEntity>, world: World) :
-    HybridAquaticCritterEntity(entityType, world) {
+    HybridAquaticCritterEntity(entityType, world),
+    VariantHolder<SeaCucumberEntity.Type> {
 
     override fun remove(reason: RemovalReason) {
         if (!world.isClient && this.isDead) {
@@ -54,7 +72,22 @@ class SeaCucumberEntity(entityType: EntityType<out SeaCucumberEntity>, world: Wo
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 0.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 2.0)
         }
+        val TYPE: TrackedData<Int> = DataTracker.registerData(SeaCucumberEntity::class.java, TrackedDataHandlerRegistry.INTEGER)
     }
+
+    override fun initialize(
+        world: ServerWorldAccess,
+        difficulty: LocalDifficulty,
+        spawnReason: SpawnReason,
+        entityData: EntityData?,
+        entityNbt: NbtCompound?
+    ): EntityData? {
+        val biome = world.getBiome(this.blockPos)
+        val selectedType = Type.fromBiome(biome, Random.Default)
+        this.variant = selectedType
+        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt)
+    }
+
 
     override fun <E : GeoAnimatable> predicate(event: AnimationState<E>): PlayState {
         if (isSubmergedInWater) {
@@ -69,5 +102,68 @@ class SeaCucumberEntity(entityType: EntityType<out SeaCucumberEntity>, world: Wo
 
     override fun getMinSize(): Int {
         return -5
+    }
+
+    override fun initDataTracker() {
+        dataTracker.startTracking(TYPE, 0)
+        super.initDataTracker()
+    }
+
+    override fun writeCustomDataToNbt(nbt: NbtCompound) {
+        nbt.putString("Type", this.variant.asString())
+        super.writeCustomDataToNbt(nbt)
+    }
+
+    override fun readCustomDataFromNbt(nbt: NbtCompound) {
+        this.variant = Type.byName(nbt.getString("Type"))
+        super.readCustomDataFromNbt(nbt)
+    }
+
+    enum class Type(val id: Int, private val key: String) : StringIdentifiable {
+        BLACK_TEATFISH(0, "black_teatfish"),
+        WHITE_TEATFISH(1, "white_teatfish"),
+        GREENFISH(2, "greenfish"),
+        PRICKLY_REDFISH(3, "prickly_redfish"),
+        CURRYFISH(4, "curryfish"),
+        SANDFISH(5, "sandfish"),
+        SEA_PIG(6, "sea_pig");
+
+        override fun asString(): String {
+            return this.key
+        }
+
+        companion object {
+            val CODEC: StringIdentifiable.Codec<Type> = StringIdentifiable.createCodec { entries.toTypedArray() }
+            private val BY_ID: IntFunction<Type> = ValueLists.createIdToValueFunction(
+                { obj: Type -> obj.id },
+                entries.toTypedArray(),
+                ValueLists.OutOfBoundsHandling.ZERO
+            )
+
+            fun byName(name: String?): Type {
+                return CODEC.byId(name, BLACK_TEATFISH) as Type
+            }
+
+            fun fromId(id: Int): Type {
+                return BY_ID.apply(id) as Type
+            }
+
+            fun fromBiome(biome: RegistryEntry<Biome?>, random: Random): Type {
+                return if (biome.isIn(BiomeTags.IS_DEEP_OCEAN)) {
+                    SEA_PIG
+                } else {
+                    val randomId = random.nextInt(0, 6)
+                    fromId(randomId)
+                }
+            }
+        }
+    }
+
+    override fun getVariant(): Type {
+        return Type.fromId((dataTracker.get(TYPE) as Int))
+    }
+
+    override fun setVariant(type: Type) {
+        dataTracker.set(TYPE, type.id)
     }
 }
