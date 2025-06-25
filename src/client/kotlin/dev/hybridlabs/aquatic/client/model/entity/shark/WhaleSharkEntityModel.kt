b276b6579f7ba1c883e759489a90a5cd9a34a540
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-06-25 09:15:50 +0300

    Started variant system rework, added 2 new whale shark colors

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/shark/WhaleSharkEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/shark/WhaleSharkEntityModel.kt
@@ -1,5 +1,19 @@
 package dev.hybridlabs.aquatic.client.model.entity.shark
 
-import dev.hybridlabs.aquatic.entity.shark.HybridAquaticSharkEntity
+import dev.hybridlabs.aquatic.entity.shark.WhaleSharkEntity
+import net.minecraft.util.Identifier
 
-class WhaleSharkEntityModel : HybridAquaticSharkEntityModel<HybridAquaticSharkEntity>("whale_shark")
+class WhaleSharkEntityModel : HybridAquaticSharkEntityModel<WhaleSharkEntity>("whale_shark") {
+
+    private val BLUE_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/shark/whale_shark.png")
+    private val BROWN_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/shark/whale_shark_brown.png")
+    private val GRAY_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/shark/whale_shark_gray.png")
+
+    override fun getTextureResource(animatable: WhaleSharkEntity): Identifier {
+        return when (animatable.variant) {
+            WhaleSharkEntity.Type.BLUE -> BLUE_TEXTURE
+            WhaleSharkEntity.Type.BROWN -> BROWN_TEXTURE
+            WhaleSharkEntity.Type.GRAY -> GRAY_TEXTURE
+        }
+    }
+}
--- src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/WhaleSharkEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/WhaleSharkEntity.kt
@@ -1,16 +1,31 @@
 package dev.hybridlabs.aquatic.entity.shark
 
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
+import net.minecraft.entity.EntityData
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnReason
+import net.minecraft.entity.VariantHolder
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.data.DataTracker
+import net.minecraft.entity.data.TrackedData
+import net.minecraft.entity.data.TrackedDataHandlerRegistry
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.util.StringIdentifiable
+import net.minecraft.util.function.ValueLists
+import net.minecraft.world.LocalDifficulty
+import net.minecraft.world.ServerWorldAccess
 import net.minecraft.world.World
 import software.bernie.geckolib.core.animation.AnimatableManager
 import software.bernie.geckolib.core.animation.AnimationController
 import software.bernie.geckolib.core.animation.RawAnimation
+import java.util.function.IntFunction
+import kotlin.random.Random
 
+@Suppress("DEPRECATION")
 class WhaleSharkEntity(entityType: EntityType<out WhaleSharkEntity>, world: World) :
-    HybridAquaticSharkEntity(entityType, world, listOf(HybridAquaticEntityTags.NONE), true, false) {
+    HybridAquaticSharkEntity(entityType, world, listOf(HybridAquaticEntityTags.NONE), true, false),
+    VariantHolder<WhaleSharkEntity.Type> {
 
     private var isFeeding = false
 
@@ -23,6 +38,17 @@ class WhaleSharkEntity(entityType: EntityType<out WhaleSharkEntity>, world: Worl
             state.setAndContinue(animation)
         })
         super.registerControllers(controllerRegistrar)
+    }
+
+    override fun initialize(
+        world: ServerWorldAccess,
+        difficulty: LocalDifficulty,
+        spawnReason: SpawnReason,
+        entityData: EntityData?,
+        entityNbt: NbtCompound?
+    ): EntityData? {
+        setVariant(Type.entries.random(Random))
+        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt)
     }
 
     override fun tick() {
@@ -58,6 +84,7 @@ class WhaleSharkEntity(entityType: EntityType<out WhaleSharkEntity>, world: Worl
 
         val MOUTH_OPEN: RawAnimation = RawAnimation.begin().thenPlay("misc.mouth_open")
         val MOUTH_CLOSED: RawAnimation = RawAnimation.begin().thenPlay("misc.mouth_closed")
+        val TYPE: TrackedData<Int> = DataTracker.registerData(WhaleSharkEntity::class.java, TrackedDataHandlerRegistry.INTEGER)
     }
 
     override fun getMaxSize(): Int {
@@ -66,5 +93,55 @@ class WhaleSharkEntity(entityType: EntityType<out WhaleSharkEntity>, world: Worl
 
     override fun getMinSize(): Int {
         return -5
+    }
+
+    override fun initDataTracker() {
+        dataTracker.startTracking(TYPE, 0)
+        super.initDataTracker()
+    }
+
+    override fun writeCustomDataToNbt(nbt: NbtCompound) {
+        nbt.putString("Type", this.variant.asString())
+        super.writeCustomDataToNbt(nbt)
+    }
+
+    override fun readCustomDataFromNbt(nbt: NbtCompound) {
+        this.variant = Type.byName(nbt.getString("Type"))
+        super.readCustomDataFromNbt(nbt)
+    }
+
+    enum class Type(val id: Int, private val key: String) : StringIdentifiable {
+        BLUE(0, "blue"),
+        BROWN(1, "brown"),
+        GRAY(2, "gray");
+
+        override fun asString(): String {
+            return this.key
+        }
+
+        companion object {
+            val CODEC: StringIdentifiable.Codec<Type> = StringIdentifiable.createCodec { entries.toTypedArray() }
+            private val BY_ID: IntFunction<Type> = ValueLists.createIdToValueFunction(
+                { obj: Type -> obj.id },
+                entries.toTypedArray(),
+                ValueLists.OutOfBoundsHandling.ZERO
+            )
+
+            fun byName(name: String?): Type {
+                return CODEC.byId(name, BLUE) as Type
+            }
+
+            fun fromId(id: Int): Type {
+                return BY_ID.apply(id) as Type
+            }
+        }
+    }
+
+    override fun getVariant(): Type {
+        return Type.fromId((dataTracker.get(TYPE) as Int))
+    }
+
+    override fun setVariant(type: Type) {
+        dataTracker.set(TYPE, type.id)
     }
 }
