0a449fc8420ee1df08b4d2acbef17e596dc1bef4
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-06-28 22:50:36 +0300

    Started clownfish variants, remember to adjust eye height

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/ClownfishEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/ClownfishEntityModel.kt
@@ -1,5 +1,31 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
-import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
+import dev.hybridlabs.aquatic.entity.fish.ClownfishEntity
+import net.minecraft.util.Identifier
 
-class ClownfishEntityModel : HybridAquaticFishEntityModel<HybridAquaticFishEntity>("clownfish")
+class ClownfishEntityModel : HybridAquaticFishEntityModel<ClownfishEntity>("clownfish") {
+
+    private val OCELLARIS_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/fish/clownfish/clownfish_ocellaris.png")
+    private val CLARKII_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/fish/clownfish/clownfish_clarkii.png")
+    private val TOMATO_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/fish/clownfish/clownfish_tomato.png")
+    private val CINNAMON_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/fish/clownfish/clownfish_cinnamon.png")
+
+    private val SMALL_CLOWNFISH_MODEL = Identifier("hybrid-aquatic", "geo/fish/clownfish/clownfish_small.geo.json")
+    private val BIG_CLOWNFISH_MODEL = Identifier("hybrid-aquatic", "geo/fish/clownfish/clownfish_big.geo.json")
+
+    override fun getTextureResource(animatable: ClownfishEntity): Identifier {
+        return when (animatable.variant) {
+            ClownfishEntity.Type.OCELLARIS -> OCELLARIS_TEXTURE
+            ClownfishEntity.Type.CLARKII -> CLARKII_TEXTURE
+            ClownfishEntity.Type.TOMATO -> TOMATO_TEXTURE
+            ClownfishEntity.Type.CINNAMON -> CINNAMON_TEXTURE
+        }
+    }
+
+    override fun getModelResource(animatable: ClownfishEntity): Identifier {
+        return when (animatable.variant) {
+            ClownfishEntity.Type.OCELLARIS -> BIG_CLOWNFISH_MODEL
+            else -> SMALL_CLOWNFISH_MODEL
+        }
+    }
+}
--- src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/fish/ClownfishEntityRenderer.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/render/entity/fish/ClownfishEntityRenderer.kt
@@ -1,8 +1,8 @@
 package dev.hybridlabs.aquatic.client.render.entity.fish
 
 import dev.hybridlabs.aquatic.client.model.entity.fish.ClownfishEntityModel
-import dev.hybridlabs.aquatic.entity.fish.HybridAquaticFishEntity
+import dev.hybridlabs.aquatic.entity.fish.ClownfishEntity
 import net.minecraft.client.render.entity.EntityRendererFactory.Context
 
-class ClownfishEntityRenderer(context: Context) : HybridAquaticFishEntityRenderer<HybridAquaticFishEntity>(context,
+class ClownfishEntityRenderer(context: Context) : HybridAquaticFishEntityRenderer<ClownfishEntity>(context,
     ClownfishEntityModel(), true, false)
