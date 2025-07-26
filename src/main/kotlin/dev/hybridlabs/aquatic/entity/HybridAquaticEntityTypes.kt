63c56148624583e657643782c514cc2a6e4356dd
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-07-26 07:11:52 +0300

    Made seahorse hitboxes smaller

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/HybridAquaticEntityTypes.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/HybridAquaticEntityTypes.kt
@@ -270,7 +270,7 @@ object HybridAquaticEntityTypes {
     val SEAHORSE = registerFish(
         "seahorse",
         ::SeahorseEntity,
-        EntityDimensions.fixed(0.5f, 0.7f),
+        EntityDimensions.fixed(0.3f, 0.5f),
         SeahorseEntity.createMobAttributes()
     )
 
