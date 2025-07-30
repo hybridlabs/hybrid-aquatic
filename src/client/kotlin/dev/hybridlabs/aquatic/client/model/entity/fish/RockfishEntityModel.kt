6a2d199f7c36e2878ac699c8fe1c3eb70f26e811
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-07-30 04:46:31 +0300

    Rockfish variants and animation rework

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/RockfishEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/RockfishEntityModel.kt
@@ -1,5 +1,19 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.RockfishEntity
+import net.minecraft.util.Identifier
 
-class RockfishEntityModel : HybridAquaticFishEntityModel<RockfishEntity>("rockfish")
+class RockfishEntityModel : HybridAquaticFishEntityModel<RockfishEntity>("rockfish") {
+
+    private val VERMILION_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/fish/rockfish/vermilion_rockfish.png")
+    private val COPPER_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/fish/rockfish/copper_rockfish.png")
+    private val YELLOWEYE_TEXTURE = Identifier("hybrid-aquatic", "textures/entity/fish/rockfish/yelloweye_rockfish.png")
+
+    override fun getTextureResource(animatable: RockfishEntity): Identifier {
+        return when (animatable.variant) {
+            RockfishEntity.Type.VERMILION -> VERMILION_TEXTURE
+            RockfishEntity.Type.COPPER -> COPPER_TEXTURE
+            RockfishEntity.Type.YELLOWEYE -> YELLOWEYE_TEXTURE
+        }
+    }
+}
--- src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/RockfishEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/RockfishEntity.kt
@@ -1,25 +1,53 @@
 package dev.hybridlabs.aquatic.entity.fish
 
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
+import net.minecraft.entity.EntityData
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnReason
+import net.minecraft.entity.VariantHolder
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.data.DataTracker
+import net.minecraft.entity.data.TrackedData
+import net.minecraft.entity.data.TrackedDataHandlerRegistry
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.util.StringIdentifiable
+import net.minecraft.util.function.ValueLists
+import net.minecraft.world.LocalDifficulty
+import net.minecraft.world.ServerWorldAccess
 import net.minecraft.world.World
+import java.util.function.IntFunction
+import kotlin.random.Random
 
 class RockfishEntity(entityType: EntityType<out RockfishEntity>, world: World) :
-    HybridAquaticSchoolingFishEntity(entityType, world,
+    HybridAquaticSchoolingFishEntity(
+        entityType, world,
         listOf(
-            HybridAquaticEntityTags.NONE),
+            HybridAquaticEntityTags.NONE
+        ),
         listOf(
             HybridAquaticEntityTags.LARGE_PREY,
             HybridAquaticEntityTags.SHARK,
-            )) {
+        )
+    ),
+    VariantHolder<RockfishEntity.Type> {
 
     override fun getLimitPerChunk(): Int {
         return 3
     }
 
-        companion object {
+    override fun initialize(
+        world: ServerWorldAccess,
+        difficulty: LocalDifficulty,
+        spawnReason: SpawnReason,
+        entityData: EntityData?,
+        entityNbt: NbtCompound?
+    ): EntityData? {
+        variant = Type.entries.random(Random)
+        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt)
+    }
+
+    companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return createLivingAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 6.0)
@@ -28,5 +56,58 @@ class RockfishEntity(entityType: EntityType<out RockfishEntity>, world: World) :
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 0.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 8.0)
         }
+
+        val TYPE: TrackedData<Int> =
+            DataTracker.registerData(RockfishEntity::class.java, TrackedDataHandlerRegistry.INTEGER)
+    }
+
+    override fun initDataTracker() {
+        dataTracker.startTracking(TYPE, 0)
+        super.initDataTracker()
+    }
+
+    override fun writeCustomDataToNbt(nbt: NbtCompound) {
+        nbt.putString("Type", this.variant.asString())
+        super.writeCustomDataToNbt(nbt)
+    }
+
+    override fun readCustomDataFromNbt(nbt: NbtCompound) {
+        this.variant = Type.byName(nbt.getString("Type"))
+        super.readCustomDataFromNbt(nbt)
+    }
+
+    enum class Type(val id: Int, private val key: String) : StringIdentifiable {
+        VERMILION(0, "vermilion"),
+        COPPER(1, "copper"),
+        YELLOWEYE(2, "yelloweye");
+
+        override fun asString(): String {
+            return this.key
+        }
+
+        companion object {
+            val CODEC: StringIdentifiable.Codec<Type> = StringIdentifiable.createCodec { entries.toTypedArray() }
+            private val BY_ID: IntFunction<Type> = ValueLists.createIdToValueFunction(
+                { obj: Type -> obj.id },
+                entries.toTypedArray(),
+                ValueLists.OutOfBoundsHandling.ZERO
+            )
+
+            fun byName(name: String?): Type {
+                return CODEC.byId(name, VERMILION) as Type
+            }
+
+            fun fromId(id: Int): Type {
+                return BY_ID.apply(id) as Type
+            }
+        }
+    }
+
+    override fun getVariant(): Type {
+        return Type.fromId((dataTracker.get(TYPE) as Int))
+    }
+
+    override fun setVariant(type: Type) {
+        dataTracker.set(TYPE, type.id)
     }
 }
