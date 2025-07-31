26f4ac762344da18d00daaabba84c87e59123c56
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-01 00:16:31 +0300

    Added whitespotted boxfish variant

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/BoxfishEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/BoxfishEntity.kt
@@ -1,10 +1,26 @@
 package dev.hybridlabs.aquatic.entity.fish
 
+import dev.hybridlabs.aquatic.tag.HybridAquaticBiomeTags
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
+import net.minecraft.entity.EntityData
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.SpawnReason
+import net.minecraft.entity.VariantHolder
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.data.DataTracker
+import net.minecraft.entity.data.TrackedData
+import net.minecraft.entity.data.TrackedDataHandlerRegistry
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.util.StringIdentifiable
+import net.minecraft.util.function.ValueLists
+import net.minecraft.world.LocalDifficulty
+import net.minecraft.world.ServerWorldAccess
 import net.minecraft.world.World
+import net.minecraft.world.biome.Biome
+import java.util.function.IntFunction
+import kotlin.random.Random
 
 class BoxfishEntity(entityType: EntityType<out BoxfishEntity>, world: World) :
     HybridAquaticFishEntity(entityType, world,
@@ -13,10 +29,22 @@ class BoxfishEntity(entityType: EntityType<out BoxfishEntity>, world: World) :
         listOf(
             HybridAquaticEntityTags.MEDIUM_PREY,
             HybridAquaticEntityTags.LARGE_PREY,
-            HybridAquaticEntityTags.SHARK)) {
+            HybridAquaticEntityTags.SHARK)),
+    VariantHolder<BoxfishEntity.Companion.Type> {
 
     override fun getLimitPerChunk(): Int {
         return 1
+    }
+
+    override fun initialize(
+        world: ServerWorldAccess,
+        difficulty: LocalDifficulty,
+        spawnReason: SpawnReason,
+        entityData: EntityData?,
+        entityNbt: NbtCompound?
+    ): EntityData? {
+        variant = Type.entries.random(Random)
+        return super.initialize(world, difficulty, spawnReason, entityData, entityNbt)
     }
 
     companion object {
@@ -28,5 +56,57 @@ class BoxfishEntity(entityType: EntityType<out BoxfishEntity>, world: World) :
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 0.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 4.0)
         }
+
+        enum class Type(val id: Int, private val key: String) : StringIdentifiable {
+            LONGHORN_COWFISH(0, "whitespotted"),
+            WHITESPOTTED(1, "whitespotted");
+
+            override fun asString(): String {
+                return this.key
+            }
+
+            companion object {
+                val CODEC: StringIdentifiable.Codec<Type> = StringIdentifiable.createCodec { entries.toTypedArray() }
+                private val BY_ID: IntFunction<Type> = ValueLists.createIdToValueFunction(
+                    { obj: Type -> obj.id },
+                    entries.toTypedArray(),
+                    ValueLists.OutOfBoundsHandling.ZERO
+                )
+
+                fun byName(name: String?): Type {
+                    return CODEC.byId(name, LONGHORN_COWFISH) as Type
+                }
+
+                fun fromId(id: Int): Type {
+                    return BY_ID.apply(id) as Type
+                }
+            }
+        }
+
+        val TYPE: TrackedData<Int> =
+            DataTracker.registerData(BoxfishEntity::class.java, TrackedDataHandlerRegistry.INTEGER)
+    }
+
+    override fun initDataTracker() {
+        dataTracker.startTracking(TYPE, 0)
+        super.initDataTracker()
+    }
+
+    override fun writeCustomDataToNbt(nbt: NbtCompound) {
+        nbt.putString("Type", this.variant.asString())
+        super.writeCustomDataToNbt(nbt)
+    }
+
+    override fun readCustomDataFromNbt(nbt: NbtCompound) {
+        this.variant = Type.byName(nbt.getString("Type"))
+        super.readCustomDataFromNbt(nbt)
+    }
+
+    override fun getVariant(): Type {
+        return Type.fromId((dataTracker.get(TYPE) as Int))
+    }
+
+    override fun setVariant(type: Type) {
+        dataTracker.set(TYPE, type.id)
     }
 }
