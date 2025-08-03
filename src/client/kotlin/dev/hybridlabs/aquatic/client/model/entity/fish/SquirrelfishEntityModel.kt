b46d570971888ecf0f5f2eb85c676f85a579df3d
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-03 16:59:05 +0300

    Squirrelfish improvements

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/SquirrelfishEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/SquirrelfishEntityModel.kt
@@ -1,5 +1,11 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.SquirrelfishEntity
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.util.Identifier
 
-class SquirrelfishEntityModel : HybridAquaticFishEntityModel<SquirrelfishEntity>("squirrelfish")
+class SquirrelfishEntityModel : HybridAquaticFishEntityModel<SquirrelfishEntity>("squirrelfish") {
+    override fun getRenderType(animatable: SquirrelfishEntity, texture: Identifier): RenderLayer {
+        return RenderLayer.getEntityTranslucent(texture)
+    }
+}
