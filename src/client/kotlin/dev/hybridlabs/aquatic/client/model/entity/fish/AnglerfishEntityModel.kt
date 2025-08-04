b1d2fee3af007cc74077d4aab25af4e6fc7b2c40
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-04 03:18:38 +0300

    Anglerfish visual update

--- src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/AnglerfishEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/AnglerfishEntityModel.kt
@@ -1,5 +1,11 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.AnglerfishEntity
+import net.minecraft.client.render.RenderLayer
+import net.minecraft.util.Identifier
 
-class AnglerfishEntityModel : HybridAquaticFishEntityModel<AnglerfishEntity>("anglerfish")
+class AnglerfishEntityModel : HybridAquaticFishEntityModel<AnglerfishEntity>("anglerfish") {
+    override fun getRenderType(animatable: AnglerfishEntity, texture: Identifier): RenderLayer {
+        return RenderLayer.getEntityTranslucent(texture)
+    }
+}
