4aececdc8726ff5cd34ca76238543e2e78022530
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-07 17:43:37 +0300

    African Butterflyfish adjustment

--- src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/AfricanButterflyEntity.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/AfricanButterflyEntity.kt
@@ -66,8 +66,8 @@ class AfricanButterflyEntity(entityType: EntityType<out AfricanButterflyEntity>,
     companion object {
         fun createMobAttributes(): DefaultAttributeContainer.Builder {
             return createLivingAttributes()
-                .add(EntityAttributes.GENERIC_MAX_HEALTH, 3.0)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.6)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 2.0)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.5)
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1.0)
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 0.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 4.0)
