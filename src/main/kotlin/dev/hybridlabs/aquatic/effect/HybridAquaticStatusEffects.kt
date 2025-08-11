bf3d5b64d5c50e51ae3c8a82e8b5779b4c939d34
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-11 07:18:54 +0300

    Effects mostly ported now

--- src/main/kotlin/dev/hybridlabs/aquatic/effect/HybridAquaticStatusEffects.kt
+++ src/main/kotlin/dev/hybridlabs/aquatic/effect/HybridAquaticStatusEffects.kt
@@ -4,6 +4,7 @@ import dev.hybridlabs.aquatic.HybridAquatic
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.util.Identifier
 
 object HybridAquaticStatusEffects {
@@ -14,7 +15,11 @@ object HybridAquaticStatusEffects {
     val SPININESS = register("spininess", SpininessStatusEffect())
     val CORROSION = register("corrosion", CorrosionStatusEffect())
 
-    private fun register(id: String, effect: StatusEffect): StatusEffect {
-        return Registry.register(Registries.STATUS_EFFECT, Identifier(HybridAquatic.MOD_ID, id), effect)
+    private fun register(id: String, effect: StatusEffect): RegistryEntry<StatusEffect> {
+        return Registry.registerReference(
+            Registries.STATUS_EFFECT,
+            Identifier.of(HybridAquatic.MOD_ID, id),
+            effect
+        )
     }
 }
