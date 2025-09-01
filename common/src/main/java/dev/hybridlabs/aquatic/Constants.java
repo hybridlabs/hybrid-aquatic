528dc32b942a251ef99b1afcbccb6d4988e1e220
Kevin Murphy <kevin@eorbit.net>
2025-09-01 05:51:37 -0700

    Fix mod_id

--- common/src/main/java/dev/hybridlabs/aquatic/Constants.java
+++ common/src/main/java/dev/hybridlabs/aquatic/Constants.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 
 public class Constants {
 
-    public static final String MOD_ID = "examplemod";
-    public static final String FORGE_MOD_ID = "examplemod";
-    public static final String MOD_NAME = "Example MOd";
+    public static final String MOD_ID = "hybrid-aquatic";
+    public static final String FORGE_MOD_ID = "hybrid-aquatic";
+    public static final String MOD_NAME = "Hybrid Aquatic";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
--- common/src/main/kotlin/dev/hybridlabs/aquatic/Example.kt
+++
@@ -1,4 +0,0 @@
-package dev.hybridlabs.aquatic
-
-class Example {
-}
