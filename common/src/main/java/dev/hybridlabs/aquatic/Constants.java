da268e14d757be75e74846fd331a1eb283ee914e
Kevin Murphy <kevin@eorbit.net>
2025-09-07 06:15:55 -0700

    Fix internal Forge mod id

--- common/src/main/java/dev/hybridlabs/aquatic/Constants.java
+++ common/src/main/java/dev/hybridlabs/aquatic/Constants.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public class Constants {
 
     public static final String MOD_ID = "hybrid-aquatic";
-    public static final String FORGE_MOD_ID = "hybrid-aquatic";
+    public static final String FORGE_MOD_ID = "hybrid_aquatic";
     public static final String MOD_NAME = "Hybrid Aquatic";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
