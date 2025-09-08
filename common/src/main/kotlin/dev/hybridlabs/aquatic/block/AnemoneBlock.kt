e532529d70af641c368c15a6812122dd77878d05
Kevin Murphy <kevin@eorbit.net>
2025-09-08 12:25:21 -0700

    Call the method in the superclass, dummy.

--- common/src/main/kotlin/dev/hybridlabs/aquatic/block/AnemoneBlock.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/block/AnemoneBlock.kt
@@ -13,12 +13,7 @@ import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.GameRules
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.LevelAccessor
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.Blocks
-import net.minecraft.world.level.block.BushBlock
-import net.minecraft.world.level.block.EntityBlock
-import net.minecraft.world.level.block.RenderShape
-import net.minecraft.world.level.block.SimpleWaterloggedBlock
+import net.minecraft.world.level.block.*
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.entity.BlockEntityType
@@ -101,7 +96,7 @@ class AnemoneBlock(settings: Properties) : BushBlock(settings), EntityBlock, Sim
         state: BlockState,
         type: BlockEntityType<T>
     ): BlockEntityTicker<T>? {
-        return getTicker(level, state, type)
+        return super.getTicker(level, state, type)
     }
 
     override fun getCollisionShape(
