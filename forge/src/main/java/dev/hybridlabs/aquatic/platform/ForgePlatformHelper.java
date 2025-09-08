91a866bb4099a77856de8a2bfd1a5be265cdaaaa
Kevin Murphy <kevin@eorbit.net>
2025-09-07 23:20:50 -0700

    Clean up some unused stuff in the platform helper.

--- forge/src/main/java/dev/hybridlabs/aquatic/platform/ForgePlatformHelper.java
+++ forge/src/main/java/dev/hybridlabs/aquatic/platform/ForgePlatformHelper.java
@@ -154,9 +154,4 @@ public class ForgePlatformHelper implements PlatformHelper {
         return MobCategory.byName(name);
     }
 
-    @Override
-    public String getSkullRendererTarget() {
-        return "";
-    }
-
 }
