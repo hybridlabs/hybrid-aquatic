841843284825aff0279afd31c1d9105535bf755f
Kevin Murphy <kevin@eorbit.net>
2025-09-12 22:39:59 -0700

    Fixed diving helmet overlay on Forge.

--- forge/src/main/java/dev/hybridlabs/aquatic/mixin/client/InGameHudMixin.java
+++ forge/src/main/java/dev/hybridlabs/aquatic/mixin/client/InGameHudMixin.java
@@ -4,18 +4,18 @@ import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.hybridlabs.aquatic.item.HybridAquaticItems;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.gui.overlay.ForgeGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
+@Mixin(ForgeGui.class)
 public class InGameHudMixin {
     @Unique
     private static final ResourceLocation DIVING_HELMET_OVERLAY = new ResourceLocation("hybrid-aquatic",
@@ -28,7 +28,7 @@ public class InGameHudMixin {
 
         if (player != null && client.options.getCameraType().isFirstPerson()) {
             ItemStack helmet = player.getInventory().getArmor(3);
-            if (helmet.getItem() == HybridAquaticItems.INSTANCE.getDIVING_HELMET()) {
+            if (helmet.getItem() == HybridAquaticItems.INSTANCE.getDIVING_HELMET().get()) {
                 renderDivingHelmetOverlay(context);
             }
         }
