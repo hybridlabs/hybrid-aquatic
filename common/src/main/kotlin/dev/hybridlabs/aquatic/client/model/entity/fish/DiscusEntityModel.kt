c3ba8ff3773fa60266177d40ca930a6e22384110
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-15 15:12:12 +0300

    Discus remodel and reanimation

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/DiscusEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/DiscusEntityModel.kt
@@ -1,5 +1,11 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.DiscusEntity
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.resources.ResourceLocation
 
-class DiscusEntityModel : HybridAquaticFishEntityModel<DiscusEntity>("discus")
+class DiscusEntityModel : HybridAquaticFishEntityModel<DiscusEntity>("discus") {
+    override fun getRenderType(animatable: DiscusEntity, texture: ResourceLocation): RenderType {
+        return RenderType.entityTranslucent(texture)
+    }
+}
