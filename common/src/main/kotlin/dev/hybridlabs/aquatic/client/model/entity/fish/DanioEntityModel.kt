ae14a6262e83914b12b411a0e1dffe8f6c23ec3c
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-15 12:46:55 +0300

    Gave danio translucent fins

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/DanioEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/DanioEntityModel.kt
@@ -1,5 +1,11 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.DanioEntity
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.resources.ResourceLocation
 
-class DanioEntityModel : HybridAquaticFishEntityModel<DanioEntity>("danio")
+class DanioEntityModel : HybridAquaticFishEntityModel<DanioEntity>("danio") {
+    override fun getRenderType(animatable: DanioEntity, texture: ResourceLocation): RenderType {
+        return RenderType.entityTranslucent(texture)
+    }
+}
