19a6029274afa595767fc72cdacf973198bf7c12
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-24 00:08:33 +0300

    Sea Otters

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/mammal/OtterEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/mammal/OtterEntityModel.kt
@@ -1,5 +1,37 @@
 package dev.hybridlabs.aquatic.client.model.entity.mammal
 
 import dev.hybridlabs.aquatic.entity.mammal.OtterEntity
+import net.minecraft.resources.ResourceLocation
 
-class OtterEntityModel : HybridAquaticMammalEntityModel<OtterEntity>("otter")
+class OtterEntityModel : HybridAquaticMammalEntityModel<OtterEntity>("otter") {
+
+    private val RIVER_OTTER_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/mammal/otter/river_otter.png")
+    private val SEA_OTTER_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/mammal/otter/sea_otter.png")
+
+    private val RIVER_OTTER_MODEL = ResourceLocation("hybrid-aquatic", "geo/mammal/otter/river_otter.geo.json")
+    private val SEA_OTTER_MODEL = ResourceLocation("hybrid-aquatic", "geo/mammal/otter/sea_otter.geo.json")
+
+    private val RIVER_OTTER_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/river_otter.animation.json")
+    private val SEA_OTTER_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/sea_otter.animation.json")
+
+    override fun getTextureResource(animatable: OtterEntity): ResourceLocation {
+        return when (animatable.variant) {
+            OtterEntity.Companion.Type.RIVER -> RIVER_OTTER_TEXTURE
+            OtterEntity.Companion.Type.SEA -> SEA_OTTER_TEXTURE
+        }
+    }
+
+    override fun getModelResource(animatable: OtterEntity): ResourceLocation {
+        return when (animatable.variant) {
+            OtterEntity.Companion.Type.RIVER -> RIVER_OTTER_MODEL
+            OtterEntity.Companion.Type.SEA -> SEA_OTTER_MODEL
+        }
+    }
+
+    override fun getAnimationResource(animatable: OtterEntity): ResourceLocation {
+        return when (animatable.variant) {
+            OtterEntity.Companion.Type.RIVER -> RIVER_OTTER_ANIMATION
+            OtterEntity.Companion.Type.SEA -> SEA_OTTER_ANIMATION
+        }
+    }
+}
--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/mammal/OtterEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/mammal/OtterEntity.kt
@@ -1,14 +1,23 @@
 package dev.hybridlabs.aquatic.entity.mammal
 
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
+import net.minecraft.core.Holder
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.syncher.EntityDataAccessor
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.server.level.ServerLevel
-import net.minecraft.world.entity.AgeableMob
-import net.minecraft.world.entity.EntityDimensions
-import net.minecraft.world.entity.EntityType
-import net.minecraft.world.entity.Pose
+import net.minecraft.tags.BiomeTags
+import net.minecraft.util.ByIdMap
+import net.minecraft.util.StringRepresentable
+import net.minecraft.world.DifficultyInstance
+import net.minecraft.world.entity.*
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.ServerLevelAccessor
+import net.minecraft.world.level.biome.Biome
+import java.util.function.IntFunction
 
 class OtterEntity(entityType: EntityType<out OtterEntity>, world: Level) :
     HybridAquaticMammalEntity(entityType, world,
@@ -16,7 +25,22 @@ class OtterEntity(entityType: EntityType<out OtterEntity>, world: Level) :
             HybridAquaticEntityTags.CRUSTACEAN,
             HybridAquaticEntityTags.SMALL_PREY),
         listOf(
-            HybridAquaticEntityTags.NONE)) {
+            HybridAquaticEntityTags.NONE)),
+    VariantHolder<OtterEntity.Companion.Type> {
+
+    override fun finalizeSpawn(
+        world: ServerLevelAccessor,
+        difficulty: DifficultyInstance,
+        spawnReason: MobSpawnType,
+        entityData: SpawnGroupData?,
+        entityNbt: CompoundTag?
+    ): SpawnGroupData? {
+        val biome = world.getBiome(this.blockPosition())
+        val selectedType = Type.fromBiome(biome)
+        this.variant = selectedType
+        return super.finalizeSpawn(world, difficulty, spawnReason, entityData, entityNbt)
+    }
+
 
     override fun getBreedOffspring(p0: ServerLevel, p1: AgeableMob): AgeableMob? {
         return null
@@ -39,5 +63,68 @@ class OtterEntity(entityType: EntityType<out OtterEntity>, world: Level) :
                 .add(Attributes.ATTACK_KNOCKBACK, 1.0)
                 .add(Attributes.FOLLOW_RANGE, 32.0)
         }
+
+        val TYPE: EntityDataAccessor<Int> =
+            SynchedEntityData.defineId(OtterEntity::class.java, EntityDataSerializers.INT)
+
+        enum class Type(val id: Int, private val key: String) : StringRepresentable {
+            RIVER(0, "river"),
+            SEA(1, "sea");
+
+            override fun getSerializedName(): String {
+                return this.key
+            }
+
+            companion object {
+                val CODEC: StringRepresentable.EnumCodec<Type> = StringRepresentable.fromEnum { entries.toTypedArray() }
+                private val BY_ID: IntFunction<Type> = ByIdMap.continuous(
+                    { obj: Type -> obj.id },
+                    entries.toTypedArray(),
+                    ByIdMap.OutOfBoundsStrategy.ZERO
+                )
+
+                fun byName(name: String?): Type {
+                    return CODEC.byName(name, RIVER) as Type
+                }
+
+                fun fromId(id: Int): Type {
+                    return BY_ID.apply(id) as Type
+                }
+
+                fun fromBiome(biome: Holder<Biome>): Type {
+                    return when {
+                        biome.`is`(BiomeTags.IS_BEACH) -> {
+                            SEA
+                        }
+                        else -> {
+                            RIVER
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun defineSynchedData() {
+        entityData.define(TYPE, 0)
+        super.defineSynchedData()
+    }
+
+    override fun addAdditionalSaveData(nbt: CompoundTag) {
+        nbt.putString("Type", this.variant.toString())
+        super.addAdditionalSaveData(nbt)
+    }
+
+    override fun readAdditionalSaveData(nbt: CompoundTag) {
+        this.variant = Type.byName(nbt.getString("Type"))
+        super.readAdditionalSaveData(nbt)
+    }
+
+    override fun getVariant(): Type {
+        return Type.fromId((entityData.get(TYPE) as Int))
+    }
+
+    override fun setVariant(type: Type) {
+        entityData.set(TYPE, type.id)
     }
 }
