b690ef58329eed090d5e62cbac10f63319501c0d
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-23 13:43:16 +0300

    Translucency

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/PupfishEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/PupfishEntityModel.kt
@@ -1,5 +1,11 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.PupfishEntity
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.resources.ResourceLocation
 
-class PupfishEntityModel : HybridAquaticFishEntityModel<PupfishEntity>("pupfish")
+class PupfishEntityModel : HybridAquaticFishEntityModel<PupfishEntity>("pupfish") {
+    override fun getRenderType(animatable: PupfishEntity, texture: ResourceLocation): RenderType {
+        return RenderType.entityTranslucent(texture)
+    }
+}
