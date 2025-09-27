e35dac6c37b5d3538669deaff68782b658a538f4
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-27 18:30:48 +0300

    Fixed spininess and thalassophobia

--- common/src/main/kotlin/dev/hybridlabs/aquatic/effect/SpininessMobEffect.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/effect/SpininessMobEffect.kt
@@ -1,6 +1,7 @@
 package dev.hybridlabs.aquatic.effect
 
 import net.minecraft.tags.DamageTypeTags
+import net.minecraft.util.RandomSource
 import net.minecraft.world.damagesource.DamageTypes
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectCategory
@@ -9,17 +10,37 @@ import net.minecraft.world.entity.LivingEntity
 class SpininessMobEffect : MobEffect(MobEffectCategory.BENEFICIAL, 0x695672) {
 
     override fun isDurationEffectTick(duration: Int, amplifier: Int): Boolean {
-        return duration % 40 == 0
+        return true
     }
 
     override fun applyEffectTick(entity: LivingEntity, amplifier: Int) {
-        entity.lastDamageSource?.let { damageSource ->
-            val attacker = damageSource.directEntity
-            if (attacker is LivingEntity && !attacker.isDeadOrDying) {
-                if (!damageSource.`is`(DamageTypeTags.AVOIDS_GUARDIAN_THORNS) && !damageSource.`is`(DamageTypes.THORNS)) {
-                    attacker.hurt(entity.damageSources().thorns(entity), 2.0f)
-                }
-            }
+        val level = amplifier + 1
+        val damageSource = entity.lastDamageSource ?: return
+        val attacker = damageSource.directEntity as? LivingEntity ?: return
+
+        if (attacker.isDeadOrDying) return
+        if (damageSource.`is`(DamageTypeTags.AVOIDS_GUARDIAN_THORNS) || damageSource.`is`(DamageTypes.THORNS)) return
+
+        if (shouldHit(level, entity.random)) {
+            attacker.hurt(entity.damageSources().thorns(entity), getDamage(level))
+        }
+    }
+
+    private fun shouldHit(level: Int, random: RandomSource): Boolean {
+        return if (level <= 0) {
+            false
+        } else {
+            random.nextFloat() < 0.15f * level.toFloat()
+        }
+    }
+
+    private fun getDamage(level: Int): Float {
+        return when {
+            level <= 0 -> 0f
+            level <= 3 -> 1f
+            level <= 6 -> 2f
+            level <= 9 -> 3f
+            else -> (level - 6).toFloat()
         }
     }
 }
