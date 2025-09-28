de5d64da25999ec55b8dcb13f646b795b9dd9eab
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-28 15:40:13 +0300

    Squirrelfish shoaling

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/SquirrelfishEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/SquirrelfishEntityModel.kt
@@ -1,11 +1,58 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
+import dev.hybridlabs.aquatic.CommonClass
 import dev.hybridlabs.aquatic.entity.fish.SquirrelfishEntity
+import net.minecraft.client.Minecraft
+import net.minecraft.client.model.geom.PartNames
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.util.Mth
+import software.bernie.geckolib.core.animation.AnimationState
 
 class SquirrelfishEntityModel : HybridAquaticFishEntityModel<SquirrelfishEntity>("squirrelfish") {
     override fun getRenderType(animatable: SquirrelfishEntity, texture: ResourceLocation): RenderType {
         return RenderType.entityTranslucent(texture)
+    }
+
+    override fun getModelResource(animatable: SquirrelfishEntity): ResourceLocation {
+        val fish = animatable.getFishCount()
+
+        val texturePath = when (fish) {
+            1 -> "geo/fish/squirrelfish/squirrelfish.geo.json"
+            2 -> "geo/fish/squirrelfish/squirrelfish_two.geo.json"
+            3 -> "geo/fish/squirrelfish/squirrelfish_three.geo.json"
+            else -> "geo/fish/squirrelfish/squirrelfish.geo.json"
+        }
+        return CommonClass.locate(texturePath)
+    }
+
+    override fun getAnimationResource(animatable: SquirrelfishEntity): ResourceLocation {
+        val fish = animatable.getFishCount()
+
+        val texturePath = when (fish) {
+            1 -> "animations/squirrelfish.animation.json"
+            2 -> "animations/squirrelfish_two.animation.json"
+            3 -> "animations/squirrelfish_three.animation.json"
+            else -> "animations/squirrelfish.animation.json"
+        }
+        return CommonClass.locate(texturePath)
+    }
+
+    override fun setCustomAnimations(
+        animatable: SquirrelfishEntity,
+        instanceId: Long,
+        animationState: AnimationState<SquirrelfishEntity>
+    ) {
+        super.setCustomAnimations(animatable, instanceId, animationState)
+        val deltaTime: Float = Minecraft.getInstance().deltaFrameTime
+
+        val body = animationProcessor.getBone(PartNames.BODY)
+        val body2 = animationProcessor.getBone("body2")
+        val body3 = animationProcessor.getBone("body3")
+
+        val xRot = Mth.clamp(Mth.lerp(deltaTime, animatable.xRot, animatable.xRotO), -45f, 45f)
+        body.rotX = xRot * -Mth.DEG_TO_RAD
+        body2?.rotX = xRot * -Mth.DEG_TO_RAD
+        body3?.rotX = xRot * -Mth.DEG_TO_RAD
     }
 }
--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/SquirrelfishEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/SquirrelfishEntity.kt
@@ -1,15 +1,22 @@
 package dev.hybridlabs.aquatic.entity.fish
 
+import dev.hybridlabs.aquatic.item.HybridAquaticItems
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
 import net.minecraft.core.BlockPos
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.syncher.EntityDataAccessor
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.util.RandomSource
-import net.minecraft.world.entity.EntityType
-import net.minecraft.world.entity.MobSpawnType
+import net.minecraft.world.DifficultyInstance
+import net.minecraft.world.damagesource.DamageSource
+import net.minecraft.world.entity.*
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.ServerLevelAccessor
 
+@Suppress("DEPRECATION", "UNUSED_PARAMETER")
 class SquirrelfishEntity(entityType: EntityType<out SquirrelfishEntity>, world: Level) :
     HybridAquaticSchoolingFishEntity(
         entityType, world,
@@ -25,7 +32,104 @@ class SquirrelfishEntity(entityType: EntityType<out SquirrelfishEntity>, world: 
         return 4
     }
 
+    override fun defineSynchedData() {
+        super.defineSynchedData()
+        entityData.define(FISHCOUNT, ONE_FISH)
+    }
+
+    override fun onSyncedDataUpdated(key: EntityDataAccessor<*>) {
+        super.onSyncedDataUpdated(key)
+        if (key == FISHCOUNT) {
+            refreshDimensions()
+        }
+    }
+
+    fun getFishCount(): Int {
+        return entityData.get(FISHCOUNT)
+    }
+
+    private fun setFishCount(state: Int) {
+        entityData.set(FISHCOUNT, state)
+        refreshDimensions()
+    }
+
+    override fun addAdditionalSaveData(nbt: CompoundTag) {
+        super.addAdditionalSaveData(nbt)
+        nbt.putInt("FishCount", getFishCount())
+    }
+
+    override fun readAdditionalSaveData(nbt: CompoundTag) {
+        super.readAdditionalSaveData(nbt)
+        setFishCount(nbt.getInt("FishCount").coerceAtMost(THREE_FISH))
+    }
+
+    override fun tick() {
+        super.tick()
+
+        if (!level().isClientSide) {
+            when (health.toInt()) {
+                3 -> setFishCount(THREE_FISH)
+                2 -> setFishCount(TWO_FISH)
+                1 -> setFishCount(ONE_FISH)
+            }
+        }
+    }
+
+    override fun finalizeSpawn(
+        world: ServerLevelAccessor,
+        difficulty: DifficultyInstance,
+        spawnReason: MobSpawnType,
+        entityData: SpawnGroupData?,
+        entityNbt: CompoundTag?,
+    ): SpawnGroupData? {
+        val startingHealth = this.random.nextIntBetweenInclusive(1, 3)
+        this.health = startingHealth.toFloat()
+
+        when (startingHealth) {
+            3 -> setFishCount(THREE_FISH)
+            2 -> setFishCount(TWO_FISH)
+            1 -> setFishCount(ONE_FISH)
+        }
+
+        return super.finalizeSpawn(world, difficulty, spawnReason, entityData, entityNbt)
+    }
+
+    override fun getDimensions(pose: Pose): EntityDimensions {
+        val scale = when (getFishCount()) {
+            ONE_FISH -> 1.0f
+            TWO_FISH -> 2.0f
+            THREE_FISH -> 3.0f
+            else -> 1.0f
+        }
+
+        return super.getDimensions(pose).scale(scale)
+    }
+
+    override fun getStandingEyeHeight(pose: Pose, dimensions: EntityDimensions): Float {
+        return dimensions.height * 0.5f
+    }
+
+    override fun hurt(source: DamageSource, amount: Float): Boolean {
+        val oldHealth = this.health.toInt()
+        val result = super.hurt(source, amount)
+        val newHealth = this.health.toInt()
+
+        if (result && !level().isClientSide) {
+            if (oldHealth - newHealth == 1 && newHealth > 0) {
+                spawnAtLocation(HybridAquaticItems.SQUIRRELFISH.get())
+            }
+        }
+        return result
+    }
+
     companion object {
+        private val FISHCOUNT: EntityDataAccessor<Int> =
+            SynchedEntityData.defineId(SquirrelfishEntity::class.java, EntityDataSerializers.INT)
+
+        const val ONE_FISH = 1
+        const val TWO_FISH = 2
+        const val THREE_FISH = 3
+
         fun createMobAttributes(): AttributeSupplier.Builder {
             return createLivingAttributes()
                 .add(Attributes.MAX_HEALTH, 3.0)
