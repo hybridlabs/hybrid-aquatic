657dbc0367b9c2256dc6702a8c9d739bdab3379a
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-28 18:01:04 +0300

    Damselfish shoals

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/DamselfishEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/DamselfishEntityModel.kt
@@ -1,6 +1,55 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
+import dev.hybridlabs.aquatic.CommonClass
 import dev.hybridlabs.aquatic.entity.fish.DamselfishEntity
+import net.minecraft.client.Minecraft
+import net.minecraft.client.model.geom.PartNames
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.util.Mth
+import software.bernie.geckolib.core.animation.AnimationState
 
-class DamselfishEntityModel : HybridAquaticFishEntityModel<DamselfishEntity>("damselfish")
+class DamselfishEntityModel : HybridAquaticFishEntityModel<DamselfishEntity>("damselfish") {
+
+    override fun getModelResource(animatable: DamselfishEntity): ResourceLocation {
+        val fish = animatable.getFishCount()
+
+        val texturePath = when (fish) {
+            1 -> "geo/fish/damselfish/damselfish.geo.json"
+            2 -> "geo/fish/damselfish/damselfish_two.geo.json"
+            3 -> "geo/fish/damselfish/damselfish_three.geo.json"
+            else -> "geo/fish/damselfish/damselfish.geo.json"
+        }
+        return CommonClass.locate(texturePath)
+    }
+
+    override fun getAnimationResource(animatable: DamselfishEntity): ResourceLocation {
+        val fish = animatable.getFishCount()
+
+        val texturePath = when (fish) {
+            1 -> "animations/damselfish.animation.json"
+            2 -> "animations/damselfish_two.animation.json"
+            3 -> "animations/damselfish_three.animation.json"
+            else -> "animations/damselfish.animation.json"
+        }
+        return CommonClass.locate(texturePath)
+    }
+
+    override fun setCustomAnimations(
+        animatable: DamselfishEntity,
+        instanceId: Long,
+        animationState: AnimationState<DamselfishEntity>
+    ) {
+        super.setCustomAnimations(animatable, instanceId, animationState)
+        val deltaTime: Float = Minecraft.getInstance().deltaFrameTime
+
+        val body = animationProcessor.getBone(PartNames.BODY)
+        val body2 = animationProcessor.getBone("body2")
+        val body3 = animationProcessor.getBone("body3")
+
+        val xRot = Mth.clamp(Mth.lerp(deltaTime, animatable.xRot, animatable.xRotO), -45f, 45f)
+        body.rotX = xRot * -Mth.DEG_TO_RAD
+        body2?.rotX = xRot * -Mth.DEG_TO_RAD
+        body3?.rotX = xRot * -Mth.DEG_TO_RAD
+    }
+}
 
