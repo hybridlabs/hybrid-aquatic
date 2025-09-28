345c2edaab49f3bc319a84f0f5ac93b09f065925
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-28 04:28:42 +0300

    Truly overcomplicated how mackerels work

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/MackerelEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/MackerelEntityModel.kt
@@ -1,5 +1,54 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
+import dev.hybridlabs.aquatic.CommonClass
 import dev.hybridlabs.aquatic.entity.fish.MackerelEntity
+import net.minecraft.client.Minecraft
+import net.minecraft.client.model.geom.PartNames
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.util.Mth
+import software.bernie.geckolib.core.animation.AnimationState
 
-class MackerelEntityModel : HybridAquaticFishEntityModel<MackerelEntity>("mackerel")
+class MackerelEntityModel : HybridAquaticFishEntityModel<MackerelEntity>("mackerel") {
+
+    override fun getModelResource(animatable: MackerelEntity): ResourceLocation {
+        val mackerels = animatable.getMackerels()
+
+        val texturePath = when (mackerels) {
+            1 -> "geo/fish/mackerel/mackerel.geo.json"
+            2 -> "geo/fish/mackerel/mackerel_two.geo.json"
+            3 -> "geo/fish/mackerel/mackerel_three.geo.json"
+            else -> "geo/fish/mackerel/mackerel.geo.json"
+        }
+        return CommonClass.locate(texturePath)
+    }
+
+    override fun getAnimationResource(animatable: MackerelEntity): ResourceLocation {
+        val mackerels = animatable.getMackerels()
+
+        val texturePath = when (mackerels) {
+            1 -> "animations/mackerel.animation.json"
+            2 -> "animations/mackerel_two.animation.json"
+            3 -> "animations/mackerel_three.animation.json"
+            else -> "animations/mackerel.animation.json"
+        }
+        return CommonClass.locate(texturePath)
+    }
+
+    override fun setCustomAnimations(
+        animatable: MackerelEntity,
+        instanceId: Long,
+        animationState: AnimationState<MackerelEntity>
+    ) {
+        super.setCustomAnimations(animatable, instanceId, animationState)
+        val deltaTime: Float = Minecraft.getInstance().deltaFrameTime
+
+        val body = animationProcessor.getBone(PartNames.BODY)
+        val body2 = animationProcessor.getBone("body2")
+        val body3 = animationProcessor.getBone("body3")
+
+        val xRot = Mth.clamp(Mth.lerp(deltaTime, animatable.xRot, animatable.xRotO), -45f, 45f)
+        body.rotX = xRot * -Mth.DEG_TO_RAD
+        body2?.rotX = xRot * -Mth.DEG_TO_RAD
+        body3?.rotX = xRot * -Mth.DEG_TO_RAD
+    }
+}
--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/HybridAquaticEntityTypes.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/HybridAquaticEntityTypes.kt
@@ -219,7 +219,7 @@ object HybridAquaticEntityTypes {
     val MACKEREL = registerFish(
         "mackerel",
         ::MackerelEntity,
-        EntityDimensions.fixed(0.4f, 0.25f),
+        EntityDimensions.scalable(0.3f, 0.3f),
         MackerelEntity::createMobAttributes
     )
 
