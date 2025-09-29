c9fcd35f9d94c74520b533e67b38cada5cc6546d
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-09-30 00:01:03 +0300

    Fixed a thing

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/SurgeonfishEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/SurgeonfishEntity.kt
@@ -1,8 +1,6 @@
 package dev.hybridlabs.aquatic.entity.fish
 
 import dev.hybridlabs.aquatic.entity.HybridAquaticEntityTypes
-import dev.hybridlabs.aquatic.entity.ai.goal.boids.BoidGoal
-import dev.hybridlabs.aquatic.entity.ai.goal.boids.StayInWaterGoal
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.syncher.EntityDataAccessor
@@ -11,7 +9,10 @@ import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.util.ByIdMap
 import net.minecraft.util.StringRepresentable
 import net.minecraft.world.DifficultyInstance
-import net.minecraft.world.entity.*
+import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.MobSpawnType
+import net.minecraft.world.entity.SpawnGroupData
+import net.minecraft.world.entity.VariantHolder
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.level.Level
@@ -31,26 +32,6 @@ class SurgeonfishEntity(entityType: EntityType<out SurgeonfishEntity>, world: Le
         )
     ),
     VariantHolder<SurgeonfishEntity.Companion.Type> {
-
-    override fun registerGoals() {
-        super.registerGoals()
-        goalSelector.addGoal(5, BoidGoal(this, 0.25f, 0.5f, 8 / 20f, 1 / 20f))
-        goalSelector.addGoal(3, StayInWaterGoal(this))
-    }
-
-    override fun canCollideWith(entity: Entity): Boolean {
-        if (entity is SurgeonfishEntity) {
-            return false
-        }
-        return super.canCollideWith(entity)
-    }
-
-    override fun doPush(entity: Entity) {
-        if (entity is SurgeonfishEntity) {
-            return
-        }
-        super.doPush(entity)
-    }
 
     override fun getMaxSpawnClusterSize(): Int {
         return 3
@@ -92,7 +73,7 @@ class SurgeonfishEntity(entityType: EntityType<out SurgeonfishEntity>, world: Le
         fun createMobAttributes(): AttributeSupplier.Builder {
             return createLivingAttributes()
                 .add(Attributes.MAX_HEALTH, 3.0)
-                .add(Attributes.MOVEMENT_SPEED, 0.3)
+                .add(Attributes.MOVEMENT_SPEED, 0.6)
                 .add(Attributes.ATTACK_DAMAGE, 1.0)
                 .add(Attributes.ATTACK_KNOCKBACK, 0.0)
                 .add(Attributes.FOLLOW_RANGE, 4.0)
