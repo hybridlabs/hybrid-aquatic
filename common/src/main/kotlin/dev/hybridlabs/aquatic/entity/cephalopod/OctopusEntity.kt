59a27898822c2bb9355d4e3b4423fd37786a8177
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-01 00:54:59 +0300

    Tentacular

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/cephalopod/OctopusEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/cephalopod/OctopusEntity.kt
@@ -1,10 +1,19 @@
 package dev.hybridlabs.aquatic.entity.cephalopod
 
+import com.mojang.serialization.Codec
+import dev.hybridlabs.aquatic.entity.feature.OverlayTextureFeature
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.syncher.EntityDataAccessor
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData
+import net.minecraft.util.ByIdMap
+import net.minecraft.util.StringRepresentable
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.level.Level
+import java.util.function.IntFunction
 
 class OctopusEntity(entityType: EntityType<out OctopusEntity>, world: Level) :
     HybridAquaticOctopusEntity(
@@ -14,7 +23,7 @@ class OctopusEntity(entityType: EntityType<out OctopusEntity>, world: Level) :
         HybridAquaticEntityTags.SHARK,
         false,
         true
-    ) {
+    ), OverlayTextureFeature {
 
     companion object {
         fun createMobAttributes(): AttributeSupplier.Builder {
@@ -25,5 +34,56 @@ class OctopusEntity(entityType: EntityType<out OctopusEntity>, world: Level) :
                 .add(Attributes.ATTACK_KNOCKBACK, 0.0)
                 .add(Attributes.FOLLOW_RANGE, 8.0)
         }
+
+        val OverlayTexture: EntityDataAccessor<Int> =
+            SynchedEntityData.defineId(OctopusEntity::class.java, EntityDataSerializers.INT)
+
+        enum class OverlayTextures(val id: Int, val key: String) : StringRepresentable {
+            TINT(0, "tint");
+
+            override fun getSerializedName(): String {
+                return this.key
+            }
+
+            companion object {
+                val CODEC: Codec<OverlayTextures> =
+                    StringRepresentable.fromEnum { OverlayTextures.entries.toTypedArray() }
+                val BY_ID: IntFunction<OverlayTextures> = ByIdMap.continuous(
+                    { overlayTex: OverlayTextures -> overlayTex.id },
+                    OverlayTextures.entries.toTypedArray(),
+                    ByIdMap.OutOfBoundsStrategy.WRAP
+                )
+
+                fun byId(id: Int): OverlayTextures {
+                    return BY_ID.apply(id)
+                }
+            }
+        }
+    }
+
+    private var overlayTexture
+        get() = OctopusEntity.Companion.OverlayTextures.byId(entityData.get(OverlayTexture))
+        set(value) {
+            entityData.set(OverlayTexture, value.id)
+        }
+
+    override fun getOverlayTextureName(): String {
+        return OctopusEntity.Companion.OverlayTextures.byId(entityData.get(OverlayTexture)).serializedName
+    }
+
+    override fun defineSynchedData() {
+        entityData.define(OverlayTexture, 0)
+        super.defineSynchedData()
+    }
+
+    override fun addAdditionalSaveData(nbt: CompoundTag) {
+        nbt.putInt("texture_overlay", this.overlayTexture.id)
+        super.addAdditionalSaveData(nbt)
+    }
+
+    override fun readAdditionalSaveData(nbt: CompoundTag) {
+        if (nbt.contains("texture_overlay")) this.overlayTexture =
+            OctopusEntity.Companion.OverlayTextures.byId(nbt.getInt("texture_overlay"))
+        super.readAdditionalSaveData(nbt)
     }
 }
