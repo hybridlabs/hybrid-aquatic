91d8cc17adcca5977f23a1b18a62393d6974d617
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-02 19:49:23 +0300

    Tetra asset update

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/TetraEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/TetraEntityModel.kt
@@ -1,5 +1,17 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.TetraEntity
+import net.minecraft.resources.ResourceLocation
 
-class TetraEntityModel : HybridAquaticFishEntityModel<TetraEntity>("tetra")
+class TetraEntityModel : HybridAquaticFishEntityModel<TetraEntity>("tetra") {
+
+    private val NEON_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/fish/tetra/neon_tetra.png")
+    private val CAVE_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/fish/tetra/cave_tetra.png")
+
+    override fun getTextureResource(animatable: TetraEntity): ResourceLocation {
+        return when (animatable.variant) {
+            TetraEntity.Companion.Type.NEON -> NEON_TEXTURE
+            TetraEntity.Companion.Type.CAVE -> CAVE_TEXTURE
+        }
+    }
+}
--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/TetraEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/TetraEntity.kt
@@ -2,12 +2,23 @@ package dev.hybridlabs.aquatic.entity.fish
 
 import dev.hybridlabs.aquatic.entity.ai.goal.boids.BoidGoal
 import dev.hybridlabs.aquatic.entity.ai.goal.boids.StayInWaterGoal
+import dev.hybridlabs.aquatic.tag.HybridAquaticBiomeTags
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
-import net.minecraft.world.entity.Entity
-import net.minecraft.world.entity.EntityType
+import net.minecraft.core.Holder
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.syncher.EntityDataAccessor
+import net.minecraft.network.syncher.EntityDataSerializers
+import net.minecraft.network.syncher.SynchedEntityData
+import net.minecraft.util.ByIdMap
+import net.minecraft.util.StringRepresentable
+import net.minecraft.world.DifficultyInstance
+import net.minecraft.world.entity.*
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.ServerLevelAccessor
+import net.minecraft.world.level.biome.Biome
+import java.util.function.IntFunction
 
 class TetraEntity(entityType: EntityType<out TetraEntity>, world: Level) :
     HybridAquaticSchoolingFishEntity(
@@ -20,7 +31,7 @@ class TetraEntity(entityType: EntityType<out TetraEntity>, world: Level) :
             HybridAquaticEntityTags.LARGE_PREY,
             HybridAquaticEntityTags.SHARK
         )
-    ) {
+    ), VariantHolder<TetraEntity.Companion.Type> {
 
     override fun registerGoals() {
         super.registerGoals()
@@ -46,6 +57,19 @@ class TetraEntity(entityType: EntityType<out TetraEntity>, world: Level) :
         return 3
     }
 
+    override fun finalizeSpawn(
+        world: ServerLevelAccessor,
+        difficulty: DifficultyInstance,
+        spawnReason: MobSpawnType,
+        entityData: SpawnGroupData?,
+        entityNbt: CompoundTag?
+    ): SpawnGroupData? {
+        val biome = world.getBiome(this.blockPosition())
+        val selectedType = TetraEntity.Companion.Type.fromBiome(biome)
+        this.variant = selectedType
+        return super.finalizeSpawn(world, difficulty, spawnReason, entityData, entityNbt)
+    }
+
     companion object {
         fun createMobAttributes(): AttributeSupplier.Builder {
             return createLivingAttributes()
@@ -55,5 +79,69 @@ class TetraEntity(entityType: EntityType<out TetraEntity>, world: Level) :
                 .add(Attributes.ATTACK_KNOCKBACK, 0.0)
                 .add(Attributes.FOLLOW_RANGE, 4.0)
         }
+
+        val TYPE: EntityDataAccessor<Int> =
+            SynchedEntityData.defineId(TetraEntity::class.java, EntityDataSerializers.INT)
+
+        enum class Type(val id: Int, private val key: String) : StringRepresentable {
+            NEON(0, "neon"),
+            CAVE(1, "cave");
+
+            override fun getSerializedName(): String {
+                return this.key
+            }
+
+            companion object {
+                val CODEC: StringRepresentable.EnumCodec<Type> = StringRepresentable.fromEnum { entries.toTypedArray() }
+                private val BY_ID: IntFunction<Type> = ByIdMap.continuous(
+                    { obj: Type -> obj.id },
+                    entries.toTypedArray(),
+                    ByIdMap.OutOfBoundsStrategy.ZERO
+                )
+
+                fun byName(name: String?): Type {
+                    return CODEC.byName(name, NEON) as Type
+                }
+
+                fun fromId(id: Int): Type {
+                    return BY_ID.apply(id) as Type
+                }
+
+                fun fromBiome(biome: Holder<Biome>): Type {
+                    return when {
+                        biome.`is`(HybridAquaticBiomeTags.CAVES) -> {
+                            CAVE
+                        }
+
+                        else -> {
+                            NEON
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun defineSynchedData() {
+        entityData.define(TYPE, 0)
+        super.defineSynchedData()
+    }
+
+    override fun addAdditionalSaveData(nbt: CompoundTag) {
+        nbt.putString("Type", this.variant.serializedName)
+        super.addAdditionalSaveData(nbt)
+    }
+
+    override fun readAdditionalSaveData(nbt: CompoundTag) {
+        this.variant = Type.byName(nbt.getString("Type"))
+        super.readAdditionalSaveData(nbt)
+    }
+
+    override fun getVariant(): Type {
+        return Type.fromId((entityData.get(TYPE) as Int))
+    }
+
+    override fun setVariant(type: Type) {
+        entityData.set(TYPE, type.id)
     }
 }
