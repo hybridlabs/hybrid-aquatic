b4c58ea4ffc8d2fc8fcefc8efefe007366304a99
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-02 14:14:32 +0300

    Ratfish asset update

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/RatfishEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/RatfishEntityModel.kt
@@ -1,6 +1,24 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.RatfishEntity
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.resources.ResourceLocation
+import kotlin.random.Random
 
-class RatfishEntityModel : HybridAquaticFishEntityModel<RatfishEntity>("ratfish")
+class RatfishEntityModel : HybridAquaticFishEntityModel<RatfishEntity>("ratfish") {
+    override fun getRenderType(animatable: RatfishEntity, texture: ResourceLocation): RenderType {
+        return RenderType.entityTranslucent(texture)
+    }
+
+    private val commonTextures = listOf(
+        ResourceLocation("hybrid-aquatic", "textures/entity/fish/ratfish/ratfish_brown.png"),
+        ResourceLocation("hybrid-aquatic", "textures/entity/fish/ratfish/ratfish_silver.png"),
+        )
+
+    override fun getTextureResource(animatable: RatfishEntity): ResourceLocation {
+        val seed = animatable.uuid.leastSignificantBits
+        val random = Random(seed)
+        return commonTextures[random.nextInt(commonTextures.size)]
+    }
+}
 
