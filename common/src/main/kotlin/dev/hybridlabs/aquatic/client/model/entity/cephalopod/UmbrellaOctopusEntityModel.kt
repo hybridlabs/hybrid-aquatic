eebba7ff70d63035b44d7a5c0d3f66b300036a61
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-02 13:09:34 +0300

    Umbrella octopus asset update

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/cephalopod/UmbrellaOctopusEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/cephalopod/UmbrellaOctopusEntityModel.kt
@@ -1,5 +1,23 @@
 package dev.hybridlabs.aquatic.client.model.entity.cephalopod
 
 import dev.hybridlabs.aquatic.entity.cephalopod.UmbrellaOctopusEntity
+import net.minecraft.resources.ResourceLocation
+import kotlin.random.Random
 
-class UmbrellaOctopusEntityModel : HybridAquaticOctopusEntityModel<UmbrellaOctopusEntity>("umbrella_octopus")
+class UmbrellaOctopusEntityModel : HybridAquaticOctopusEntityModel<UmbrellaOctopusEntity>("umbrella_octopus") {
+
+    private val commonTextures = listOf(
+        ResourceLocation("hybrid-aquatic", "textures/entity/cephalopod/umbrella_octopus/umbrella_octopus_yellow.png"),
+        ResourceLocation("hybrid-aquatic", "textures/entity/cephalopod/umbrella_octopus/umbrella_octopus_brown.png"),
+        ResourceLocation("hybrid-aquatic", "textures/entity/cephalopod/umbrella_octopus/umbrella_octopus_orange.png"),
+        ResourceLocation("hybrid-aquatic", "textures/entity/cephalopod/umbrella_octopus/umbrella_octopus_pink.png"),
+        ResourceLocation("hybrid-aquatic", "textures/entity/cephalopod/umbrella_octopus/umbrella_octopus_purple.png"),
+        ResourceLocation("hybrid-aquatic", "textures/entity/cephalopod/umbrella_octopus/umbrella_octopus_white.png"),
+    )
+
+    override fun getTextureResource(animatable: UmbrellaOctopusEntity): ResourceLocation {
+        val seed = animatable.uuid.leastSignificantBits
+        val random = Random(seed)
+        return commonTextures[random.nextInt(commonTextures.size)]
+    }
+}
