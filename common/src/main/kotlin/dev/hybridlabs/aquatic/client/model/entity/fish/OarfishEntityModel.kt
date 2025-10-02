08be4691d0acbc2c48b94b9baccb7a0049d1709a
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-02 23:30:42 +0300

    Tweaking oarfish

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/OarfishEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/OarfishEntityModel.kt
@@ -1,32 +1,11 @@
 package dev.hybridlabs.aquatic.client.model.entity.fish
 
 import dev.hybridlabs.aquatic.entity.fish.OarfishEntity
-import net.minecraft.client.Minecraft
-import net.minecraft.client.model.geom.PartNames
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.util.Mth
-import software.bernie.geckolib.core.animation.AnimationState
 
 class OarfishEntityModel : HybridAquaticFishEntityModel<OarfishEntity>("oarfish") {
     override fun getRenderType(animatable: OarfishEntity, texture: ResourceLocation): RenderType {
         return RenderType.entityTranslucent(texture)
-    }
-
-    override fun setCustomAnimations(
-        animatable: OarfishEntity,
-        instanceId: Long,
-        animationState: AnimationState<OarfishEntity>
-    ) {
-        if (!animatable.isFeeding()) return
-
-        super.setCustomAnimations(animatable, instanceId, animationState)
-
-        val deltaTime: Float = Minecraft.getInstance().deltaFrameTime
-
-        val body = animationProcessor.getBone(PartNames.BODY)
-
-        val xRot = Mth.clamp(Mth.lerp(deltaTime, animatable.xRot, animatable.xRotO), -45f, 45f)
-        body.rotX = xRot * -Mth.DEG_TO_RAD
     }
 }
