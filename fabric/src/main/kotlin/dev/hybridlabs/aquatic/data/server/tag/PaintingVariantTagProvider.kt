994aab554ebc9084a7b495be131af8d94da3d98c
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2025-10-02 18:51:59 +0300

    make datagen for minecraft/tags/painting_variant/placeable.json

---
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/server/tag/PaintingVariantTagProvider.kt
@@ -0,0 +1,31 @@
+package dev.hybridlabs.aquatic.data.server.tag
+
+import dev.hybridlabs.aquatic.painting.HybridAquaticPaintings
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
+import net.minecraft.core.HolderLookup
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceKey
+import net.minecraft.tags.PaintingVariantTags
+import net.minecraft.world.entity.decoration.PaintingVariant
+import java.util.concurrent.CompletableFuture
+
+class PaintingVariantTagProvider (output: FabricDataOutput, registriesFuture: CompletableFuture<HolderLookup.Provider>) :
+    FabricTagProvider<PaintingVariant>(output, Registries.PAINTING_VARIANT, registriesFuture) {
+    override fun addTags(arg: HolderLookup.Provider) {
+        setOf(
+            HybridAquaticPaintings.TEST_PAINTING1
+        ).forEach { painting ->
+            getOrCreateTagBuilder(PaintingVariantTags.PLACEABLE).add(painting)
+        }
+    }
+
+    override fun reverseLookup(element: PaintingVariant): ResourceKey<PaintingVariant> {
+        return BuiltInRegistries.PAINTING_VARIANT.getResourceKey(element).orElseThrow {
+            IllegalArgumentException(
+                "Painting Variant $element is not registered"
+            )
+        } as ResourceKey<PaintingVariant>
+    }
+}
