353e2a72c2f442e249b66b4f3bf80c44ed3bf2ad
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2025-10-02 18:25:55 +0300

    Proper paintings registry

--- common/src/main/kotlin/dev/hybridlabs/aquatic/painting/HybridAquaticPaintings.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/painting/HybridAquaticPaintings.kt
@@ -1,17 +1,20 @@
 package dev.hybridlabs.aquatic.painting
 
 import dev.hybridlabs.aquatic.CommonClass
+import dev.hybridlabs.aquatic.Constants.MOD_ID
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.decoration.PaintingVariant
 import java.util.function.Supplier
 
 object HybridAquaticPaintings {
     val TEST_PAINTING1 = registerPainting("test_painting1", 32, 32)
 
-    fun register(id: String, painting: Supplier<PaintingVariant>) {
-        CommonClass.PAINTINGS.register(id, painting)
+    fun register(id: String, painting: Supplier<PaintingVariant>): Supplier<PaintingVariant> {
+        return CommonClass.PAINTINGS.register(id, painting)
     }
 
-    fun registerPainting(id: String, width: Int, height: Int) {
+    fun registerPainting(id: String, width: Int, height: Int): ResourceLocation {
         register(id) { PaintingVariant(width, height) }
+        return ResourceLocation(MOD_ID, id)
     }
 }
