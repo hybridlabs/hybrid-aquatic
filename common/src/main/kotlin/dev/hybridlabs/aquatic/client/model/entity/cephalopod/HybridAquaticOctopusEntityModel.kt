59825aa0feb8a40583d464366add400277b864b3
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-04 03:48:32 +0300

    Revert "Octoprogress"
    This reverts commit ba50d7a9697bc5e4f72791fdd0c10faf8e206dee.

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/cephalopod/HybridAquaticOctopusEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/cephalopod/HybridAquaticOctopusEntityModel.kt
@@ -3,6 +3,7 @@ package dev.hybridlabs.aquatic.client.model.entity.cephalopod
 import dev.hybridlabs.aquatic.CommonClass
 import dev.hybridlabs.aquatic.entity.cephalopod.HybridAquaticOctopusEntity
 import net.minecraft.client.Minecraft
+import net.minecraft.client.model.geom.PartNames
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.util.Mth
 import software.bernie.geckolib.core.animation.AnimationState
@@ -35,7 +36,7 @@ abstract class HybridAquaticOctopusEntityModel<T : HybridAquaticOctopusEntity>(p
         super.setCustomAnimations(animatable, instanceId, animationState)
         val deltaTime: Float = Minecraft.getInstance().deltaFrameTime
 
-        val body = animationProcessor.getBone("octopus")
+        val body = animationProcessor.getBone(PartNames.BODY)
 
         if (!animatable.isSitting()) {
             val xRot = Mth.clamp(Mth.lerp(deltaTime, animatable.xRot, animatable.xRotO), -45f, 45f)
--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/cephalopod/HybridAquaticOctopusEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/cephalopod/HybridAquaticOctopusEntity.kt
@@ -63,7 +63,6 @@ open class HybridAquaticOctopusEntity(
                     } else {
                         this.deltaMovement = deltaMovement.subtract(0.0, 0.01, 0.0)
                         this.yHeadRot = 0F
-                        this.xRot = 0F
                     }
                 } else if (random.nextFloat() <= 0.001f) {
                     this.sittingTimer = random.nextInt(200, 650)
