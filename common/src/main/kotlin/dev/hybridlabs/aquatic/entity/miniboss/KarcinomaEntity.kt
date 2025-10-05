4b7ad329c73e488d5b3fb37c8b89d7e03d589335
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-05 17:34:15 +0300

    Karcinoma tweaks

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/miniboss/KarcinomaEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/miniboss/KarcinomaEntity.kt
@@ -4,13 +4,19 @@ import net.minecraft.sounds.SoundEvent
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.MoverType
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl
 import net.minecraft.world.entity.ai.control.SmoothSwimmingMoveControl
+import net.minecraft.world.entity.ai.navigation.PathNavigation
 import net.minecraft.world.entity.ai.navigation.WaterBoundPathNavigation
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.pathfinder.BlockPathTypes
+import net.minecraft.world.phys.Vec3
+import software.bernie.geckolib.constant.DefaultAnimations
+import software.bernie.geckolib.core.animation.AnimatableManager
+
 
 class KarcinomaEntity(entityType: EntityType<out HybridAquaticMinionEntity>, world: Level) :
     HybridAquaticMinionEntity(entityType, world) {
@@ -19,6 +25,28 @@ class KarcinomaEntity(entityType: EntityType<out HybridAquaticMinionEntity>, wor
         moveControl = SmoothSwimmingMoveControl(this, 85, 5, 1.0F, 0.1f, true)
         lookControl = SmoothSwimmingLookControl(this, 10)
         navigation = WaterBoundPathNavigation(this, world)
+    }
+
+    override fun createNavigation(level: Level): PathNavigation {
+        return WaterBoundPathNavigation(this, level)
+    }
+
+    override fun registerControllers(controllers: AnimatableManager.ControllerRegistrar) {
+        controllers.add(DefaultAnimations.genericWalkRunIdleController(this))
+        controllers.add(DefaultAnimations.genericAttackAnimation(this, DefaultAnimations.ATTACK_SWING))
+    }
+
+    override fun travel(travelVector: Vec3) {
+        if (this.isEffectiveAi && this.isInWater) {
+            this.moveRelative(this.speed, travelVector)
+            this.move(MoverType.SELF, this.deltaMovement)
+            this.deltaMovement = deltaMovement.scale(0.9)
+            if (this.target == null) {
+                this.deltaMovement = deltaMovement.add(0.0, -0.005, 0.0)
+            }
+        } else {
+            super.travel(travelVector)
+        }
     }
 
     override fun getHurtSound(source: DamageSource): SoundEvent {
@@ -33,7 +61,7 @@ class KarcinomaEntity(entityType: EntityType<out HybridAquaticMinionEntity>, wor
         fun createMobAttributes(): AttributeSupplier.Builder {
             return createLivingAttributes()
                 .add(Attributes.MAX_HEALTH, 6.0)
-                .add(Attributes.MOVEMENT_SPEED, 0.6)
+                .add(Attributes.MOVEMENT_SPEED, 1.0)
                 .add(Attributes.ATTACK_DAMAGE, 4.0)
                 .add(Attributes.ATTACK_KNOCKBACK, 0.0)
                 .add(Attributes.FOLLOW_RANGE, 24.0)
