8d1fac6b3047f6af103e2a5cb3dc1499ff3f677d
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-06 04:44:12 +0300

    Item group reorganization

--- fabric/src/main/kotlin/dev/hybridlabs/aquatic/item/HybridAquaticItemGroups.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/item/HybridAquaticItemGroups.kt
@@ -81,20 +81,6 @@ object HybridAquaticItemGroups {
                 entries.accept(HybridAquaticItems.LOPHELIA_CORAL_BLOCK.get())
                 entries.accept(HybridAquaticItems.THORN_CORAL_BLOCK.get())
 
-                entries.accept(HybridAquaticItems.SUN_CORAL.get())
-                entries.accept(HybridAquaticItems.LEAF_CORAL.get())
-                entries.accept(HybridAquaticItems.BUTTON_CORAL.get())
-                entries.accept(HybridAquaticItems.ROSE_CORAL.get())
-                entries.accept(HybridAquaticItems.LOPHELIA_CORAL.get())
-                entries.accept(HybridAquaticItems.THORN_CORAL.get())
-
-                entries.accept(HybridAquaticItems.SUN_CORAL_FAN.get())
-                entries.accept(HybridAquaticItems.LEAF_CORAL_FAN.get())
-                entries.accept(HybridAquaticItems.BUTTON_CORAL_FAN.get())
-                entries.accept(HybridAquaticItems.ROSE_CORAL_FAN.get())
-                entries.accept(HybridAquaticItems.LOPHELIA_CORAL_FAN.get())
-                entries.accept(HybridAquaticItems.THORN_CORAL_FAN.get())
-
                 entries.accept(HybridAquaticItems.DEAD_SUN_CORAL_BLOCK.get())
                 entries.accept(HybridAquaticItems.DEAD_LEAF_CORAL_BLOCK.get())
                 entries.accept(HybridAquaticItems.DEAD_BUTTON_CORAL_BLOCK.get())
@@ -102,12 +88,26 @@ object HybridAquaticItemGroups {
                 entries.accept(HybridAquaticItems.DEAD_LOPHELIA_CORAL_BLOCK.get())
                 entries.accept(HybridAquaticItems.DEAD_THORN_CORAL_BLOCK.get())
 
+                entries.accept(HybridAquaticItems.SUN_CORAL.get())
+                entries.accept(HybridAquaticItems.LEAF_CORAL.get())
+                entries.accept(HybridAquaticItems.BUTTON_CORAL.get())
+                entries.accept(HybridAquaticItems.ROSE_CORAL.get())
+                entries.accept(HybridAquaticItems.LOPHELIA_CORAL.get())
+                entries.accept(HybridAquaticItems.THORN_CORAL.get())
+
                 entries.accept(HybridAquaticItems.DEAD_SUN_CORAL.get())
                 entries.accept(HybridAquaticItems.DEAD_LEAF_CORAL.get())
                 entries.accept(HybridAquaticItems.DEAD_BUTTON_CORAL.get())
                 entries.accept(HybridAquaticItems.DEAD_ROSE_CORAL.get())
                 entries.accept(HybridAquaticItems.DEAD_LOPHELIA_CORAL.get())
                 entries.accept(HybridAquaticItems.DEAD_THORN_CORAL.get())
+
+                entries.accept(HybridAquaticItems.SUN_CORAL_FAN.get())
+                entries.accept(HybridAquaticItems.LEAF_CORAL_FAN.get())
+                entries.accept(HybridAquaticItems.BUTTON_CORAL_FAN.get())
+                entries.accept(HybridAquaticItems.ROSE_CORAL_FAN.get())
+                entries.accept(HybridAquaticItems.LOPHELIA_CORAL_FAN.get())
+                entries.accept(HybridAquaticItems.THORN_CORAL_FAN.get())
 
                 entries.accept(HybridAquaticItems.DEAD_SUN_CORAL_FAN.get())
                 entries.accept(HybridAquaticItems.DEAD_LEAF_CORAL_FAN.get())
