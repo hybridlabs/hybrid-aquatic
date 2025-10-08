266d3f3d3692131a1e23831dd3d1c2e7a9d7bb19
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-08 05:36:11 +0300

    Missing animation shit

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/HybridAquaticSharkEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/shark/HybridAquaticSharkEntity.kt
@@ -71,7 +71,7 @@ open class HybridAquaticSharkEntity(
         setPathfindingMalus(BlockPathTypes.WATER, 0.0f)
         setPathfindingMalus(BlockPathTypes.DANGER_FIRE, 16.0f)
         setPathfindingMalus(BlockPathTypes.DAMAGE_FIRE, -1.0f)
-        moveControl = SmoothSwimmingMoveControl(this, 85, 5, 0.02F, 0.1F, false)
+        moveControl = SmoothSwimmingMoveControl(this, 45, 3, 0.02F, 0.1F, false)
         lookControl = SmoothSwimmingLookControl(this, 15)
         navigation = WaterBoundPathNavigation(this, world)
     }
@@ -241,7 +241,7 @@ open class HybridAquaticSharkEntity(
                         state.setAndContinue(BEACHED_ANIMATION)
                     }
                     else -> {
-                        state.setAndContinue(DefaultAnimations.IDLE)
+                        state.setAndContinue(DefaultAnimations.SWIM)
                     }
                 }
             }
