46c86df86cdc62b7615c455c18004c7e506eed24
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-08 22:19:49 +0300

    Added random offsets to small algae blocks

--- common/src/main/kotlin/dev/hybridlabs/aquatic/block/HybridAquaticBlocks.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/block/HybridAquaticBlocks.kt
@@ -152,11 +152,11 @@ object HybridAquaticBlocks {
 
     val CATTAIL = register("cattail") { CattailBlock(Properties.copy(Blocks.TALL_GRASS)) }
 
-    val RED_ALGAE = register("red_algae") { RedAlgaeBlock(Properties.copy(Blocks.SEAGRASS)) }
+    val RED_ALGAE = register("red_algae") { RedAlgaeBlock(Properties.copy(Blocks.SEAGRASS).offsetType(BlockBehaviour.OffsetType.XZ)) }
     val TALL_RED_ALGAE = register("tall_red_algae") { TallRedAlgaeBlock(Properties.copy(Blocks.TALL_SEAGRASS).offsetType(BlockBehaviour.OffsetType.NONE)) }
 
-    val SEA_LETTUCE = register("sea_lettuce") { SeaLettuceBlock(Properties.copy((Blocks.SEAGRASS))) }
-    val TALL_SEA_LETTUCE = register("tall_sea_lettuce") { TallSeaLettuceBlock(Properties.copy((Blocks.TALL_SEAGRASS)).offsetType(BlockBehaviour.OffsetType.NONE)) }
+    val SEA_LETTUCE = register("sea_lettuce") { SeaLettuceBlock(Properties.copy(Blocks.SEAGRASS).offsetType(BlockBehaviour.OffsetType.XZ)) }
+    val TALL_SEA_LETTUCE = register("tall_sea_lettuce") { TallSeaLettuceBlock(Properties.copy(Blocks.TALL_SEAGRASS).offsetType(BlockBehaviour.OffsetType.NONE)) }
 
     val DEAD_LOPHELIA_CORAL_BLOCK = register(
         "dead_lophelia_coral_block"
