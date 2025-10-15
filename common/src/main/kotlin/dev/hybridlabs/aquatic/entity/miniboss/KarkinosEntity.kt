09cf6d5cfab2116ae6f0487af87e296f45010e8f
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-15 05:15:54 +0300

    Fixed Karkinos and Karcinogen underwater behaviour (needed for 1.20.1 too)

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/miniboss/KarkinosEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/miniboss/KarkinosEntity.kt
@@ -58,7 +58,7 @@ class KarkinosEntity(entityType: EntityType<out HybridAquaticMinibossEntity>, wo
     }
 
     override fun isAffectedByFluids(): Boolean {
-        return !onGround()
+        return false
     }
 
     override fun isPushedByFluid(): Boolean {
