a9408536b67a753d72740011fd4e918a09e349b1
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-21 03:39:18 +0300

    Removed crustacean and critter step sounds when climbing

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/critter/HybridAquaticCritterEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/critter/HybridAquaticCritterEntity.kt
@@ -61,10 +61,6 @@ open class HybridAquaticCritterEntity(
         if (isClimbingWall()) {
             climbingTicks++
 
-            val blockStateAtPos = level().getBlockState(blockPosition())
-            if (isMoving() && blockStateAtPos.fluidState.isEmpty && climbingTicks % 6 == 0) {
-                playStepSound(blockPosition(), blockStateAtPos)
-            }
         } else {
             climbingTicks = 0
         }
