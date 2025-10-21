239efbeb35b1fab947e65ab1656081c0e7d0d0fd
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-21 19:54:08 +0300

    Aqua

--- common/src/main/kotlin/dev/hybridlabs/aquatic/world/gen/feature/AnemoneFeature.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/world/gen/feature/AnemoneFeature.kt
@@ -8,6 +8,7 @@ import net.minecraft.core.Holder
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.tags.FluidTags
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.DirectionalBlock
 import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock
 import net.minecraft.world.level.block.state.properties.AttachFace
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
@@ -35,7 +36,7 @@ class AnemoneFeature(configuration: Codec<NoneFeatureConfiguration>) :
             .map { it.defaultBlockState() }
             .orElseGet { HybridAquaticBlocks.ANEMONE.get().defaultBlockState() }
             .setValue(FaceAttachedHorizontalDirectionalBlock.FACE, AttachFace.FLOOR)
-            .setValue(FaceAttachedHorizontalDirectionalBlock.FACING, Direction.NORTH)
+            .setValue(DirectionalBlock.FACING, Direction.NORTH)
             .setValue(BlockStateProperties.WATERLOGGED, true)
 
         for (j in 0 until 5) {
