91df891487017e7cbbb9ad402ae0d15ae45b5569
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-21 23:42:33 +0300

    Revert "partially fixed anemone gen?"
    This reverts commit e4eb5dc4048e7b5173aa85989e6993db4073014b.

--- common/src/main/kotlin/dev/hybridlabs/aquatic/world/gen/feature/AnemoneFeature.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/world/gen/feature/AnemoneFeature.kt
@@ -36,6 +36,7 @@ class AnemoneFeature(configuration: Codec<NoneFeatureConfiguration>) :
             .map { it.defaultBlockState() }
             .orElseGet { HybridAquaticBlocks.ANEMONE.get().defaultBlockState() }
             .setValue(FaceAttachedHorizontalDirectionalBlock.FACE, AttachFace.FLOOR)
+            .setValue(DirectionalBlock.FACING, Direction.NORTH)
             .setValue(BlockStateProperties.WATERLOGGED, true)
 
         for (j in 0 until 5) {
