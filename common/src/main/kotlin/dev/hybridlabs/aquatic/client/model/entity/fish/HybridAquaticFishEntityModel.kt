780268ecc6eee4c462f8964ca0041037526b4879
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-23 23:17:12 +0300

    Another fix

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/HybridAquaticFishEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/HybridAquaticFishEntityModel.kt
@@ -33,6 +33,11 @@ abstract class HybridAquaticFishEntityModel<T : HybridAquaticFishEntity>(private
         animationState: AnimationState<T>
     ) {
         super.setCustomAnimations(animatable, instanceId, animationState)
+
+        if (!animatable.isInWater) {
+            return
+        }
+
         val deltaTime = Minecraft.getInstance().deltaFrameTime
         val body = animationProcessor.getBone(PartNames.BODY)
         val body2 = animationProcessor.getBone("body_2")
