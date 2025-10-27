38652e1d40b2f759843a2a440770b993c34bcb5f
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-10-27 12:53:21 +0300

    Anemone shape

--- common/src/main/kotlin/dev/hybridlabs/aquatic/block/AnemoneBlock.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/block/AnemoneBlock.kt
@@ -135,10 +135,10 @@ class AnemoneBlock(settings: Properties) : FaceAttachedHorizontalDirectionalBloc
         val CODEC: MapCodec<AnemoneBlock> = simpleCodec(::AnemoneBlock)
         private val CEILING_SHAPE: VoxelShape = box(1.0, 0.0, 1.0, 15.0, 16.0, 16.0)
         private val FLOOR_SHAPE: VoxelShape = box(1.0, 0.0, 1.0, 15.0, 16.0, 15.0)
-        private val NORTH_SHAPE: VoxelShape = box(1.0, 0.0, 1.0, 15.0, 16.0, 15.0)
-        private val SOUTH_SHAPE: VoxelShape = box(1.0, 0.0, 1.0, 15.0, 16.0, 15.0)
-        private val WEST_SHAPE: VoxelShape = box(1.0, 0.0, 1.0, 15.0, 16.0, 15.0)
-        private val EAST_SHAPE: VoxelShape = box(1.0, 0.0, 1.0, 15.0, 16.0, 15.0)
+        private val NORTH_SHAPE: VoxelShape = box(1.0, 0.0, 0.0, 15.0, 16.0, 15.0)
+        private val SOUTH_SHAPE: VoxelShape = box(1.0, 0.0, 0.0, 15.0, 16.0, 15.0)
+        private val WEST_SHAPE: VoxelShape = box(0.0, 0.0, 1.0, 15.0, 16.0, 15.0)
+        private val EAST_SHAPE: VoxelShape = box(0.0, 0.0, 1.0, 15.0, 16.0, 15.0)
         private val COLLISION_SHAPE = box(1.0, 0.0, 1.0, 15.0, 8.0, 15.0)
     }
 }
