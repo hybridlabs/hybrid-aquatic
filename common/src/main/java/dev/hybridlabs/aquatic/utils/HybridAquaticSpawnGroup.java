4ae6684ecaea9756a96fef1a6e7054c67adce172
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2025-12-03 18:41:12 +0300

    custom Spawn Groups changes

--- common/src/main/java/dev/hybridlabs/aquatic/utils/HybridAquaticSpawnGroup.java
+++ common/src/main/java/dev/hybridlabs/aquatic/utils/HybridAquaticSpawnGroup.java
@@ -1,34 +1,24 @@
 package dev.hybridlabs.aquatic.utils;
 
+import dev.hybridlabs.aquatic.Constants;
 import net.minecraft.world.entity.MobCategory;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public enum HybridAquaticSpawnGroup {
-    HYBRID_AQUATIC_FISH("HYBRID_AQUATIC_FISH", 8, true, false, 64),
-
-    HYBRID_AQUATIC_FISH_UNDERGROUND("HYBRID_AQUATIC_FISH_UNDERGROUND", 8, true, false, 64),
-
-    HYBRID_AQUATIC_CEPHALOPOD("HYBRID_AQUATIC_CEPHALOPOD", 8, true, false, 64),
-
-    HYBRID_AQUATIC_JELLY("HYBRID_AQUATIC_JELLY", 5, true, false, 64),
-
-    HYBRID_AQUATIC_JELLY_UNDERGROUND("HYBRID_AQUATIC_JELLY_UNDERGROUND", 5, true, false, 64),
-
-    HYBRID_AQUATIC_SHARK("HYBRID_AQUATIC_SHARK", 8, true, true, 128),
-
-    HYBRID_AQUATIC_SHARK_UNDERGROUND("HYBRID_AQUATIC_SHARK_UNDERGROUND", 8, true, false, 128),
-
-    HYBRID_AQUATIC_CRUSTACEAN("HYBRID_AQUATIC_CRUSTACEAN", 8, true, false, 64),
-
-    HYBRID_AQUATIC_CRUSTACEAN_UNDERGROUND("HYBRID_AQUATIC_CRUSTACEAN_UNDERGROUND", 8, true, false, 64),
-
-    HYBRID_AQUATIC_CRITTER("HYBRID_AQUATIC_CRITTER", 8, true, false, 64),
-
-    HYBRID_AQUATIC_MINIBOSS("HYBRID_AQUATIC_MINIBOSS", 10, false, true, 128),
-
-    HYBRID_AQUATIC_MINION("HYBRID_AQUATIC_MINION", 12, false, true, 128);
+    HYBRID_AQUATIC_FISH("fish", 8, true, false, 64),
+    HYBRID_AQUATIC_FISH_UNDERGROUND("fish_underground", 8, true, false, 64),
+    HYBRID_AQUATIC_CEPHALOPOD("cephalopod", 8, true, false, 64),
+    HYBRID_AQUATIC_JELLY("jelly", 5, true, false, 64),
+    HYBRID_AQUATIC_JELLY_UNDERGROUND("jelly_underground", 5, true, false, 64),
+    HYBRID_AQUATIC_SHARK("shark", 8, true, true, 128),
+    HYBRID_AQUATIC_SHARK_UNDERGROUND("shark_underground", 8, true, false, 128),
+    HYBRID_AQUATIC_CRUSTACEAN("crustacean", 8, true, false, 64),
+    HYBRID_AQUATIC_CRUSTACEAN_UNDERGROUND("crustacean_underground", 8, true, false, 64),
+    HYBRID_AQUATIC_CRITTER("critter", 8, true, false, 64),
+    HYBRID_AQUATIC_MINIBOSS("miniboss", 10, false, true, 128),
+    HYBRID_AQUATIC_MINION("minion", 12, false, true, 128);
 
     public MobCategory spawnGroup;
     public final String name;
@@ -38,7 +28,7 @@ public enum HybridAquaticSpawnGroup {
     public final int immediateDespawnRange;
 
     HybridAquaticSpawnGroup(String name, int spawnCap, boolean peaceful, boolean rare, int immediateDespawnRange) {
-        this.name = name;
+        this.name = Constants.MOD_ID + ":" + name;
         this.spawnCap = spawnCap;
         this.peaceful = peaceful;
         this.rare = rare;
