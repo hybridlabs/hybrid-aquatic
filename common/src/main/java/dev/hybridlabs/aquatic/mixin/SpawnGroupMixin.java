0ec52b6868b3443edc547ce4e6b6c07f86e36b08
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2025-12-03 18:00:15 +0300

    include SpawnGroupMixin.java in mixin config for neoforge

--- common/src/main/java/dev/hybridlabs/aquatic/mixin/SpawnGroupMixin.java
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/SpawnGroupMixin.java
@@ -24,8 +24,8 @@ public class SpawnGroupMixin {
     private static MobCategory[] $VALUES;
 
     @Unique
-    private static MobCategory createHybridAquaticSpawnGroups(String enumname, int ordinal,
-                                                              HybridAquaticSpawnGroup spawnGroup) {
+    private static MobCategory hybrid_aquatic$createHybridAquaticSpawnGroups(String enumname, int ordinal,
+                                                                             HybridAquaticSpawnGroup spawnGroup) {
         return ((MobCategory) (Object) new SpawnGroupMixin(spawnGroup.name, ordinal, spawnGroup.name,
                 spawnGroup.spawnCap, spawnGroup.peaceful, spawnGroup.rare, spawnGroup.immediateDespawnRange));
     }
@@ -43,9 +43,9 @@ public class SpawnGroupMixin {
         for (int i = 0; i < haSpawnGroups.length; i++) {
             int pos = vanillaSpawnGroupsLength + i;
             HybridAquaticSpawnGroup haSpawnGroup = haSpawnGroups[i];
-            haSpawnGroup.spawnGroup = $VALUES[pos] = createHybridAquaticSpawnGroups(haSpawnGroup.name(), pos,
-                    haSpawnGroup);
-
+            haSpawnGroup.spawnGroup = $VALUES[pos] = hybrid_aquatic$createHybridAquaticSpawnGroups(haSpawnGroup.name(), pos,
+                haSpawnGroup);
+            
             HybridAquaticSpawnGroup.BY_NAME.put(haSpawnGroup.name(), haSpawnGroup.spawnGroup);
         }
     }
