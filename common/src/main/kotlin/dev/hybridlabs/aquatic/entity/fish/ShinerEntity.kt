625e7f49992e8a2a0a673c33f58625502e7f40e9
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-12-15 05:09:13 +0200

    Cleanup

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/ShinerEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/ShinerEntity.kt
@@ -1,7 +1,5 @@
 package dev.hybridlabs.aquatic.entity.fish
 
-import dev.hybridlabs.aquatic.entity.ai.goal.boids.BoidGoal
-import dev.hybridlabs.aquatic.entity.ai.goal.boids.StayInWaterGoal
 import dev.hybridlabs.aquatic.tag.HybridAquaticEntityTags
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier
