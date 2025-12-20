5b740c4453abcfbbb152419d5d85229c88514fa1
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-12-20 15:12:58 +0200

    More stuff

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/entity/fish/PlecoEntityRenderer.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/entity/fish/PlecoEntityRenderer.kt
@@ -5,4 +5,4 @@ import dev.hybridlabs.aquatic.entity.fish.PlecoEntity
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context
 
 class PlecoEntityRenderer(context: Context) :
-    HybridAquaticFishEntityRenderer<PlecoEntity>(context, PlecoEntityModel(), false, false)
+    HybridAquaticFishEntityRenderer<PlecoEntity>(context, PlecoEntityModel(), true, false)
