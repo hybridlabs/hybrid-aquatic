41d63cb6fc0a55a660b3dca4fe0d3c949d7690d9
MysticKoko <132279944+mystickoko@users.noreply.github.com>
2025-12-23 02:51:09 -0800

    stuff
    # Conflicts:
    #	gradle.properties

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/OceanSunfishEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/OceanSunfishEntityModel.kt
@@ -5,18 +5,18 @@ import net.minecraft.resources.ResourceLocation
 
 class OceanSunfishEntityModel : HybridAquaticFishEntityModel<OceanSunfishEntity>("ocean_sunfish") {
 
-    private val OCEAN_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/ocean_sunfish_ocean.png")
-    private val HOODWINKER_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/hoodwinker_ocean_sunfish.png")
-    private val SHARPTAIL_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/sharptail_ocean_sunfish.png")
-    private val GIANT_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/giant_ocean_sunfish.png")
+    private val OCEAN_TEXTURE = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/ocean_sunfish.png")
+    private val HOODWINKER_TEXTURE = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/hoodwinker_sunfish.png")
+    private val SHARPTAIL_TEXTURE = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/sharptail_sunfish.png")
+    private val GIANT_TEXTURE = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/giant_sunfish.png")
 
-    private val SUNFISH_MODEL = ResourceLocation("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish.geo.json")
-    private val HOODWINKER_MODEL = ResourceLocation("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish_hoodwinker.geo.json")
-    private val GIANT_MODEL = ResourceLocation("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish_giant.geo.json")
+    private val SUNFISH_MODEL = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish.geo.json")
+    private val HOODWINKER_MODEL = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish_hoodwinker.geo.json")
+    private val GIANT_MODEL = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish_giant.geo.json")
 
-    private val SUNFISH_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/ocean_sunfish.animation.json")
-    private val HOODWINKER_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/ocean_sunfish_hoodwinker.animation.json")
-    private val GIANT_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/ocean_sunfish_giant.animation.json")
+    private val SUNFISH_ANIMATION = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "animations/ocean_sunfish.animation.json")
+    private val HOODWINKER_ANIMATION = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "animations/ocean_sunfish_hoodwinker.animation.json")
+    private val GIANT_ANIMATION = ResourceLocation.fromNamespaceAndPath("hybrid-aquatic", "animations/ocean_sunfish_giant.animation.json")
 
     override fun getTextureResource(animatable: OceanSunfishEntity): ResourceLocation {
         return when (animatable.variant) {
