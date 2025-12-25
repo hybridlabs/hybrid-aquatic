a7dd367576695575b87c67b5d05a54831be13c27
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-12-25 02:04:55 +0200

    Fixed sunfish file names

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/OceanSunfishEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/OceanSunfishEntityModel.kt
@@ -11,12 +11,12 @@ class OceanSunfishEntityModel : HybridAquaticFishEntityModel<OceanSunfishEntity>
     private val GIANT_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/fish/ocean_sunfish/giant_sunfish.png")
 
     private val SUNFISH_MODEL = ResourceLocation("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish.geo.json")
-    private val HOODWINKER_MODEL = ResourceLocation("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish_hoodwinker.geo.json")
-    private val GIANT_MODEL = ResourceLocation("hybrid-aquatic", "geo/fish/ocean_sunfish/ocean_sunfish_giant.geo.json")
+    private val HOODWINKER_MODEL = ResourceLocation("hybrid-aquatic", "geo/fish/ocean_sunfish/hoodwinker_sunfish.geo.json")
+    private val GIANT_MODEL = ResourceLocation("hybrid-aquatic", "geo/fish/ocean_sunfish/giant_sunfish.geo.json")
 
     private val SUNFISH_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/ocean_sunfish.animation.json")
-    private val HOODWINKER_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/ocean_sunfish_hoodwinker.animation.json")
-    private val GIANT_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/ocean_sunfish_giant.animation.json")
+    private val HOODWINKER_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/hoodwinker_sunfish.animation.json")
+    private val GIANT_ANIMATION = ResourceLocation("hybrid-aquatic", "animations/giant_sunfish.animation.json")
 
     override fun getTextureResource(animatable: OceanSunfishEntity): ResourceLocation {
         return when (animatable.variant) {
