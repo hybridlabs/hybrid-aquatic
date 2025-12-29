866f1dedff0879b09cfce1683c11a1b0888f8213
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-12-29 13:53:19 +0200

    More unfucking

--- fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/HybridAquaticDataGenerator.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/HybridAquaticDataGenerator.kt
@@ -10,15 +10,13 @@ import dev.hybridlabs.aquatic.data.server.loot.EntityTypeLootTableProvider
 import dev.hybridlabs.aquatic.data.server.loot.FishingLootTableProvider
 import dev.hybridlabs.aquatic.data.server.loot.GenericLootTableProvider
 import dev.hybridlabs.aquatic.data.server.seamessage.SeaMessageProvider
-import dev.hybridlabs.aquatic.data.server.tag.BiomeTagProvider
-import dev.hybridlabs.aquatic.data.server.tag.BlockTagProvider
-import dev.hybridlabs.aquatic.data.server.tag.EntityTypeTagProvider
-import dev.hybridlabs.aquatic.data.server.tag.ItemTagProvider
-import dev.hybridlabs.aquatic.data.server.tag.PaintingVariantTagProvider
+import dev.hybridlabs.aquatic.data.server.tag.*
+import dev.hybridlabs.aquatic.data.server.worldgen.BiomeProvider
 import dev.hybridlabs.aquatic.data.server.worldgen.ConfiguredFeatureProvider
 import dev.hybridlabs.aquatic.data.server.worldgen.PlacedFeatureProvider
 import dev.hybridlabs.aquatic.data.structure_spawn_modifier.StructureSpawnModifierProvider
 import dev.hybridlabs.aquatic.registry.HybridAquaticRegistryKeys
+import dev.hybridlabs.aquatic.world.gen.biome.HybridAquaticBiomes
 import dev.hybridlabs.aquatic.world.gen.feature.HybridAquaticConfiguredFeatures
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
@@ -36,6 +34,7 @@ object HybridAquaticDataGenerator : DataGeneratorEntrypoint {
         pack.addProvider(::FishingLootTableProvider)
         pack.addProvider(::GenericLootTableProvider)
         pack.addProvider(::AdvancementProvider)
+        pack.addProvider(::BiomeProvider)
         pack.addProvider(::BiomeTagProvider)
         pack.addProvider(::BlockTagProvider)
         pack.addProvider(::PaintingVariantTagProvider)
@@ -51,6 +50,9 @@ object HybridAquaticDataGenerator : DataGeneratorEntrypoint {
     override fun buildRegistry(registryBuilder: RegistrySetBuilder) {
         registryBuilder.add(HybridAquaticRegistryKeys.SEA_MESSAGE) {}
         registryBuilder.add(HybridAquaticRegistryKeys.STRUCTURE_SPAWN_MODIFIER) {}
+        registryBuilder.add(Registries.BIOME) {
+            HybridAquaticBiomes
+        }
         registryBuilder.add(Registries.CONFIGURED_FEATURE) {
             HybridAquaticConfiguredFeatures
         }
