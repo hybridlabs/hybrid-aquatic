064814c879fc89dc21cbe57843a9c7fe827be0ff
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-12-29 05:19:58 +0200

    Unfucked what I wrongfully fucked

--- fabric/src/main/kotlin/dev/hybridlabs/aquatic/HybridAquatic.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/HybridAquatic.kt
@@ -21,6 +21,7 @@ import dev.hybridlabs.aquatic.potions.HybridAquaticPotions
 import dev.hybridlabs.aquatic.registry.HybridAquaticRegistryKeys
 import dev.hybridlabs.aquatic.tag.HybridAquaticBiomeTags
 import dev.hybridlabs.aquatic.utils.HybridAquaticCustomTrades.registerCustomTrades
+import dev.hybridlabs.aquatic.world.gen.biome.HybridAquaticBiomes
 import dev.hybridlabs.aquatic.world.gen.feature.DunegrassFeature
 import dev.hybridlabs.aquatic.world.gen.feature.FeatureBiomeModifications
 import dev.hybridlabs.aquatic.world.gen.feature.HybridAquaticConfiguredFeatures
@@ -56,6 +57,8 @@ object HybridAquatic : ModInitializer {
         HybridAquaticEntityTypes
         HybridAquaticBlockEntityTypes
         HybridAquaticPaintings
+
+        HybridAquaticBiomes.addBiomes()
 
         HybridAquaticBiomeTags
 
