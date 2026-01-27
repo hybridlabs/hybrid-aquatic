5966334743c32eb3a8f5dabd6f556a1fa15d24b6
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-01-27 21:04:57 +0300

    cleaned-up HABlocks

--- common/src/main/kotlin/dev/hybridlabs/aquatic/block/HybridAquaticBlocks.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/block/HybridAquaticBlocks.kt
@@ -30,46 +30,53 @@ import java.util.function.Supplier
  * The registry of all blocks in Hybrid Aquatic.
  */
 object HybridAquaticBlocks {
-    val ANEMONE = register(
-        "anemone"
-    ) {
-        AnemoneBlock(
-            Properties.of().mapColor(MapColor.TERRACOTTA_PINK).randomTicks().strength(0.4f).noOcclusion()
-                .pushReaction(PushReaction.DESTROY).sound(SoundType.SLIME_BLOCK).noParticlesOnBreak()
+    val ANEMONE = register("anemone") {
+        AnemoneBlock(Properties.of()
+            .mapColor(MapColor.TERRACOTTA_PINK)
+            .randomTicks()
+            .strength(0.4f)
+            .noOcclusion()
+            .pushReaction(PushReaction.DESTROY)
+            .sound(SoundType.SLIME_BLOCK)
+            .noParticlesOnBreak()
         )
     }
 
-    val GIANT_GREEN_ANEMONE = register(
-        "giant_green_anemone"
-    ) {
-        GiantGreenAnemoneBlock(
-            Properties.of().mapColor(MapColor.COLOR_LIGHT_GREEN).randomTicks().strength(0.4f).noOcclusion()
-                .pushReaction(PushReaction.DESTROY).sound(SoundType.SLIME_BLOCK).noParticlesOnBreak()
+    val GIANT_GREEN_ANEMONE = register("giant_green_anemone") {
+        GiantGreenAnemoneBlock(Properties.of()
+            .mapColor(MapColor.COLOR_LIGHT_GREEN)
+            .randomTicks()
+            .strength(0.4f)
+            .noOcclusion()
+            .pushReaction(PushReaction.DESTROY)
+            .sound(SoundType.SLIME_BLOCK)
+            .noParticlesOnBreak()
         )
     }
 
-    val STRAWBERRY_ANEMONE = register(
-        "strawberry_anemone"
-    ) {
-        StrawberryAnemoneBlock(
-            Properties.of().mapColor(MapColor.COLOR_RED).randomTicks().strength(0.4f).noOcclusion()
-                .pushReaction(PushReaction.DESTROY).sound(SoundType.SLIME_BLOCK).noParticlesOnBreak()
+    val STRAWBERRY_ANEMONE = register("strawberry_anemone") {
+        StrawberryAnemoneBlock(Properties.of()
+            .mapColor(MapColor.COLOR_RED)
+            .randomTicks()
+            .strength(0.4f)
+            .noOcclusion()
+            .pushReaction(PushReaction.DESTROY)
+            .sound(SoundType.SLIME_BLOCK)
+            .noParticlesOnBreak()
         )
     }
 
-    val MESSAGE_IN_A_BOTTLE = register(
-        "message_in_a_bottle",
-    ) {
-        MessageInABottleBlock(
-            Properties.copy(Blocks.GLASS).instabreak().pushReaction(PushReaction.DESTROY)
-        )
+    val MESSAGE_IN_A_BOTTLE = register("message_in_a_bottle",) {
+        MessageInABottleBlock(Properties.copy(Blocks.GLASS)
+            .instabreak()
+            .pushReaction(PushReaction.DESTROY))
     }
 
     val WHITE_SAND = register("white_sand") { SandBlock(14406560, Properties.copy(Blocks.SAND)) }
     val WHITE_SANDSTONE = register("white_sandstone") { Block(Properties.copy(Blocks.SANDSTONE)) }
 
-    val SUSPICIOUS_RED_SAND = register("suspicious_red_sand") { BrushableBlock(
-        Blocks.RED_SAND,
+    val SUSPICIOUS_RED_SAND = register("suspicious_red_sand") {
+        BrushableBlock(Blocks.RED_SAND,
         Properties.copy(Blocks.RED_SAND),
         SoundEvents.BRUSH_SAND,
         SoundEvents.BRUSH_SAND_COMPLETED) }
@@ -106,76 +113,56 @@ object HybridAquaticBlocks {
     val CHERRY_CRATE = register("cherry_crate") { CrateBlock(Properties.copy(Blocks.CHERRY_PLANKS)) }
     val BAMBOO_CRATE = register("bamboo_crate") { CrateBlock(Properties.copy(Blocks.BAMBOO_PLANKS)) }
 
-    val SARGASSUM = register(
-        "sargassum"
-    ) {
-        SargassumBlock(
-            Properties.copy(Blocks.KELP).noOcclusion()
-        )
+    val SARGASSUM = register("sargassum") {
+        SargassumBlock(Properties.copy(Blocks.KELP)
+            .noOcclusion())
     }
 
-    val SARGASSUM_PLANT = register(
-        "sargassum_plant"
-    ) {
-        SargassumPlantBlock(
-            Properties.copy(Blocks.KELP_PLANT).noOcclusion().dropsLike(SARGASSUM.get())
-        )
+    val SARGASSUM_PLANT = register("sargassum_plant") {
+        SargassumPlantBlock(Properties.copy(Blocks.KELP_PLANT)
+            .noOcclusion()
+            .dropsLike(SARGASSUM.get()))
     }
 
-    val BULL_KELP = register(
-        "bull_kelp"
-    ) {
-        BullKelpBlock(
-            Properties.copy(Blocks.KELP).noOcclusion()
-        )
+    val BULL_KELP = register("bull_kelp") {
+        BullKelpBlock(Properties.copy(Blocks.KELP)
+            .noOcclusion())
     }
 
-    val BULL_KELP_PLANT = register(
-        "bull_kelp_plant"
-    ) {
-        BullKelpPlantBlock(
-            Properties.copy(Blocks.KELP_PLANT).noOcclusion().dropsLike(BULL_KELP.get())
-        )
+    val BULL_KELP_PLANT = register("bull_kelp_plant") {
+        BullKelpPlantBlock(Properties.copy(Blocks.KELP_PLANT)
+            .noOcclusion()
+            .dropsLike(BULL_KELP.get()))
     }
 
-    val FLOATING_SARGASSUM = register(
-        "floating_sargassum"
-    ) {
-        FloatingSargassumBlock(
-            Properties.copy(Blocks.LILY_PAD).noCollission().instabreak().mapColor(MapColor.WOOD)
-        )
+    val FLOATING_SARGASSUM = register("floating_sargassum") {
+        FloatingSargassumBlock(Properties.copy(Blocks.LILY_PAD)
+            .noCollission()
+            .instabreak()
+            .mapColor(MapColor.WOOD))
     }
 
-    val WATER_LETTUCE = register(
-        "water_lettuce"
-    ) {
-        WaterLettuceBlock(
-            Properties.copy(Blocks.LILY_PAD).noCollission().instabreak()
-        )
+    val WATER_LETTUCE = register("water_lettuce") {
+        WaterLettuceBlock(Properties.copy(Blocks.LILY_PAD)
+            .noCollission()
+            .instabreak())
     }
 
-    val JUNGLE_LILY_PAD = register(
-        "jungle_lily_pad"
-    ) {
-        JungleLilyPadBlock(
-            Properties.copy(Blocks.LILY_PAD).instabreak()
-        )
+    val JUNGLE_LILY_PAD = register("jungle_lily_pad") {
+        JungleLilyPadBlock(Properties.copy(Blocks.LILY_PAD)
+            .instabreak())
     }
 
-    val RAFT = register(
-        "raft"
-    ) {
-        RaftBlock(
-            Properties.copy(Blocks.OAK_WOOD)
-        )
+    val RAFT = register("raft") {
+        RaftBlock(Properties.copy(Blocks.OAK_WOOD))
     }
 
-    val GLOWING_PLANKTON = register(
-        "glowing_plankton"
-    ) {
-        GlowingPlanktonBlock(
-            Properties.copy(Blocks.LILY_PAD).noOcclusion().replaceable().noCollission().instabreak()
-        )
+    val GLOWING_PLANKTON = register("glowing_plankton") {
+        GlowingPlanktonBlock(Properties.copy(Blocks.LILY_PAD)
+        .noOcclusion()
+        .replaceable()
+        .noCollission()
+        .instabreak())
     }
 
 
@@ -186,24 +173,16 @@ object HybridAquaticBlocks {
     val SEA_LETTUCE = register("sea_lettuce") { SeaLettuceBlock(Properties.copy(Blocks.SEAGRASS).offsetType(BlockBehaviour.OffsetType.XZ)) }
     val TALL_SEA_LETTUCE = register("tall_sea_lettuce") { TallSeaLettuceBlock(Properties.copy(Blocks.TALL_SEAGRASS).offsetType(BlockBehaviour.OffsetType.NONE)) }
 
-    val DEAD_LOPHELIA_CORAL_BLOCK = register(
-        "dead_lophelia_coral_block"
-    ) {
-        Block(
-            Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
-                .mapColor(MapColor.TERRACOTTA_WHITE)
-        )
+    val DEAD_LOPHELIA_CORAL_BLOCK = register("dead_lophelia_coral_block") {
+        Block(Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
+            .mapColor(MapColor.TERRACOTTA_WHITE))
     }
 
-    val LOPHELIA_CORAL_BLOCK = register(
-        "lophelia_coral_block"
-    ) {
-        CoralBlock(
-            DEAD_LOPHELIA_CORAL_BLOCK.get(),
+    val LOPHELIA_CORAL_BLOCK = register("lophelia_coral_block") {
+        CoralBlock(DEAD_LOPHELIA_CORAL_BLOCK.get(),
             Properties.copy(Blocks.FIRE_CORAL_BLOCK)
                 .mapColor(MapColor.TERRACOTTA_WHITE)
-                .sound(SoundType.CORAL_BLOCK)
-        )
+                .sound(SoundType.CORAL_BLOCK))
     }
 
     val DEAD_LOPHELIA_CORAL_FAN = register("dead_lophelia_coral_fan") { BaseCoralFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_FAN)) }
@@ -213,24 +192,17 @@ object HybridAquaticBlocks {
     val DEAD_LOPHELIA_CORAL_WALL_FAN = register("dead_lophelia_coral_wall_fan") { BaseCoralWallFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_WALL_FAN)) }
     val LOPHELIA_CORAL_WALL_FAN = register("lophelia_coral_wall_fan") { CoralWallFanBlock(DEAD_LOPHELIA_CORAL_WALL_FAN.get(), Properties.copy(Blocks.FIRE_CORAL_WALL_FAN)) }
 
-    val DEAD_ROSE_CORAL_BLOCK = register(
-        "dead_rose_coral_block"
-    ) {
-        Block(
-            Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
-                .mapColor(MapColor.TERRACOTTA_PINK)
-        )
+    val DEAD_ROSE_CORAL_BLOCK = register("dead_rose_coral_block") {
+        Block(Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
+            .mapColor(MapColor.TERRACOTTA_PINK))
     }
 
-    val ROSE_CORAL_BLOCK = register(
-        "rose_coral_block"
-    ) {
+    val ROSE_CORAL_BLOCK = register("rose_coral_block") {
         CoralBlock(
             DEAD_ROSE_CORAL_BLOCK.get(),
             Properties.copy(Blocks.FIRE_CORAL_BLOCK)
                 .mapColor(MapColor.TERRACOTTA_PINK)
-                .sound(SoundType.CORAL_BLOCK)
-        )
+                .sound(SoundType.CORAL_BLOCK))
     }
 
     val DEAD_ROSE_CORAL_FAN = register("dead_rose_coral_fan") { BaseCoralFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_FAN)) }
@@ -240,24 +212,16 @@ object HybridAquaticBlocks {
     val DEAD_ROSE_CORAL_WALL_FAN = register("dead_rose_coral_wall_fan") { BaseCoralWallFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_WALL_FAN)) }
     val ROSE_CORAL_WALL_FAN = register("rose_coral_wall_fan") { CoralWallFanBlock(DEAD_ROSE_CORAL_WALL_FAN.get(), Properties.copy(Blocks.FIRE_CORAL_WALL_FAN)) }
 
-    val DEAD_LEAF_CORAL_BLOCK = register(
-        "dead_leaf_coral_block"
-    ) {
-        Block(
-            Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
-                .mapColor(MapColor.TERRACOTTA_GREEN)
-        )
+    val DEAD_LEAF_CORAL_BLOCK = register("dead_leaf_coral_block") {
+        Block(Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
+            .mapColor(MapColor.TERRACOTTA_GREEN))
     }
 
-    val LEAF_CORAL_BLOCK = register(
-        "leaf_coral_block"
-    ) {
-        CoralBlock(
-            DEAD_LEAF_CORAL_BLOCK.get(),
+    val LEAF_CORAL_BLOCK = register("leaf_coral_block") {
+        CoralBlock(DEAD_LEAF_CORAL_BLOCK.get(),
             Properties.copy(Blocks.FIRE_CORAL_BLOCK)
                 .mapColor(MapColor.TERRACOTTA_GREEN)
-                .sound(SoundType.CORAL_BLOCK)
-        )
+                .sound(SoundType.CORAL_BLOCK))
     }
 
     val DEAD_LEAF_CORAL_FAN = register("dead_leaf_coral_fan") { BaseCoralFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_FAN)) }
@@ -267,25 +231,18 @@ object HybridAquaticBlocks {
     val DEAD_LEAF_CORAL_WALL_FAN = register("dead_leaf_coral_wall_fan") { BaseCoralWallFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_WALL_FAN)) }
     val LEAF_CORAL_WALL_FAN = register("leaf_coral_wall_fan") { CoralWallFanBlock(DEAD_LEAF_CORAL_WALL_FAN.get(), Properties.copy(Blocks.FIRE_CORAL_WALL_FAN)) }
 
-    val DEAD_SUN_CORAL_BLOCK = register(
-        "dead_sun_coral_block"
-    ) {
-        Block(
-            Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
-                .mapColor(MapColor.TERRACOTTA_WHITE)
-        )
+    val DEAD_SUN_CORAL_BLOCK = register("dead_sun_coral_block") {
+        Block(Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
+            .mapColor(MapColor.TERRACOTTA_WHITE))
     }
 
-    val SUN_CORAL_BLOCK = register(
-        "sun_coral_block"
-    ) {
-        CoralBlock(
-            DEAD_SUN_CORAL_BLOCK.get(),
+    val SUN_CORAL_BLOCK = register("sun_coral_block") {
+        CoralBlock(DEAD_SUN_CORAL_BLOCK.get(),
             Properties.copy(Blocks.FIRE_CORAL_BLOCK)
                 .mapColor(MapColor.TERRACOTTA_ORANGE)
-                .sound(SoundType.CORAL_BLOCK)
-        )
+                .sound(SoundType.CORAL_BLOCK))
     }
+
     val DEAD_SUN_CORAL_FAN = register("dead_sun_coral_fan") { BaseCoralFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_FAN)) }
     val SUN_CORAL_FAN = register("sun_coral_fan") { CoralFanBlock(DEAD_SUN_CORAL_FAN.get(), Properties.copy(Blocks.FIRE_CORAL_FAN)) }
     val DEAD_SUN_CORAL = register("dead_sun_coral") { BaseCoralPlantBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL)) }
@@ -293,24 +250,16 @@ object HybridAquaticBlocks {
     val DEAD_SUN_CORAL_WALL_FAN = register("dead_sun_coral_wall_fan") { BaseCoralWallFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_WALL_FAN)) }
     val SUN_CORAL_WALL_FAN = register("sun_coral_wall_fan") { CoralWallFanBlock(DEAD_SUN_CORAL_WALL_FAN.get(), Properties.copy(Blocks.FIRE_CORAL_WALL_FAN)) }
 
-    val DEAD_BUTTON_CORAL_BLOCK = register(
-        "dead_button_coral_block"
-    ) {
-        Block(
-            Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
-                .mapColor(MapColor.TERRACOTTA_WHITE)
-        )
+    val DEAD_BUTTON_CORAL_BLOCK = register("dead_button_coral_block") {
+        Block(Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
+                .mapColor(MapColor.TERRACOTTA_WHITE))
     }
 
-    val BUTTON_CORAL_BLOCK = register(
-        "button_coral_block"
-    ) {
-        CoralBlock(
-            DEAD_BUTTON_CORAL_BLOCK.get(),
+    val BUTTON_CORAL_BLOCK = register("button_coral_block") {
+        CoralBlock(DEAD_BUTTON_CORAL_BLOCK.get(),
             Properties.copy(Blocks.FIRE_CORAL_BLOCK)
                 .mapColor(MapColor.TERRACOTTA_WHITE)
-                .sound(SoundType.CORAL_BLOCK)
-        )
+                .sound(SoundType.CORAL_BLOCK))
     }
 
     val DEAD_BUTTON_CORAL_FAN = register("dead_button_coral_fan") { BaseCoralFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_FAN)) }
@@ -320,24 +269,16 @@ object HybridAquaticBlocks {
     val DEAD_BUTTON_CORAL_WALL_FAN = register("dead_button_coral_wall_fan") { BaseCoralWallFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_WALL_FAN)) }
     val BUTTON_CORAL_WALL_FAN = register("button_coral_wall_fan") { CoralWallFanBlock(DEAD_BUTTON_CORAL_WALL_FAN.get(), Properties.copy(Blocks.FIRE_CORAL_WALL_FAN)) }
 
-    val DEAD_THORN_CORAL_BLOCK = register(
-        "dead_thorn_coral_block"
-    ) {
-        Block(
-            Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
-                .mapColor(MapColor.TERRACOTTA_WHITE)
-        )
+    val DEAD_THORN_CORAL_BLOCK = register("dead_thorn_coral_block") {
+        Block(Properties.copy(Blocks.DEAD_FIRE_CORAL_BLOCK)
+                .mapColor(MapColor.TERRACOTTA_WHITE))
     }
 
-    val THORN_CORAL_BLOCK = register(
-        "thorn_coral_block"
-    ) {
-        CoralBlock(
-            DEAD_THORN_CORAL_BLOCK.get(),
+    val THORN_CORAL_BLOCK = register("thorn_coral_block") {
+        CoralBlock(DEAD_THORN_CORAL_BLOCK.get(),
             Properties.copy(Blocks.FIRE_CORAL_BLOCK)
                 .mapColor(MapColor.COLOR_BLACK)
-                .sound(SoundType.CORAL_BLOCK)
-        )
+                .sound(SoundType.CORAL_BLOCK))
     }
 
     val DEAD_THORN_CORAL_FAN = register("dead_thorn_coral_fan") { BaseCoralFanBlock(Properties.copy(Blocks.DEAD_FIRE_CORAL_FAN)) }
@@ -347,21 +288,19 @@ object HybridAquaticBlocks {
     val THORN_CORAL = register("thorn_coral") { CoralPlantBlock(DEAD_THORN_CORAL.get(), Properties.copy(Blocks.FIRE_CORAL)) }
     val THORN_CORAL_WALL_FAN = register("thorn_coral_wall_fan") { CoralWallFanBlock(DEAD_THORN_CORAL_WALL_FAN.get(), Properties.copy(Blocks.FIRE_CORAL_WALL_FAN)) }
 
-    val GLOWSTICK = register(
-        "glowstick"
-    ) {
-        GlowstickBlock(
-            Properties.copy(Blocks.TORCH).noCollission().lightLevel(GlowstickBlock::luminance).noOcclusion()
-        )
+    val GLOWSTICK = register("glowstick") {
+        GlowstickBlock(Properties.copy(Blocks.TORCH)
+            .noCollission()
+            .lightLevel(GlowstickBlock::luminance)
+            .noOcclusion())
     }
 
-    val WALL_GLOWSTICK = register(
-        "wall_glowstick"
-    ) {
-        WallGlowstickBlock(
-            Properties.copy(Blocks.WALL_TORCH).dropsLike(GLOWSTICK.get()).noCollission()
-                .lightLevel(GlowstickBlock::luminance).noOcclusion()
-        )
+    val WALL_GLOWSTICK = register("wall_glowstick") {
+        WallGlowstickBlock(Properties.copy(Blocks.WALL_TORCH)
+            .dropsLike(GLOWSTICK.get())
+            .noCollission()
+            .lightLevel(GlowstickBlock::luminance)
+            .noOcclusion())
     }
 
     val THERMAL_VENT = register("hydrothermal_vent_shaft") {
@@ -428,7 +367,8 @@ object HybridAquaticBlocks {
     }
 
     val GLOWSLIME_BLOCK = register("glowslime_block") {
-        SlimeBlock(Properties.copy(Blocks.SLIME_BLOCK).lightLevel { 14 })
+        SlimeBlock(Properties.copy(Blocks.SLIME_BLOCK)
+            .lightLevel { 14 })
     }
 
     val DECORATIVE_BUBBLE_COLUMN = register("decorative_bubble_column") {
