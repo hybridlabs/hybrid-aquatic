da00b74d9a43b437672aacdf5c1748261b4ab7fd
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-01-28 21:48:01 +0300

    cant place seagrass on aerated blocks now

---
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/TallSeagrassBlockMixin.java
@@ -0,0 +1,21 @@
+package dev.hybridlabs.aquatic.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import dev.hybridlabs.aquatic.block.HybridAquaticBlocks;
+import net.minecraft.world.level.block.TallSeagrassBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(TallSeagrassBlock.class)
+public class TallSeagrassBlockMixin {
+	@ModifyReturnValue(
+			method = "mayPlaceOn",
+			at = @At(value = "RETURN")
+	)
+	private boolean addSeaGrass(boolean original, @Local(ordinal = 0, argsOnly = true) BlockState blockState) {
+		if(blockState.is(HybridAquaticBlocks.INSTANCE.getAERATED_SAND().get())) return false;
+		else return original;
+	}
+}
