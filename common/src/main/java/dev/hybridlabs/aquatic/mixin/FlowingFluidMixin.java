4410755070780c615014d3330736afb7484b4fad
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-01-28 12:19:56 +0300

    Decorative Bubble Column Block parity

---
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/FlowingFluidMixin.java
@@ -0,0 +1,26 @@
+package dev.hybridlabs.aquatic.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.hybridlabs.aquatic.Constants;
+import dev.hybridlabs.aquatic.block.HybridAquaticBlocks;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FlowingFluid;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(FlowingFluid.class)
+public class FlowingFluidMixin {
+	@WrapOperation(
+			method = "canHoldFluid",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z")
+	)
+	private boolean decorativeBubbleColumn_givesAirParity(BlockState instance, Block block, Operation<Boolean> original) {
+		Constants.LOG.info("{}: {} {} {}", "BAG.givesAir", instance, block, original);
+		return instance.is(HybridAquaticBlocks.INSTANCE.getDECORATIVE_BUBBLE_COLUMN().get()) ? block == Blocks.BUBBLE_COLUMN : original.call(instance, block);
+	}
+}
--- common/src/main/java/dev/hybridlabs/aquatic/mixin/LivingEntityMixin.java
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/LivingEntityMixin.java
@@ -2,6 +2,7 @@ package dev.hybridlabs.aquatic.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.hybridlabs.aquatic.Constants;
 import dev.hybridlabs.aquatic.block.HybridAquaticBlocks;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.block.Block;
@@ -9,13 +10,21 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-	@WrapOperation(method = "baseTick", at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z"))
-	private boolean addDecorativeBubbleColumn(BlockState instance, Block block, Operation<Boolean> original) {
+	@WrapOperation(
+			method = "baseTick",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z"),
+			slice = @Slice(
+					from = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z"),
+					to = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;canBreatheUnderwater()Z"))
+	)
+	private boolean decorativeBubbleColumn_baseTickParity(BlockState instance, Block block, Operation<Boolean> original) {
+		Constants.LOG.info("{}: {} {} {}", "LE.baseTick", instance, block, original);
 		return instance.is(HybridAquaticBlocks.INSTANCE.getDECORATIVE_BUBBLE_COLUMN().get()) ? block == Blocks.BUBBLE_COLUMN : original.call(instance, block);
 	}
 }
