c6922d490b3b7eb6fb06c50c9f47fc6c17c9a509
ximossi <116030901+abrossimow@users.noreply.github.com>
2026-01-29 22:45:34 +0100

    Adds config options for trade additions

--- common/src/main/kotlin/dev/hybridlabs/aquatic/config/HybridAquaticConfig.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/config/HybridAquaticConfig.kt
@@ -10,12 +10,18 @@ data class HybridAquaticConfig(
      */
     val dataVersion: Int = 7,
 
+
+
     val entitySpawnConfig: List<EntitySpawnConfig> = EntitySpawnConfigGenerator.generate(),
+    val enableWanderingTraderTrades: Boolean = true,
+    val enableVillagerTrades: Boolean = true,
 ) {
     companion object {
         val CODEC: Codec<HybridAquaticConfig> = RecordCodecBuilder.create { instance ->
             instance.group(
                 Codec.INT.fieldOf("data_version").forGetter(HybridAquaticConfig::dataVersion),
+                Codec.BOOL.fieldOf("enable_wandering_trader_trades").forGetter(HybridAquaticConfig::enableWanderingTraderTrades),
+                Codec.BOOL.fieldOf("enable_villager_trades").forGetter(HybridAquaticConfig::enableVillagerTrades),
                 EntitySpawnConfig.CODEC.listOf().fieldOf("spawn_configuration").forGetter(HybridAquaticConfig::entitySpawnConfig),
             ).apply(instance, ::HybridAquaticConfig)
         }
