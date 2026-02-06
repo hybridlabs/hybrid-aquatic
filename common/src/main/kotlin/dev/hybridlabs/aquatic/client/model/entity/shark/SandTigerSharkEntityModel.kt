18f9958a1bbb670038fcb9f2e52f591ec921d529
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-02-06 20:51:46 +0200

    fixed sand tiger model crash

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/shark/SandTigerSharkEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/shark/SandTigerSharkEntityModel.kt
@@ -2,4 +2,4 @@ package dev.hybridlabs.aquatic.client.model.entity.shark
 
 import dev.hybridlabs.aquatic.entity.shark.SandTigerSharkEntity
 
-class TigerSharkEntityModel : HybridAquaticSharkEntityModel<SandTigerSharkEntity>("tiger_shark")
+class SandTigerSharkEntityModel : HybridAquaticSharkEntityModel<SandTigerSharkEntity>("sand_tiger_shark")
--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/entity/shark/SandTigerSharkEntityRenderer.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/entity/shark/SandTigerSharkEntityRenderer.kt
@@ -1,8 +1,8 @@
 package dev.hybridlabs.aquatic.client.render.entity.shark
 
-import dev.hybridlabs.aquatic.client.model.entity.shark.TigerSharkEntityModel
+import dev.hybridlabs.aquatic.client.model.entity.shark.SandTigerSharkEntityModel
 import dev.hybridlabs.aquatic.entity.shark.SandTigerSharkEntity
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context
 
 class SandTigerSharkEntityRenderer(context: Context) :
-    HybridAquaticSharkEntityRenderer<SandTigerSharkEntity>(context, TigerSharkEntityModel(), true)
+    HybridAquaticSharkEntityRenderer<SandTigerSharkEntity>(context, SandTigerSharkEntityModel(), true)
