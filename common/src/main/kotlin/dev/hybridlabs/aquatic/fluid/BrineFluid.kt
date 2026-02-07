60521863d7c7b1bf003ceb1e68a8f229b9dfdfd7
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-02-07 19:48:26 +0200

    Some very minimal work on brine

--- common/src/main/kotlin/dev/hybridlabs/aquatic/fluid/BrineFluid.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/fluid/BrineFluid.kt
@@ -96,6 +96,14 @@ abstract class BrineFluid : FlowingFluid() {
         return 5
     }
 
+    override fun getAmount(state: FluidState): Int {
+        return 8
+    }
+
+    override fun isSource(state: FluidState): Boolean {
+        return false
+    }
+
     public override fun canBeReplacedWith(
         fluidState: FluidState,
         blockReader: BlockGetter,
