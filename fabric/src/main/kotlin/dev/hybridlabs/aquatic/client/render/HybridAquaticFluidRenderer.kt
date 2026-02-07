1af980b787e3a83f14bc2ca28a663c058df32675
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-02-07 15:20:09 +0300

    fluid fabric renderer

---
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/client/render/HybridAquaticFluidRenderer.kt
@@ -0,0 +1,22 @@
+package dev.hybridlabs.aquatic.client.render
+
+import dev.hybridlabs.aquatic.CommonClass
+import dev.hybridlabs.aquatic.fluid.HybridAquaticFluids
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.world.level.material.Fluid
+
+class HybridAquaticFluidRenderer {
+    init {
+        registerFluid("brine", HybridAquaticFluids.BRINE.get(), HybridAquaticFluids.FLOWING_BRINE.get())
+    }
+
+    fun registerFluid(id: String, still: Fluid, flowing: Fluid) {
+        FluidRenderHandlerRegistry.INSTANCE.register(still, flowing, SimpleFluidRenderHandler(
+            CommonClass.locate("block/${id}_still"), CommonClass.locate("block/${id}_flowing")
+        ))
+        BlockRenderLayerMap.INSTANCE.putFluids(RenderType.translucent(), still, flowing)
+    }
+}
