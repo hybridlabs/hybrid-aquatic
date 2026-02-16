5ed758bbb8b0a6582745a4b97ec487e34e9a9f91
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-02-16 13:08:32 +0200

    AAAAAAAAAAAAAAAAAAAAAAA

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/ai/goal/HybridAquaticRandomSwimmingGoal.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/ai/goal/HybridAquaticRandomSwimmingGoal.kt
@@ -4,10 +4,28 @@ import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.behavior.BehaviorUtils
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal
 import net.minecraft.world.phys.Vec3
+import kotlin.math.sqrt
 
-open class HybridAquaticRandomSwimmingGoal(mob: PathfinderMob, speed: Double, interval: Int) :
-    RandomStrollGoal(mob, speed, interval) {
+open class HybridAquaticRandomSwimmingGoal(
+    mob: PathfinderMob,
+    speed: Double,
+    interval: Int,
+    private val minDistance: Double
+) : RandomStrollGoal(mob, speed, interval) {
+
     override fun getPosition(): Vec3? {
-        return BehaviorUtils.getRandomSwimmablePos(this.mob, 32, 16)
+        val origin = mob.position()
+        val minDistanceSqr = minDistance * minDistance
+
+        repeat(4) {
+            val pos = BehaviorUtils.getRandomSwimmablePos(this.mob, 32, 16)
+            if (pos != null) {
+                if (origin.distanceToSqr(pos) >= minDistanceSqr) {
+                    return pos
+                }
+            }
+        }
+
+        return null
     }
 }
--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/mammal/HybridAquaticDolphinEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/mammal/HybridAquaticDolphinEntity.kt
@@ -50,7 +50,7 @@ open class HybridAquaticDolphinEntity(type: EntityType<out HybridAquaticDolphinE
         setPathfindingMalus(BlockPathTypes.WATER, 0.0f)
         setPathfindingMalus(BlockPathTypes.DANGER_FIRE, 16.0f)
         setPathfindingMalus(BlockPathTypes.DAMAGE_FIRE, -1.0f)
-        moveControl = SmoothSwimmingMoveControl(this, 45, 3, 0.02F, 0.1F, false)
+        moveControl = SmoothSwimmingMoveControl(this, 85, 10, 0.02F, 0.1F, true)
         lookControl = SmoothSwimmingLookControl(this, 15)
         navigation = WaterBoundPathNavigation(this, world)
     }
@@ -61,7 +61,7 @@ open class HybridAquaticDolphinEntity(type: EntityType<out HybridAquaticDolphinE
         goalSelector.addGoal(1, WaterAnimalBreedGoal(this, 1.1))
         goalSelector.addGoal(5, HybridAquaticJumpGoal(this, 10, 7.0))
         goalSelector.addGoal(2, TemptGoal(this, 1.1, BREEDING_INGREDIENT, false))
-        goalSelector.addGoal(3, HybridAquaticRandomSwimmingGoal(this, 1.0, 2))
+        goalSelector.addGoal(3, HybridAquaticRandomSwimmingGoal(this, 1.0, 20, 10.0))
         goalSelector.addGoal(5, WaterAnimalFollowParentGoal(this, 1.1))
         goalSelector.addGoal(6, MeleeAttackGoal(this, 1.2, true))
     }
