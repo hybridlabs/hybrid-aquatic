45774c5a9d9ba177c7ed79fbcdbb03e4da68f3d3
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-02-28 20:18:35 +0200

    Fixed package

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/entity/shark/WhaleSharkEntityRenderer.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/entity/shark/WhaleSharkEntityRenderer.kt
@@ -1,7 +1,6 @@
-package dev.hybridlabs.aquatic.client.renderer.entity.shark
+package dev.hybridlabs.aquatic.client.render.entity.shark
 
 import dev.hybridlabs.aquatic.client.model.entity.shark.WhaleSharkEntityModel
-import dev.hybridlabs.aquatic.client.render.entity.shark.HybridAquaticSharkEntityRenderer
 import dev.hybridlabs.aquatic.client.render.entity.shark.layer.HybridAquaticSharkEntityLayer
 import dev.hybridlabs.aquatic.entity.shark.WhaleSharkEntity
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context
