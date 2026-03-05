b6ccc7b0473924a71eaf9e79dcc8c003d122d23e
Ivan Kh. <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-05 10:20:15 +0300

    why is there so much code for this might not work, i dont have the compiler to check if it does. (should tho)

--- common/src/main/kotlin/dev/hybridlabs/aquatic/item/HookItem.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/item/HookItem.kt
@@ -12,19 +12,11 @@ open class HookItem(settings: Properties) : Item(settings) {
     override fun appendHoverText(stack: ItemStack, world: Level?, tooltip: MutableList<Component>, context: TooltipFlag) {
         val isTideLoaded = Services.PLATFORM.isModLoaded("tide")
 
-        if (isTideLoaded) {
-            val tideComponent = Component.translatable(this.descriptionId.plus(".description")).withStyle(ChatFormatting.GRAY)
-            val hookTideComponent = Component.translatable("item.hybrid-aquatic.hook.description_tide").withStyle(ChatFormatting.GRAY)
+        val text = Component.translatable(this.descriptionId.plus(".description")).withStyle(ChatFormatting.GRAY)
+        val hookComponent = Component.translatable("item.hybrid-aquatic.hook.description${ if (isTideLoaded) "_tide" else "" }").withStyle(ChatFormatting.GRAY)
 
-            tooltip.add(tideComponent)
-            tooltip.add(hookTideComponent)
-        } else {
-            val text = Component.translatable(this.descriptionId.plus(".description")).withStyle(ChatFormatting.GRAY)
-            val hookComponent = Component.translatable("item.hybrid-aquatic.hook.description").withStyle(ChatFormatting.GRAY)
-
-            tooltip.add(text)
-            tooltip.add(hookComponent)
-        }
+        tooltip.add(text)
+        tooltip.add(hookComponent)
 
         super.appendHoverText(stack, world, tooltip, context)
     }
