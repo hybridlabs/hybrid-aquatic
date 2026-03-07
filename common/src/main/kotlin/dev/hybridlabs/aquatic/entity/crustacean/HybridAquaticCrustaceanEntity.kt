630cd8939e36989c99f5ba620ec19612ebecfc0a
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-07 23:44:59 +0300

    no way this is the fix

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/crustacean/HybridAquaticCrustaceanEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/crustacean/HybridAquaticCrustaceanEntity.kt
@@ -21,6 +21,8 @@ import net.minecraft.world.entity.SpawnGroupData
 import net.minecraft.world.entity.ai.control.MoveControl
 import net.minecraft.world.entity.ai.goal.PanicGoal
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal
+import net.minecraft.world.entity.ai.navigation.GroundPathNavigation
+import net.minecraft.world.entity.ai.navigation.PathNavigation
 import net.minecraft.world.entity.animal.WaterAnimal
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
@@ -53,8 +55,11 @@ open class HybridAquaticCrustaceanEntity(
         setPathfindingMalus(BlockPathTypes.DANGER_FIRE, 16.0f)
         setPathfindingMalus(BlockPathTypes.DAMAGE_FIRE, -1.0f)
         moveControl = MoveControl(this)
-
         setMaxUpStep(1.0f)
+    }
+
+    override fun createNavigation(level: Level): PathNavigation {
+        return GroundPathNavigation(this, level)
     }
 
     override fun registerGoals() {
