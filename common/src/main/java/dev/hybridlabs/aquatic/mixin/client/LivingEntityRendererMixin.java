00a51a0e2bb0e34c7a281fa17b56ec93608a6c8f
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-14 16:11:46 +0300

    kinda fixed player renderer

--- common/src/main/java/dev/hybridlabs/aquatic/mixin/client/LivingEntityRendererMixin.java
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/client/LivingEntityRendererMixin.java
@@ -3,6 +3,7 @@ package dev.hybridlabs.aquatic.mixin.client;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import dev.hybridlabs.aquatic.entity.misc.ArgonautEntity;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,10 +14,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntityRenderer.class)
 public class LivingEntityRendererMixin<T extends LivingEntity> {
 
-    @Inject(method = "setupRotations", at = @At("TAIL"))
-    public void render(T entity, PoseStack matrices, float animationProgress, float bodyYaw, float tickDelta, CallbackInfo ci) {
-        if (entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof ArgonautEntity argonaut) {
-            matrices.mulPose(Axis.XP.rotationDegrees(-argonaut.getViewXRot(tickDelta)));
-        }
-    }
+	@Inject(
+			method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+			at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V", shift = At.Shift.AFTER)
+	)
+	private void rotateInArgonaut(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
+		if (entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof ArgonautEntity argonaut) {
+			poseStack.scale(-1.0F, -1.0F, 1.0F);
+			poseStack.mulPose(Axis.XP.rotationDegrees(-argonaut.getViewXRot(partialTicks)));
+			poseStack.scale(-1.0F, -1.0F, 1.0F);
+		}
+	}
 }
