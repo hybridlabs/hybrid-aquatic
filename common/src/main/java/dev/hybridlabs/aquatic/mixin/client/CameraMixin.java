ada65a345406ca4da80815438501220953373102
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-14 16:47:50 +0300

    Improved first-person camera in argonaut

--- common/src/main/java/dev/hybridlabs/aquatic/mixin/client/CameraMixin.java
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/client/CameraMixin.java
@@ -4,11 +4,14 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.hybridlabs.aquatic.entity.misc.ArgonautEntity;
 import net.minecraft.client.Camera;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockGetter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Camera.class)
 public abstract class CameraMixin {
@@ -18,6 +21,20 @@ public abstract class CameraMixin {
 	
 	@Shadow
 	protected abstract double getMaxZoom(double startingDistance);
+	
+	@Shadow
+	protected abstract void move(double distanceOffset, double verticalOffset, double horizontalOffset);
+	
+	@Inject(
+		method = "setup",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;setPosition(DDD)V", shift = At.Shift.AFTER)
+	)
+	private void changeCameraPosInArgonaut(BlockGetter level, Entity entity, boolean detached, boolean thirdPersonReverse, float partialTick, CallbackInfo ci) {
+		if (!detached && entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof ArgonautEntity argonaut) {
+			var argonautRotationRadX = Math.toRadians(argonaut.getRotationVector().x);
+			this.move(argonautRotationRadX * 0.25, Math.abs(argonautRotationRadX * 0.25), 0.0);
+		}
+	}
 	
 	@ModifyArg(
 		method = "setup",
