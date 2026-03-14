286ce6f181a114bf338e4b5adacf48a404553b76
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-14 15:56:33 +0300

    changed f5 camera distance when you're inside an argonaut

---
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/client/CameraMixin.java
@@ -0,0 +1,36 @@
+package dev.hybridlabs.aquatic.mixin.client;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import dev.hybridlabs.aquatic.entity.misc.ArgonautEntity;
+import net.minecraft.client.Camera;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(Camera.class)
+public abstract class CameraMixin {
+	
+	@Shadow
+	private Entity entity;
+	
+	@ModifyExpressionValue(
+		method = "setup",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getMaxZoom(D)D")
+	)
+	private double changeCameraDistance(double original) {
+		return entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof ArgonautEntity ? 7.5d : original;
+	}
+	
+	@ModifyExpressionValue(
+		method = "setup",
+		slice = @Slice(
+			from = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;getMaxZoom(D)D", ordinal = 0),
+			to = @At(value = "TAIL")),
+		at = @At(value = "CONSTANT", args = "doubleValue=0.0", ordinal = 0)
+	)
+	private double changeCameraHeight(double original) {
+		return entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof ArgonautEntity ? 2.0d : original;
+	}
+}
