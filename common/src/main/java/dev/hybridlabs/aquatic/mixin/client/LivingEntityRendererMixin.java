4dcf088024180bdd56bf63d59b0ca40462f61710
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-03-14 16:29:13 +0300

    head doesnt rotate in an argonaut anymore

--- common/src/main/java/dev/hybridlabs/aquatic/mixin/client/LivingEntityRendererMixin.java
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/client/LivingEntityRendererMixin.java
@@ -1,5 +1,6 @@
 package dev.hybridlabs.aquatic.mixin.client;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import dev.hybridlabs.aquatic.entity.misc.ArgonautEntity;
@@ -9,14 +10,15 @@ import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntityRenderer.class)
 public class LivingEntityRendererMixin<T extends LivingEntity> {
 
 	@Inject(
-			method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
-			at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V", shift = At.Shift.AFTER)
+		method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+		at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(FFF)V", shift = At.Shift.AFTER)
 	)
 	private void rotateInArgonaut(T entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
 		if (entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof ArgonautEntity argonaut) {
@@ -24,5 +26,13 @@ public class LivingEntityRendererMixin<T extends LivingEntity> {
 			poseStack.mulPose(Axis.XP.rotationDegrees(-argonaut.getViewXRot(partialTicks)));
 			poseStack.scale(-1.0F, -1.0F, 1.0F);
 		}
+	}
+	
+	@ModifyArg(
+		method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/EntityModel;setupAnim(Lnet/minecraft/world/entity/Entity;FFFFF)V"), index = 5
+	)
+	private float removeHeadPitch(float headPitch, @Local(argsOnly = true) T entity) {
+		return entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof ArgonautEntity ? 0 : headPitch;
 	}
 }
