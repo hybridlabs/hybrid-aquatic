8b2d09c4b56634b5ea32ca161a069585218c97c3
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-03-16 07:20:49 +0200

    Improved marine snow rendering

--- common/src/main/java/dev/hybridlabs/aquatic/mixin/client/WeatherDisplayMixin.java
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/client/WeatherDisplayMixin.java
@@ -30,21 +30,21 @@ import static net.minecraft.client.renderer.LevelRenderer.getLightColor;
 
 @Mixin(LevelRenderer.class)
 public abstract class WeatherDisplayMixin implements ResourceManagerReloadListener, AutoCloseable {
-	
+
 	@Unique
 	private static final ResourceLocation MARINE_SNOW = CommonClass.locate("textures/environment/marine_snow.png");
-	
+
 	@Shadow private int ticks;
 	@Final @Shadow private float[] rainSizeX;
 	@Final @Shadow private float[] rainSizeZ;
 	@Final @Shadow private Minecraft minecraft;
-	
+
 	@Inject(method = "renderSnowAndRain", at=@At("HEAD"))
 	void renderWeatherInject(LightTexture manager, float tickDelta, double cameraX, double cameraY, double cameraZ, CallbackInfo ci) {
 		if (minecraft.player != null && minecraft.level != null) {
 			float f = this.minecraft.level.getRainLevel(tickDelta);
 			Level world = this.minecraft.level;
-			if (f > 0.0f && cameraY < world.getSeaLevel() - 56 && world.getBiome(minecraft.player.blockPosition()).is(HybridAquaticBiomeTags.INSTANCE.getALL_TRENCHES())) {
+			if (f > 0.0f && cameraY < world.getSeaLevel() && world.getBiome(minecraft.player.blockPosition()).is(HybridAquaticBiomeTags.INSTANCE.getALL_TRENCHES())) {
 				manager.turnOnLightLayer();
 				int xFloored = Mth.floor(cameraX);
 				int yFloored = Mth.floor(cameraY);
@@ -58,12 +58,13 @@ public abstract class WeatherDisplayMixin implements ResourceManagerReloadListen
 				if (Minecraft.useFancyGraphics()) {
 					layers = 10;
 				}
-				
+
 				RenderSystem.depthMask(Minecraft.useShaderTransparency());
 				int m = -1;
 				RenderSystem.setShader(GameRenderer::getParticleShader);
 				BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
-				
+                float f1 = (float)this.ticks + tickDelta;
+
 				for (int n = zFloored - layers; n <= zFloored + layers; ++n) {
 					for (int o = xFloored - layers; o <= xFloored + layers; ++o) {
 						int p = (n - zFloored + 16) * 32 + o - xFloored + 16;
@@ -78,59 +79,75 @@ public abstract class WeatherDisplayMixin implements ResourceManagerReloadListen
 							if (r < height) {
 								r = height;
 							}
-							
+
 							if (s < height) {
 								s = height;
 							}
-							
+
 							int t = Math.max(height, yFloored);
-							
+
 							if (r != s) {
 								RandomSource random = RandomSource.create(((long) o * o * 3121 + o * 45238971L ^ (long) n * n * 418711 + n * 13761L));
 								mutable.set(o, r, n);
 								Biome.Precipitation precipitation = biome.getPrecipitationAt(mutable);
-								float h;
-								float y;
-								if (precipitation == Biome.Precipitation.RAIN) {
+                                if (precipitation == Biome.Precipitation.RAIN) {
 									if (m != 0) {
-										if (m >= 0) {
-											tessellator.end();
-										}
 
-										m = 0;
+                                        m = 0;
 										RenderSystem.setShaderTexture(0, MARINE_SNOW);
 										bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
 									}
-									
-									int u = this.ticks + o * o * 3121 + o * 45238971 + n * n * 418711 + n * 13761 & 31;
-									h = -((float) u + tickDelta) / 512.0F * (3.0F + random.nextFloat());
-									double v = (double) o + 1.0 - cameraX;
-									double w = (double) n + 1.0 - cameraZ;
-									float x = (float) Math.sqrt(v * v + w * w) / (float) layers;
-									y = ((1.0F - x * x) * 0.5F + 0.5F) * f;
-									mutable.set(o, t, n);
-									int z = getLightColor(world, mutable);
-									
-									float seaLevelDist = Math.max(0.0f, (float) (world.getSeaLevel() - cameraY) / 128.0f);
-									float lerpedAlpha = Mth.lerp(seaLevelDist, 0.0f, 1.0f);
-									float clampedAlpha = Mth.clamp(y - lerpedAlpha, 0.0f, 1.0f);
-									
-									bufferBuilder.vertex((double) o - cameraX - d + 0.5, (double) s - cameraY, (double) n - cameraZ - e + 0.5).uv(0.0F, (float) r * 0.25F + h).color(1.0F, 1.0F, 1.0F, clampedAlpha * f).uv2(z).endVertex();
-									bufferBuilder.vertex((double) o - cameraX + d + 0.5, (double) s - cameraY, (double) n - cameraZ + e + 0.5).uv(1.0F, (float) r * 0.25F + h).color(1.0F, 1.0F, 1.0F, clampedAlpha * f).uv2(z).endVertex();
-									bufferBuilder.vertex((double) o - cameraX + d + 0.5, (double) r - cameraY, (double) n - cameraZ + e + 0.5).uv(1.0F, (float) s * 0.25F + h).color(1.0F, 1.0F, 1.0F, clampedAlpha * f).uv2(z).endVertex();
-									bufferBuilder.vertex((double) o - cameraX - d + 0.5, (double) r - cameraY, (double) n - cameraZ - e + 0.5).uv(0.0F, (float) s * 0.25F + h).color(1.0F, 1.0F, 1.0F, clampedAlpha * f).uv2(z).endVertex();
-								}
+
+                                    float f5 = -((float)(this.ticks & 767) + tickDelta) / 768.0F;
+                                    float f6 = (float)(random.nextDouble() + (double)f1 * 0.01 * (double)((float)random.nextGaussian()));
+                                    float f7 = (float)(random.nextDouble() + (double)(f1 * (float)random.nextGaussian()) * 0.001);
+
+                                    double dx = (double)o + 0.5 - cameraX;
+                                    double dz = (double)n + 0.5 - cameraZ;
+                                    float distanceFactor = (float)Math.sqrt(dx * dx + dz * dz) / (float)layers;
+                                    float fade = Mth.clamp((float)(world.getSeaLevel() - cameraY) / 48.0f, 0.0f, 0.8f);
+                                    float alpha = (((1.0F - distanceFactor * distanceFactor) * 0.3F + 0.5F) * f) * fade;
+
+                                    mutable.set(o, t, n);
+                                    int light = getLightColor(world, mutable);
+                                    int l3 = light >> 16 & 0xFFFF;
+                                    int i4 = light & 0xFFFF;
+                                    int j4 = (l3 * 3 + 240) / 4;
+                                    int k4 = (i4 * 3 + 240) / 4;
+
+                                    bufferBuilder.vertex((double)o - cameraX - d + 0.5, (double)s - cameraY, (double)n - cameraZ - e + 0.5)
+                                            .uv(0.0F + f6, (float)r * 0.25F + f5 + f7)
+                                            .color(1.0F, 1.0F, 1.0F, alpha)
+                                            .uv2(k4, j4)
+                                            .endVertex();
+                                    bufferBuilder.vertex((double)o - cameraX + d + 0.5, (double)s - cameraY, (double)n - cameraZ + e + 0.5)
+                                            .uv(1.0F + f6, (float)r * 0.25F + f5 + f7)
+                                            .color(1.0F, 1.0F, 1.0F, alpha)
+                                            .uv2(k4, j4)
+                                            .endVertex();
+                                    bufferBuilder.vertex((double)o - cameraX + d + 0.5, (double)r - cameraY, (double)n - cameraZ + e + 0.5)
+                                            .uv(1.0F + f6, (float)s * 0.25F + f5 + f7)
+                                            .color(1.0F, 1.0F, 1.0F, alpha)
+                                            .uv2(k4, j4)
+                                            .endVertex();
+                                    bufferBuilder.vertex((double)o - cameraX - d + 0.5, (double)r - cameraY, (double)n - cameraZ - e + 0.5)
+                                            .uv(0.0F + f6, (float)s * 0.25F + f5 + f7)
+                                            .color(1.0F, 1.0F, 1.0F, alpha)
+                                            .uv2(k4, j4)
+                                            .endVertex();
+                                }
 							}
 						}
 					}
 				}
-				
-				if (m >= 0) {
+
+				if (m == 0) {
 					tessellator.end();
 				}
-				
+
 				RenderSystem.enableCull();
-				RenderSystem.disableBlend();
+				RenderSystem.enableBlend();
+				RenderSystem.enableDepthTest();
 				manager.turnOffLightLayer();
 			}
 		}
