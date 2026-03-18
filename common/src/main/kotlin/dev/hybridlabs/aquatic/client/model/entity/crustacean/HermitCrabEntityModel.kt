e7c4359d2da1fbf48f5cd233854fdcced1756084
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-03-18 10:38:32 +0200

    Ominous conch hermit crabs

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/crustacean/HermitCrabEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/crustacean/HermitCrabEntityModel.kt
@@ -1,27 +1,32 @@
 package dev.hybridlabs.aquatic.client.model.entity.crustacean
 
 import dev.hybridlabs.aquatic.entity.crustacean.HermitCrabEntity
+import dev.hybridlabs.aquatic.item.HybridAquaticItems
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.Items
 
 class HermitCrabEntityModel : HybridAquaticCrustaceanEntityModel<HermitCrabEntity>("hermit_crab") {
 
     private val SHELL_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/crustacean/hermit_crab/hermit_crab_shell.png")
+    private val OMINOUS_CONCH_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/crustacean/hermit_crab/hermit_crab_ominous_conch.png")
     private val BLOCK_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/crustacean/hermit_crab/hermit_crab_block.png")
     private val NONE_TEXTURE = ResourceLocation("hybrid-aquatic", "textures/entity/crustacean/hermit_crab/hermit_crab.png")
 
     private val SHELL_MODEL = ResourceLocation("hybrid-aquatic", "geo/crustacean/hermit_crab/hermit_crab_shell.geo.json")
+    private val OMINOUS_CONCH_MODEL = ResourceLocation("hybrid-aquatic", "geo/crustacean/hermit_crab/hermit_crab_ominous_conch.geo.json")
     private val BLOCK_MODEL = ResourceLocation("hybrid-aquatic", "geo/crustacean/hermit_crab/hermit_crab_block.geo.json")
     private val NONE_MODEL = ResourceLocation("hybrid-aquatic", "geo/crustacean/hermit_crab/hermit_crab.geo.json")
 
     override fun getTextureResource(animatable: HermitCrabEntity): ResourceLocation {
         return if (animatable.shellItem.`is`(Items.NAUTILUS_SHELL)) SHELL_TEXTURE
+        else if (animatable.shellItem.`is`(HybridAquaticItems.OMINOUS_CONCH.get())) OMINOUS_CONCH_TEXTURE
         else if (animatable.shellItem.isEmpty) NONE_TEXTURE
         else BLOCK_TEXTURE
     }
 
     override fun getModelResource(animatable: HermitCrabEntity): ResourceLocation {
         return if (animatable.shellItem.`is`(Items.NAUTILUS_SHELL)) SHELL_MODEL
+        else if ((animatable.shellItem.`is`(HybridAquaticItems.OMINOUS_CONCH.get()))) OMINOUS_CONCH_MODEL
         else if (animatable.shellItem.isEmpty) NONE_MODEL
         else BLOCK_MODEL
     }
--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/crustacean/HermitCrabEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/crustacean/HermitCrabEntity.kt
@@ -2,6 +2,7 @@ package dev.hybridlabs.aquatic.entity.crustacean
 
 import dev.hybridlabs.aquatic.entity.ai.goal.FleeFromEntityGoal
 import dev.hybridlabs.aquatic.entity.misc.SmallTNTEntity
+import dev.hybridlabs.aquatic.item.HybridAquaticItems
 import dev.hybridlabs.aquatic.tag.HybridAquaticItemTags
 import net.minecraft.core.Vec3i
 import net.minecraft.core.particles.ParticleTypes
@@ -61,6 +62,7 @@ class HermitCrabEntity(entityType: EntityType<out HybridAquaticCrustaceanEntity>
         val generatedRoll = when {
             roll < 0.60f -> Items.NAUTILUS_SHELL.defaultInstance
             roll < 0.85f -> Items.SKELETON_SKULL.defaultInstance
+            roll < 0.90f -> HybridAquaticItems.OMINOUS_CONCH.get().defaultInstance
             else -> Items.AIR.defaultInstance
         }
         shellItem = generatedRoll
@@ -93,11 +95,22 @@ class HermitCrabEntity(entityType: EntityType<out HybridAquaticCrustaceanEntity>
 
     override fun mobInteract(player: Player, hand: InteractionHand): InteractionResult {
         val playerStack = player.getItemInHand(hand)
+
+        if (shellItem.`is`(HybridAquaticItems.OMINOUS_CONCH.get()) &&
+            !playerStack.`is`(Items.NAUTILUS_SHELL)
+        ) {
+            return InteractionResult.PASS
+        }
+
         if (canTakeItem(playerStack) || playerStack.isEmpty) {
             val oldStack = shellItem.copy()
             shellItem = playerStack.copyWithCount(1)
 
-            if (!shellItem.`is` { item -> item.equals(Items.NAUTILUS_SHELL) || item.equals(Items.SKELETON_SKULL) }) setPersistenceRequired()
+            if (!shellItem.`is` { item ->
+                    item.equals(Items.NAUTILUS_SHELL) ||
+                            item.equals(Items.SKELETON_SKULL) ||
+                            item.equals(HybridAquaticItems.OMINOUS_CONCH)
+                }) setPersistenceRequired()
 
             if (!player.abilities.instabuild) {
                 playerStack.shrink(1)
@@ -111,11 +124,18 @@ class HermitCrabEntity(entityType: EntityType<out HybridAquaticCrustaceanEntity>
 
             return InteractionResult.sidedSuccess(level().isClientSide)
         }
+
         return super.mobInteract(player, hand)
     }
 
     override fun dropCustomDeathLoot(source: DamageSource, looting: Int, causedByPlayer: Boolean) {
         if (!shellItem.isEmpty) {
+
+            if (shellItem.`is`(HybridAquaticItems.OMINOUS_CONCH.get())) {
+                shellItem = ItemStack.EMPTY
+                return
+            }
+
             spawnAtLocation(shellItem)
             shellItem = ItemStack.EMPTY
         }
@@ -146,9 +166,10 @@ class HermitCrabEntity(entityType: EntityType<out HybridAquaticCrustaceanEntity>
                     3.0f, 1.0f, level().random.nextLong()
                 )
             }
+
             is TntBlock -> {
                 shellItem = ItemStack.EMPTY
-                addDeltaMovement(Vec3(0.0, if(isInWater) 0.2 else 0.4, 0.0))
+                addDeltaMovement(Vec3(0.0, if (isInWater) 0.2 else 0.4, 0.0))
                 goalSelector.tick()
 
                 val primedTNT = SmallTNTEntity(level(), position().x, position().y, position().z, this)
@@ -165,7 +186,7 @@ class HermitCrabEntity(entityType: EntityType<out HybridAquaticCrustaceanEntity>
     //#endregion
 
     //#region Properties
-    override fun getMaxSize() : Int {
+    override fun getMaxSize(): Int {
         return 5
     }
 
@@ -222,7 +243,8 @@ class HermitCrabEntity(entityType: EntityType<out HybridAquaticCrustaceanEntity>
             DefaultAnimations.genericWalkIdleController(this)
         )
         controllerRegistrar.add(
-            AnimationController(this, "Hide", 4,
+            AnimationController(
+                this, "Hide", 4,
                 AnimationController.AnimationStateHandler { state: AnimationState<HybridAquaticCrustaceanEntity> ->
                     if (this.isHiding) {
                         return@AnimationStateHandler state.setAndContinue(HIDE_ANIMATION)
