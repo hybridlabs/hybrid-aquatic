030fb6f8164200e8bedc7d295649c82b634c9d02
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-04-12 15:17:04 +0300

    horn stuff is done

--- common/src/main/kotlin/dev/hybridlabs/aquatic/item/instrument/HAInstruments.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/item/instrument/HAInstruments.kt
@@ -2,16 +2,23 @@ package dev.hybridlabs.aquatic.item.instrument
 
 import dev.hybridlabs.aquatic.CommonClass
 import dev.hybridlabs.aquatic.platform.registration.RegistryObject
+import net.minecraft.core.Holder
+import net.minecraft.sounds.SoundEvent
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.world.item.Instrument
 
 object HAInstruments {
-    var OMINOUS_CONCH_HORN = create("ominous_conch_horn")
+    var OMINOUS_CONCH_HORN = create("ominous_conch_horn", SoundEvents.GRASS_BREAK)
 
     const val RANGE_BLOCKS: Float = 256.0F
     const val DURATION: Int = 140
 
-    fun create(id: String): RegistryObject<Instrument> {
-        return CommonClass.INSTRUMENTS.register(id ) { Instrument(SoundEvents.NOTE_BLOCK_GUITAR, DURATION, RANGE_BLOCKS) }
+    // Instrument only takes Holder<SoundEvent>
+    fun create(id: String, soundEvent: SoundEvent): RegistryObject<Instrument> {
+        return create(id, Holder.Direct(soundEvent))
+    }
+
+    fun create(id: String, soundEventHolder: Holder<SoundEvent>): RegistryObject<Instrument> {
+        return CommonClass.INSTRUMENTS.register(id ) { Instrument(soundEventHolder, DURATION, RANGE_BLOCKS) }
     }
 }
