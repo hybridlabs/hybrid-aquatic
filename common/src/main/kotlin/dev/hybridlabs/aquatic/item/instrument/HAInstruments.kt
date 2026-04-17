8de546f890eb9813cbb612a70a424e8b7a522c74
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-04-17 17:33:43 +0300

    fixed this for real now

--- common/src/main/kotlin/dev/hybridlabs/aquatic/item/instrument/HAInstruments.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/item/instrument/HAInstruments.kt
@@ -3,30 +3,17 @@ package dev.hybridlabs.aquatic.item.instrument
 import dev.hybridlabs.aquatic.CommonClass
 import dev.hybridlabs.aquatic.platform.registration.RegistryObject
 import dev.hybridlabs.aquatic.sound.HASoundEvents
-import net.minecraft.core.Holder
 import net.minecraft.sounds.SoundEvent
 import net.minecraft.world.item.Instrument
 
 object HAInstruments {
     var OMINOUS_CONCH_HORN = createInstrument("ominous_conch_horn", HASoundEvents.OMINOUS_CONCH_BLOWS)
 
-    fun createInstrument(id: String, soundEvent: RegistryObject<SoundEvent>): RegistryObject<Instrument> {
-        return createDurationRange(id, soundEvent.asHolder())
+    fun createInstrument(id: String, soundEventRegistry: RegistryObject<SoundEvent>): RegistryObject<Instrument> {
+        return create(id, soundEventRegistry, 140, 256.0f)
     }
 
-    fun createInstrument(id: String, soundEvent: SoundEvent): RegistryObject<Instrument> {
-        return createDurationRange(id, Holder.Direct(soundEvent))
-    }
-
-    fun createInstrument(id: String, soundEventHolder: Holder<SoundEvent>): RegistryObject<Instrument> {
-        return createDurationRange(id, soundEventHolder)
-    }
-
-    fun createDurationRange(id: String, soundEventHolder: Holder<SoundEvent>): RegistryObject<Instrument> {
-        return create(id, soundEventHolder, 140, 256.0f)
-    }
-
-    fun create(id: String, soundEventHolder: Holder<SoundEvent>, duration: Int, range: Float): RegistryObject<Instrument> {
-        return CommonClass.INSTRUMENTS.register(id ) { Instrument(soundEventHolder, duration, range) }
+    fun create(id: String, soundEventRegistry: RegistryObject<SoundEvent>, duration: Int, range: Float): RegistryObject<Instrument> {
+        return CommonClass.INSTRUMENTS.register(id ) { Instrument(soundEventRegistry.asHolder(), duration, range) }
     }
 }
