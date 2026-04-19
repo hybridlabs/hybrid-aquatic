f36a6035bdec1d64f47d377b9db87f16c513f9f7
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-04-19 02:48:27 +0200

    Garden eel hiding

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/GardenEelEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/fish/GardenEelEntity.kt
@@ -5,6 +5,7 @@ import dev.hybridlabs.aquatic.tag.HAEntityTags
 import dev.hybridlabs.aquatic.world.WorldHelper
 import net.minecraft.core.BlockPos
 import net.minecraft.util.RandomSource
+import net.minecraft.world.damagesource.DamageSource
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.MobSpawnType
 import net.minecraft.world.entity.MoverType
@@ -23,6 +24,7 @@ import net.minecraft.world.phys.Vec3
 import software.bernie.geckolib.constant.DefaultAnimations
 import software.bernie.geckolib.core.animation.AnimatableManager
 import software.bernie.geckolib.core.animation.AnimationController
+import software.bernie.geckolib.core.animation.RawAnimation
 
 @Suppress("unused", "DEPRECATION")
 class GardenEelEntity(type: EntityType<out GardenEelEntity>, world: Level) :
@@ -75,12 +77,72 @@ class GardenEelEntity(type: EntityType<out GardenEelEntity>, world: Level) :
     override fun registerControllers(controllers: AnimatableManager.ControllerRegistrar) {
         controllers.add(
             AnimationController(this, "Garden Eel Controller", 4) { state ->
-                state.setAndContinue(DefaultAnimations.IDLE)
+                when {
+                    isInWater && onGround() && !isHiding -> {
+                        state.setAndContinue(DefaultAnimations.IDLE)
+                    }
+
+                    isInWater && onGround() && isHiding -> {
+                        state.setAndContinue(HIDE_ANIMATION)
+                    }
+
+                    this.moistness < 590 -> {
+                        state.setAndContinue(FLOP_ANIMATION)
+                    }
+
+                    else -> {
+                        state.setAndContinue(DefaultAnimations.IDLE)
+                    }
+                }
             }
         )
     }
 
+    override fun shouldFlopOnLand(): Boolean {
+        return false
+    }
+
+    //#region Hiding
+    private var isHiding: Boolean = false
+    private var hidingTimer: Int = 0
+    private var lastDamageTime: Long = 0
+
+    private fun startHiding() {
+        isHiding = true
+        hidingTimer = 200
+
+        attributes.getInstance(Attributes.MOVEMENT_SPEED)?.baseValue = 0.0
+        attributes.getInstance(Attributes.ARMOR)?.baseValue = 50.0
+    }
+
+    fun hidingLogic() {
+        if (!isHiding) return
+
+        hidingTimer--
+        if ((level().gameTime - lastDamageTime) <= 200) return
+
+        isHiding = false
+        attributes.getInstance(Attributes.MOVEMENT_SPEED)?.baseValue = 0.3
+        attributes.getInstance(Attributes.ARMOR)?.baseValue = 5.0
+    }
+
+    override fun hurt(source: DamageSource, amount: Float): Boolean {
+        if (!isHiding) startHiding()
+
+        lastDamageTime = level().gameTime
+
+        return super.hurt(source, amount)
+    }
+
+    override fun tick() {
+        super.tick()
+        this.hidingLogic()
+    }
+    //#endregion
+
     companion object {
+        val HIDE_ANIMATION: RawAnimation = RawAnimation.begin().thenPlay("misc.hide")
+
         fun createMobAttributes(): AttributeSupplier.Builder {
             return createLivingAttributes()
                 .add(Attributes.MAX_HEALTH, 3.0)
