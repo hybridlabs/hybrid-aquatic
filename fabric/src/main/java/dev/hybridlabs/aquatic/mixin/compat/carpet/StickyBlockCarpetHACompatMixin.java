659ebdd760f9705e4b58c43e1eb0707b343b7f12
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-04-27 21:02:15 +0300

    disable mixin if carpet is not installed remove debug stuff

--- fabric/src/main/java/dev/hybridlabs/aquatic/mixin/compat/carpet/StickyBlockCarpetHACompatMixin.java
+++ fabric/src/main/java/dev/hybridlabs/aquatic/mixin/compat/carpet/StickyBlockCarpetHACompatMixin.java
@@ -7,13 +7,11 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Debug(export = true)
 @Mixin(BlockBehaviour.class)
 abstract class StickyBlockCarpetHACompatMixin implements BlockBehaviourInterface {
 	@Inject(
