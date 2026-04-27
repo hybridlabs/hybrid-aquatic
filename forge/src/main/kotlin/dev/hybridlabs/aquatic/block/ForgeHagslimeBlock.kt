99b614cbd29c0da2acb05ab8b2c6117dba05b084
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-04-27 23:08:20 +0300

    forge sticky blocks

--- forge/src/main/kotlin/dev/hybridlabs/aquatic/block/ForgeHagslimeBlock.kt
+++ forge/src/main/kotlin/dev/hybridlabs/aquatic/block/ForgeHagslimeBlock.kt
@@ -5,10 +5,10 @@ import net.minecraft.world.level.block.state.BlockState
 class ForgeHagslimeBlock(settings: Properties) : HagslimeBlock(settings) {
 
     override fun isStickyBlock(state: BlockState): Boolean {
-        return true
+        return isSticky()
     }
 
     override fun canStickTo(state: BlockState, other: BlockState): Boolean {
-        return true
+        return isStickyToNeighbor(other)
     }
 }
