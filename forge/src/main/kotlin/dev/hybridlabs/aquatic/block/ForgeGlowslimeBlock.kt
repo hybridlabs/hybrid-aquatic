7ee18dbcb8b3fb695aacb140a21898e86694f63a
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-04-29 08:46:55 +0300

    remove debug info and make Hagslime move entities on forge

--- forge/src/main/kotlin/dev/hybridlabs/aquatic/block/ForgeGlowslimeBlock.kt
+++ forge/src/main/kotlin/dev/hybridlabs/aquatic/block/ForgeGlowslimeBlock.kt
@@ -1,18 +1,14 @@
 package dev.hybridlabs.aquatic.block
 
-import dev.hybridlabs.aquatic.Constants
-import dev.hybridlabs.aquatic.block.impl.StickyBlock
 import net.minecraft.world.level.block.state.BlockState
 
 class ForgeGlowslimeBlock(settings: Properties) : GlowslimeBlock(settings) {
 
     override fun isStickyBlock(state: BlockState): Boolean {
-        Constants.LOG.info("test1: {}, {}", this, state)
-        return (state.block as StickyBlock).isSticky()
+        return isSticky()
     }
 
     override fun canStickTo(state: BlockState, other: BlockState): Boolean {
-        Constants.LOG.info("test2: {}, {}, {}", this, state.block, other.block)
-        return (state.block as StickyBlock).isStickyToNeighbor(other)
+        return isStickyToNeighbor(other)
     }
 }
