4b16337feef46bb4b45483753b36e7f19a247da2
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-05-05 18:29:19 +0300

    Added CrustaceanDigGoal

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/ai/goal/CrustaceanDigGoal.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/ai/goal/CrustaceanDigGoal.kt
@@ -1,6 +1,9 @@
 package dev.hybridlabs.aquatic.entity.ai.goal
 
 import dev.hybridlabs.aquatic.entity.crustacean.HACrustaceanEntity
+import net.minecraft.core.particles.BlockParticleOption
+import net.minecraft.core.particles.ParticleTypes
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.ai.goal.Goal
 import java.util.*
 
@@ -10,7 +13,7 @@ class CrustaceanDigGoal(private val crustacean: HACrustaceanEntity) : Goal() {
 
     init {
         this.flags = EnumSet.of(Flag.MOVE, Flag.LOOK)
-        digCooldown = crustacean.tickCount + (10 * 30 + crustacean.getRandom().nextInt(10) * 30)
+        digCooldown = crustacean.tickCount + (10 * 40 + crustacean.getRandom().nextInt(10) * 80)
     }
 
     override fun canUse(): Boolean {
@@ -24,6 +27,22 @@ class CrustaceanDigGoal(private val crustacean: HACrustaceanEntity) : Goal() {
         digTime = 40
         crustacean.startDigging()
         digCooldown = this.crustacean.tickCount + (10 * 20 + this.crustacean.getRandom().nextInt(10) * 20)
+
+        val level = this.crustacean.level()
+        val blockpos = this.crustacean.blockPosition()
+        val blockstate = level.getBlockState(blockpos.below())
+
+        if (level is ServerLevel) {
+            level.sendParticles(
+                BlockParticleOption(ParticleTypes.BLOCK, blockstate),
+                blockpos.x + 0.5,
+                blockpos.y + 0.8,
+                blockpos.z + 0.5,
+                6,
+                0.3, 0.5, 0.3,
+                0.02
+            )
+        }
     }
 
     override fun stop() {
