5ba6e019a2d8919db508b377299cd7fd6858e81f
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-05-08 17:02:11 +0300

    Cleanup

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/cephalopod/CuttlefishEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/cephalopod/CuttlefishEntity.kt
@@ -90,14 +90,14 @@ class CuttlefishEntity(type: EntityType<out CuttlefishEntity>, world: Level) : H
         super.defineSynchedData()
     }
 
-    override fun addAdditionalSaveData(nbt: CompoundTag) {
-        nbt.putString("Type", this.variant.serializedName)
-        super.addAdditionalSaveData(nbt)
+    override fun addAdditionalSaveData(compound: CompoundTag) {
+        compound.putString("Type", this.variant.serializedName)
+        super.addAdditionalSaveData(compound)
     }
 
-    override fun readAdditionalSaveData(nbt: CompoundTag) {
-        this.variant = Type.byName(nbt.getString("Type"))
-        super.readAdditionalSaveData(nbt)
+    override fun readAdditionalSaveData(compound: CompoundTag) {
+        this.variant = Type.byName(compound.getString("Type"))
+        super.readAdditionalSaveData(compound)
     }
 
     override fun getVariant(): Type {
