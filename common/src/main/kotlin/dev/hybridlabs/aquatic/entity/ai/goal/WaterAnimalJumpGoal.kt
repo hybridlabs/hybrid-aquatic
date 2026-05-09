b960442ad3d34a39325ca77195e322464e46e8d4
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-05-09 16:53:15 +0300

    mob -> waterAnimal

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/ai/goal/WaterAnimalJumpGoal.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/ai/goal/WaterAnimalJumpGoal.kt
@@ -1,17 +1,17 @@
 package dev.hybridlabs.aquatic.entity.ai.goal
 
+import dev.hybridlabs.aquatic.entity.base.HAWaterAnimal
 import net.minecraft.core.BlockPos
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.tags.FluidTags
 import net.minecraft.util.Mth
-import net.minecraft.world.entity.PathfinderMob
 import net.minecraft.world.entity.ai.goal.JumpGoal
 import kotlin.math.abs
 import kotlin.math.atan2
 
 @Suppress("DEPRECATION")
 class WaterAnimalJumpGoal(
-    private val mob: PathfinderMob,
+    private val waterAnimal: HAWaterAnimal,
     chance: Int,
     private val jumpHeight: Double
 ) : JumpGoal() {
@@ -24,13 +24,13 @@ class WaterAnimalJumpGoal(
     }
 
     override fun canUse(): Boolean {
-        return if (mob.random.nextInt(chance) != 0) {
+        return if (waterAnimal.random.nextInt(chance) != 0) {
             false
         } else {
-            val direction = mob.motionDirection
+            val direction = waterAnimal.motionDirection
             val i = direction.stepX
             val j = direction.stepZ
-            val blockPos = mob.blockPosition()
+            val blockPos = waterAnimal.blockPosition()
             val var5 = OFFSET_MULTIPLIERS
             val var6 = var5.size
             for (var7 in 0 until var6) {
@@ -45,23 +45,23 @@ class WaterAnimalJumpGoal(
 
     private fun isWaterAt(pos: BlockPos, offsetX: Int, offsetZ: Int, multiplier: Int): Boolean {
         val blockPos = pos.offset(offsetX * multiplier, 0, offsetZ * multiplier)
-        return mob.level().getFluidState(blockPos).`is`(FluidTags.WATER) && !mob.level().getBlockState(blockPos)
+        return waterAnimal.level().getFluidState(blockPos).`is`(FluidTags.WATER) && !waterAnimal.level().getBlockState(blockPos)
             .blocksMotion()
     }
 
     private fun isAirAbove(pos: BlockPos, offsetX: Int, offsetZ: Int, multiplier: Int): Boolean {
-        return mob.level().getBlockState(
+        return waterAnimal.level().getBlockState(
             pos.offset(
                 offsetX * multiplier,
                 1,
                 offsetZ * multiplier
             )
-        ).isAir && mob.level().getBlockState(pos.offset(offsetX * multiplier, 2, offsetZ * multiplier)).isAir
+        ).isAir && waterAnimal.level().getBlockState(pos.offset(offsetX * multiplier, 2, offsetZ * multiplier)).isAir
     }
 
     override fun canContinueToUse(): Boolean {
-        val d = mob.deltaMovement.y
-        return (!(d * d < 0.029999999329447746) || mob.xRot == 0.0f || !(abs(mob.xRot) < 10.0f) || !mob.isInWater) && !mob.onGround()
+        val d = waterAnimal.deltaMovement.y
+        return (!(d * d < 0.029999999329447746) || waterAnimal.xRot == 0.0f || !(abs(waterAnimal.xRot) < 10.0f) || !waterAnimal.isInWater) && !waterAnimal.onGround()
     }
 
     override fun isInterruptable(): Boolean {
@@ -69,39 +69,39 @@ class WaterAnimalJumpGoal(
     }
 
     override fun start() {
-        val direction = mob.motionDirection
+        val direction = waterAnimal.motionDirection
         val jumpVelocity = jumpVelocity()
 
-        mob.deltaMovement =
-            mob.deltaMovement.add(
+        waterAnimal.deltaMovement =
+            waterAnimal.deltaMovement.add(
                 direction.stepX * 0.6,
                 jumpVelocity,
                 direction.stepZ * 0.6
             )
 
-        mob.navigation.stop()
+        waterAnimal.navigation.stop()
     }
 
     override fun stop() {
-        mob.xRot = 0.0f
+        waterAnimal.xRot = 0.0f
     }
 
     override fun tick() {
         val bl = inWater
         if (!bl) {
-            val fluidState = mob.level().getFluidState(mob.blockPosition())
+            val fluidState = waterAnimal.level().getFluidState(waterAnimal.blockPosition())
             inWater = fluidState.`is`(FluidTags.WATER)
         }
         if (inWater && !bl) {
-            mob.playSound(SoundEvents.DOLPHIN_JUMP, 1.0f, 1.0f)
+            waterAnimal.playSound(SoundEvents.DOLPHIN_JUMP, 1.0f, 1.0f)
         }
-        val vec3d = mob.deltaMovement
-        if (vec3d.y * vec3d.y < 0.029999999329447746 && mob.xRot != 0.0f) {
-            mob.xRot = Mth.rotLerp(0.2f, mob.xRot, 0.0f)
+        val vec3d = waterAnimal.deltaMovement
+        if (vec3d.y * vec3d.y < 0.029999999329447746 && waterAnimal.xRot != 0.0f) {
+            waterAnimal.xRot = Mth.rotLerp(0.2f, waterAnimal.xRot, 0.0f)
         } else if (vec3d.length() > 9.999999747378752E-6) {
             val d = vec3d.length()
             val e = atan2(-vec3d.y, d) * 57.2957763671875
-            mob.xRot = e.toFloat()
+            waterAnimal.xRot = e.toFloat()
         }
     }
 
