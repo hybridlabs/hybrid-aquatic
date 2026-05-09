79b8513c5a8bb7d36c343dd0b974200331455cb0
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-05-09 17:37:33 +0300

    removed Herobrine

--- fabric/src/main/kotlin/dev/hybridlabs/aquatic/block/property/FlammableProperty.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/block/property/FlammableProperty.kt
@@ -2,23 +2,36 @@ package dev.hybridlabs.aquatic.block.property
 
 import dev.hybridlabs.aquatic.block.HAPlatformBlocks
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry
+import net.minecraft.world.level.block.Block
 
 object FlammableProperty {
     val registry: FlammableBlockRegistry = FlammableBlockRegistry.getDefaultInstance()
 
     init {
-        registry.add(HAPlatformBlocks.DUNEGRASS.get(), 60, 100)
-        registry.add(HAPlatformBlocks.TALL_DUNEGRASS.get(), 60, 100)
-        registry.add(HAPlatformBlocks.CATTAIL.get(), 60, 100)
-        // same as vanilla logs
-        registry.add(HAPlatformBlocks.DRIFTWOOD_LOG.get(), 5, 5)
-        registry.add(HAPlatformBlocks.STRIPPED_DRIFTWOOD_LOG.get(), 5, 5)
-        registry.add(HAPlatformBlocks.DRIFTWOOD_WOOD.get(), 5, 5)
-        registry.add(HAPlatformBlocks.STRIPPED_DRIFTWOOD_WOOD.get(), 5, 5)
-        // same as vanilla cut wood
-        registry.add(HAPlatformBlocks.DRIFTWOOD_PLANKS.get(), 5, 20)
-        registry.add(HAPlatformBlocks.DRIFTWOOD_SLAB.get(), 5, 20)
-        registry.add(HAPlatformBlocks.DRIFTWOOD_FENCE.get(), 5, 20)
-        registry.add(HAPlatformBlocks.DRIFTWOOD_FENCE_GATE.get(), 5, 20)
+        registerGrass(HAPlatformBlocks.DUNEGRASS.get())
+        registerGrass(HAPlatformBlocks.TALL_DUNEGRASS.get())
+        registerGrass(HAPlatformBlocks.CATTAIL.get())
+
+        registerLogs(HAPlatformBlocks.DRIFTWOOD_LOG.get())
+        registerLogs(HAPlatformBlocks.STRIPPED_DRIFTWOOD_LOG.get())
+        registerLogs(HAPlatformBlocks.DRIFTWOOD_WOOD.get())
+        registerLogs(HAPlatformBlocks.STRIPPED_DRIFTWOOD_WOOD.get())
+
+        registerCutWood(HAPlatformBlocks.DRIFTWOOD_PLANKS.get())
+        registerCutWood(HAPlatformBlocks.DRIFTWOOD_SLAB.get())
+        registerCutWood(HAPlatformBlocks.DRIFTWOOD_FENCE.get())
+        registerCutWood(HAPlatformBlocks.DRIFTWOOD_FENCE_GATE.get())
+    }
+
+    fun registerGrass(input: Block) {
+        registry.add(input, 60, 100)
+    }
+
+    fun registerLogs(input: Block) {
+        registry.add(input, 5, 5)
+    }
+
+    fun registerCutWood(input: Block) {
+        registry.add(input, 5, 20)
     }
 }
