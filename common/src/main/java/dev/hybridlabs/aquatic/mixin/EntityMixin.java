20c62214035cd647ad71da9938758611576f2405
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-05-31 04:14:39 +0300

    Trying to work out the effects

--- common/src/main/java/dev/hybridlabs/aquatic/mixin/EntityMixin.java
+++ common/src/main/java/dev/hybridlabs/aquatic/mixin/EntityMixin.java
@@ -4,17 +4,29 @@ import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.hybridlabs.aquatic.block.HABlocks;
+import dev.hybridlabs.aquatic.effect.HAMobEffects;
 import dev.hybridlabs.aquatic.item.HAItems;
+import dev.hybridlabs.aquatic.tag.HAFluidTags;
+import net.minecraft.core.BlockPos;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
@@ -54,5 +66,91 @@ public class EntityMixin {
     )
     private boolean decorativeBubbleColumn_isInBubbleColumnParity(BlockState instance, Block block, Operation<Boolean> original) {
         return instance.is(HABlocks.INSTANCE.getDECORATIVE_BUBBLE_COLUMN().get()) ? block == Blocks.BUBBLE_COLUMN : original.call(instance, block);
+    }
+
+    @Inject(
+            method = "updateFluidHeightAndDoFluidPushing",
+            at = @At("HEAD")
+    )
+
+    private void hybridAquatic$applyBrineEffects(TagKey<Fluid> fluidTag, double motionScale, CallbackInfoReturnable<Boolean> cir) {
+        Entity self = (Entity) (Object) this;
+
+        if (!(self instanceof LivingEntity entity)) {
+            return;
+        }
+
+        BlockPos pos = self.blockPosition();
+        FluidState state = self.level().getFluidState(pos);
+
+        if (state.isEmpty()) {
+            return;
+        }
+
+        if (state.is(HAFluidTags.INSTANCE.getBRINE())) {
+
+                //with reinforced diving suit
+            if (isWearingReinforcedDivingSet(entity)) {
+                entity.addEffect(new MobEffectInstance(
+                        HAMobEffects.INSTANCE.getCORROSION().get(),
+                        120,
+                        0
+                ));
+
+                //with normal diving suit
+            } else if (isWearingDivingSet(entity)) {
+                entity.addEffect(new MobEffectInstance(
+                        MobEffects.POISON,
+                        120,
+                        0
+                ));
+
+                entity.addEffect(new MobEffectInstance(
+                        HAMobEffects.INSTANCE.getCORROSION().get(),
+                        120,
+                        0
+                ));
+
+                //without diving suit
+            } else {
+                entity.addEffect(new MobEffectInstance(
+                        MobEffects.POISON,
+                        120,
+                        1
+                ));
+
+                entity.addEffect(new MobEffectInstance(
+                        HAMobEffects.INSTANCE.getCORROSION().get(),
+                        120,
+                        0
+                ));
+            }
+        }
+    }
+
+    @Unique
+    private boolean isWearingDivingSet(LivingEntity entity) {
+        ItemStack helmet = entity.getItemBySlot(EquipmentSlot.HEAD);
+        ItemStack chest = entity.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack legs = entity.getItemBySlot(EquipmentSlot.LEGS);
+        ItemStack boots = entity.getItemBySlot(EquipmentSlot.FEET);
+
+        return helmet.is(HAItems.INSTANCE.getDIVING_HELMET().get())
+                && chest.is(HAItems.INSTANCE.getDIVING_SUIT().get())
+                && legs.is(HAItems.INSTANCE.getDIVING_LEGGINGS().get())
+                && boots.is(HAItems.INSTANCE.getDIVING_BOOTS().get());
+    }
+
+    @Unique
+    private boolean isWearingReinforcedDivingSet(LivingEntity entity) {
+        ItemStack helmet = entity.getItemBySlot(EquipmentSlot.HEAD);
+        ItemStack chest = entity.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack legs = entity.getItemBySlot(EquipmentSlot.LEGS);
+        ItemStack boots = entity.getItemBySlot(EquipmentSlot.FEET);
+
+        return helmet.is(HAItems.INSTANCE.getREINFORCED_DIVING_HELMET().get())
+                && chest.is(HAItems.INSTANCE.getREINFORCED_DIVING_SUIT().get())
+                && legs.is(HAItems.INSTANCE.getREINFORCED_DIVING_LEGGINGS().get())
+                && boots.is(HAItems.INSTANCE.getREINFORCED_DIVING_BOOTS().get());
     }
 }
