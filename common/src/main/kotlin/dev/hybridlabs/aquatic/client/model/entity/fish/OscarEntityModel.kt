24e4d8b1118e928f96615e5c754b04cf65ce0f0e
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-06-06 18:56:27 +0300

    Missed a few files

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/fish/OscarEntityModel.kt
+++
@@ -1,5 +0,0 @@
-package dev.hybridlabs.aquatic.client.model.entity.fish
-
-import dev.hybridlabs.aquatic.entity.fish.OscarEntity
-
-class OscarEntityModel : HAFishEntityModel<OscarEntity>("oscar")
--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/entity/fish/OscarEntityRenderer.kt
+++
@@ -1,8 +0,0 @@
-package dev.hybridlabs.aquatic.client.render.entity.fish
-
-import dev.hybridlabs.aquatic.client.model.entity.fish.OscarEntityModel
-import dev.hybridlabs.aquatic.entity.fish.OscarEntity
-import net.minecraft.client.renderer.entity.EntityRendererProvider.Context
-
-class OscarEntityRenderer(context: Context) :
-    HAFishEntityRenderer<OscarEntity>(context, OscarEntityModel(), true, false)
