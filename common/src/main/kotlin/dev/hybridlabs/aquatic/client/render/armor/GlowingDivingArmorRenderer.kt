fe5ead7ea3e5fd6a117d0e38aad52cc70241970a
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-06-07 19:07:14 +0300

    Glowing diving armor glows now

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/armor/GlowingDivingArmorRenderer.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/render/armor/GlowingDivingArmorRenderer.kt
@@ -4,7 +4,12 @@ import dev.hybridlabs.aquatic.CommonClass
 import dev.hybridlabs.aquatic.item.armor.GlowingDivingArmorItem
 import software.bernie.geckolib.model.DefaultedItemGeoModel
 import software.bernie.geckolib.renderer.GeoArmorRenderer
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer
 
 class GlowingDivingArmorRenderer : GeoArmorRenderer<GlowingDivingArmorItem>(
-    DefaultedItemGeoModel(CommonClass.locate("armor/glowing_diving_armor"))
-)
+    DefaultedItemGeoModel(CommonClass.locate("armor/glowing_diving_armor"))) {
+
+    init {
+        addRenderLayer(AutoGlowingGeoLayer(this))
+    }
+}
