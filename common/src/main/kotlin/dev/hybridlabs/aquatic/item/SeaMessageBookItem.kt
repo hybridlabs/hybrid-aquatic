5177fda05fd038f528c7786bf1c0d7242d3da1e1
Kevin Murphy <kevin@eorbit.net>
2026-06-21 13:49:19 -0700

    Roll back changes that reverted data compontents to NBT for reasons?

--- common/src/main/kotlin/dev/hybridlabs/aquatic/item/SeaMessageBookItem.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/item/SeaMessageBookItem.kt
@@ -3,9 +3,10 @@ package dev.hybridlabs.aquatic.item
 import dev.hybridlabs.aquatic.block.SeaMessage
 import dev.hybridlabs.aquatic.registry.HARegistryKeys
 import net.minecraft.ChatFormatting
-import net.minecraft.core.RegistryAccess
-import net.minecraft.nbt.Tag
+import net.minecraft.core.HolderLookup
+import net.minecraft.core.component.DataComponents
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.stats.Stats
 import net.minecraft.world.InteractionHand
@@ -15,8 +16,17 @@ import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
+import kotlin.jvm.optionals.getOrNull
 
-class SeaMessageBookItem(settings: Properties) : Item(settings) {
+class
+SeaMessageBookItem(settings: Properties) : Item(settings) {
+    override fun getName(stack: ItemStack): Component {
+        stack.get(DataComponents.WRITTEN_BOOK_CONTENT)?.let{ content->
+            if (content.title.get(false) != "") return Component.translatable(content.title.get(false))
+        }
+        return super.getName(stack)
+    }
+
 
     override fun appendHoverText(
         stack: ItemStack,
@@ -34,6 +44,10 @@ class SeaMessageBookItem(settings: Properties) : Item(settings) {
 
     override fun use(world: Level, player: Player, hand: InteractionHand): InteractionResultHolder<ItemStack> {
         val stack = player.getItemInHand(hand)
+
+        if (!stack.has(DataComponents.WRITTEN_BOOK_CONTENT))
+            setWrittenBookContent(stack, world.registryAccess())
+
         player.openItemGui(stack, hand)
         player.awardStat(Stats.ITEM_USED.get(this))
         return InteractionResultHolder.sidedSuccess(stack, world.isClientSide)
@@ -42,34 +56,20 @@ class SeaMessageBookItem(settings: Properties) : Item(settings) {
     companion object {
         const val SEA_MESSAGE_KEY: String = "sea_message"
 
-        private fun setSeaMessage(
+        private fun setWrittenBookContent(
             stack: ItemStack,
-            message: SeaMessage,
-            registryManager: RegistryAccess
-        ): ItemStack {
-            val id = message.getId(registryManager) ?: return stack
-            val nbt = stack.orCreateTag
-            nbt.putString(SEA_MESSAGE_KEY, id.toString())
-            return stack
-        }
-
-        fun getSeaMessage(stack: ItemStack, registryManager: RegistryAccess): SeaMessage? {
-            val nbt = stack.tag ?: return null
-
-            if (!nbt.contains(SEA_MESSAGE_KEY, Tag.TAG_STRING.toInt())) {
-                return null
+            registryAccess: HolderLookup.Provider
+        ) {
+            getSeaMessage(stack, registryAccess)?.let { message ->
+                stack.set(DataComponents.WRITTEN_BOOK_CONTENT, message.getWrittenBookContent())
             }
-
-            val unparsedId = nbt.getString(SEA_MESSAGE_KEY)
-            val id = ResourceLocation.tryParse(unparsedId) ?: return null
-            val registry = registryManager.registryOrThrow(HARegistryKeys.SEA_MESSAGE)
-            return registry.get(id)
         }
 
-        fun createItemStack(message: SeaMessage, registryManager: RegistryAccess): ItemStack {
-            val stack = ItemStack(HAItems.SEA_MESSAGE_BOOK.get())
-            setSeaMessage(stack, message, registryManager)
-            return stack
+        fun getSeaMessage(stack: ItemStack, registryManager: HolderLookup.Provider): SeaMessage? {
+            val customData = stack.get(DataComponents.CUSTOM_DATA)?.copyTag() ?: return null
+            val id = ResourceLocation.tryParse(customData.getString(SEA_MESSAGE_KEY)) ?: return null
+            val registry = registryManager.lookup(HARegistryKeys.SEA_MESSAGE).get()
+            return registry.get(ResourceKey.create(HARegistryKeys.SEA_MESSAGE, id)).getOrNull()?.value()
         }
     }
 }
