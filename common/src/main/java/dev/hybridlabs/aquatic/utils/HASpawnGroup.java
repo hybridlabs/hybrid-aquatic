bc0f26949832ea1ddf23569e7d365d119391a086
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-06-24 10:36:23 +0300

    Removed extra spaces and imports

--- common/src/main/java/dev/hybridlabs/aquatic/utils/HASpawnGroup.java
+++ common/src/main/java/dev/hybridlabs/aquatic/utils/HASpawnGroup.java
@@ -1,7 +1,6 @@
 package dev.hybridlabs.aquatic.utils;
 
 import dev.hybridlabs.aquatic.Constants;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.MobCategory;
 
 import java.util.Map;
