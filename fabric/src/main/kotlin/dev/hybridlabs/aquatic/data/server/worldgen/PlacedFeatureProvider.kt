88db00e50e3c81635ed19004db69ea23a7bf228f
Kevin Murphy <kevin@eorbit.net>
2026-06-26 17:30:40 -0700

    Fixed missing placed disk_suspicious sand

--- fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/server/worldgen/PlacedFeatureProvider.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/server/worldgen/PlacedFeatureProvider.kt
@@ -522,6 +522,7 @@ class PlacedFeatureProvider(
         entries.add(reg.getOrThrow(HAPlacedFeatures.PING_PONG_SPONGE_PATCH))
         entries.add(reg.getOrThrow(HAPlacedFeatures.TUBE_SPONGE_PATCH))
         entries.add(reg.getOrThrow(HAPlacedFeatures.WATER_LETTUCE))
+        entries.add(reg.getOrThrow(HAPlacedFeatures.DISK_SUSPICIOUS_SAND))
     }
 
     override fun getName(): String {
