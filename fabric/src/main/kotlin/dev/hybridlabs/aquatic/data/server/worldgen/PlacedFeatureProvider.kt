b396c8d8dd3109ddfe4e013dfe16bde666449d11
Kevin Murphy <kevin@eorbit.net>
2026-06-26 17:35:04 -0700

    Fixed missing placed mound

--- fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/server/worldgen/PlacedFeatureProvider.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/server/worldgen/PlacedFeatureProvider.kt
@@ -524,6 +524,7 @@ class PlacedFeatureProvider(
         entries.add(reg.getOrThrow(HAPlacedFeatures.WATER_LETTUCE))
         entries.add(reg.getOrThrow(HAPlacedFeatures.DISK_SUSPICIOUS_SAND))
         entries.add(reg.getOrThrow(HAPlacedFeatures.OYSTER_BED))
+        entries.add(reg.getOrThrow(HAPlacedFeatures.MOUND))
     }
 
     override fun getName(): String {
