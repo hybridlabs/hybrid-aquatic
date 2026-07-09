60a7349c52e6caab7e4c8a03c1b01edbef52272c
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-07-09 12:34:11 +0300

    Fixed chiseled white sandstone acting like a slab

--- common/src/main/kotlin/dev/hybridlabs/aquatic/block/HABlocks.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/block/HABlocks.kt
@@ -107,7 +107,7 @@ object HABlocks {
             .mapColor(DyeColor.WHITE)) }
 
     val CHISELED_WHITE_SANDSTONE = register("chiseled_white_sandstone") {
-        SlabBlock(Properties
+        Block(Properties
             .ofFullCopy(WHITE_SANDSTONE.get())
             .mapColor(DyeColor.WHITE)) }
 
