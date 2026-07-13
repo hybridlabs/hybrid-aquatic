8b5c9a2fe76a8ba8a3207110530ec123214422a5
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-07-13 14:35:49 +0300

    Made the variants properly

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/mammal/DugongEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/mammal/DugongEntityModel.kt
@@ -2,18 +2,15 @@ package dev.hybridlabs.aquatic.client.model.entity.mammal
 
 import dev.hybridlabs.aquatic.entity.mammal.DugongEntity
 import net.minecraft.resources.ResourceLocation
-import kotlin.random.Random
 
 class DugongEntityModel : HASirenianEntityModel<DugongEntity>("dugong") {
 
     override fun getTextureResource(animatable: DugongEntity): ResourceLocation {
-        val seed = animatable.uuid.leastSignificantBits
-        val random = Random(seed)
-
         return if (animatable.isBaby) {
             BABY_TEXTURE
-        } else {
-            dugongTextures[random.nextInt(dugongTextures.size)]
+        } else when (animatable.variant) {
+            DugongEntity.Companion.Type.PLAIN -> PLAIN_TEXTURE
+            DugongEntity.Companion.Type.MOSSY -> MOSSY_TEXTURE
         }
     }
 
@@ -21,9 +18,9 @@ class DugongEntityModel : HASirenianEntityModel<DugongEntity>("dugong") {
         private val BABY_TEXTURE =
             ResourceLocation("hybrid_aquatic", "textures/entity/mammal/dugong/baby_dugong.png")
 
-        private val dugongTextures = listOf(
-            ResourceLocation("hybrid_aquatic", "textures/entity/mammal/dugong/dugong.png"),
+        private val PLAIN_TEXTURE =
+            ResourceLocation("hybrid_aquatic", "textures/entity/mammal/dugong/dugong.png")
+        private val MOSSY_TEXTURE =
             ResourceLocation("hybrid_aquatic", "textures/entity/mammal/dugong/mossy_dugong.png")
-        )
     }
 }
