f867ad38ff42adaeb13a542a80b50a6e936264ee
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-07-13 14:13:26 +0300

    More changes

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/mammal/DugongEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/mammal/DugongEntityModel.kt
@@ -1,5 +1,29 @@
 package dev.hybridlabs.aquatic.client.model.entity.mammal
 
 import dev.hybridlabs.aquatic.entity.mammal.DugongEntity
+import net.minecraft.resources.ResourceLocation
+import kotlin.random.Random
 
-class DugongEntityModel : HASirenianEntityModel<DugongEntity>("dugong")
+class DugongEntityModel : HASirenianEntityModel<DugongEntity>("dugong") {
+
+    override fun getTextureResource(animatable: DugongEntity): ResourceLocation {
+        val seed = animatable.uuid.leastSignificantBits
+        val random = Random(seed)
+
+        return if (animatable.isBaby) {
+            BABY_TEXTURE
+        } else {
+            dugongTextures[random.nextInt(dugongTextures.size)]
+        }
+    }
+
+    companion object {
+        private val BABY_TEXTURE =
+            ResourceLocation("hybrid_aquatic", "textures/entity/mammal/dugong/baby_dugong.png")
+
+        private val dugongTextures = listOf(
+            ResourceLocation("hybrid_aquatic", "textures/entity/mammal/dugong/dugong.png"),
+            ResourceLocation("hybrid_aquatic", "textures/entity/mammal/dugong/mossy_dugong.png")
+        )
+    }
+}
