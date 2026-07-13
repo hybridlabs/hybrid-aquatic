12d797fb0ef42de4ceebd25b4b47ff3827a2ae69
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-07-13 06:34:50 +0300

    Changes

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/mammal/ManateeEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/mammal/ManateeEntityModel.kt
@@ -1,5 +1,29 @@
 package dev.hybridlabs.aquatic.client.model.entity.mammal
 
 import dev.hybridlabs.aquatic.entity.mammal.ManateeEntity
+import net.minecraft.resources.ResourceLocation
+import kotlin.random.Random
 
-class ManateeEntityModel : HASirenianEntityModel<ManateeEntity>("manatee")
+class ManateeEntityModel : HASirenianEntityModel<ManateeEntity>("manatee") {
+
+    override fun getTextureResource(animatable: ManateeEntity): ResourceLocation {
+        val seed = animatable.uuid.leastSignificantBits
+        val random = Random(seed)
+
+        return if (animatable.isBaby) {
+            BABY_TEXTURE
+        } else {
+            manateeTextures[random.nextInt(manateeTextures.size)]
+        }
+    }
+
+    companion object {
+        private val BABY_TEXTURE =
+            ResourceLocation("hybrid_aquatic", "textures/entity/mammal/manatee/baby_manatee.png")
+
+        private val manateeTextures = listOf(
+            ResourceLocation("hybrid_aquatic", "textures/entity/mammal/manatee/manatee.png"),
+            ResourceLocation("hybrid_aquatic", "textures/entity/mammal/manatee/mossy_manatee.png")
+        )
+    }
+}
