2b9d444ea6fca43e9c79a70c19a01c3dccc90e28
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-07-16 17:56:42 +0300

    test paintings

--- common/src/main/kotlin/dev/hybridlabs/aquatic/painting/HAPaintings.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/painting/HAPaintings.kt
@@ -1,12 +1,21 @@
 package dev.hybridlabs.aquatic.painting
 
-object HAPaintings {
-    // fun register(id: String, painting: Supplier<PaintingVariant>): Supplier<PaintingVariant> {
-    //     return CommonClass.PAINTINGS.register(id, painting)
-    // }
+import dev.hybridlabs.aquatic.CommonClass
+import dev.hybridlabs.aquatic.Constants.MOD_ID
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.entity.decoration.PaintingVariant
+import java.util.function.Supplier
 
-    // fun registerPainting(id: String, widthBlocks: Int, heightBlocks: Int): ResourceLocation {
-    //     register(id) { PaintingVariant(widthBlocks * 16, heightBlocks * 16) }
-    //     return ResourceLocation.fromNamespaceAndPath(MOD_ID, id)
-    // }
+object HAPaintings {
+    val TEST_PAINTING1 = registerPainting("test_painting1", 2, 2)
+    val TEST_PAINTING2 = registerPainting("test_painting2", 8, 8)
+
+    fun register(id: String, painting: Supplier<PaintingVariant>): Supplier<PaintingVariant> {
+        return CommonClass.PAINTINGS.register(id, painting)
+    }
+
+    fun registerPainting(id: String, widthBlocks: Int, heightBlocks: Int): ResourceLocation {
+        register(id) { PaintingVariant(widthBlocks * 16, heightBlocks * 16) }
+        return ResourceLocation(MOD_ID, id)
+    }
 }
