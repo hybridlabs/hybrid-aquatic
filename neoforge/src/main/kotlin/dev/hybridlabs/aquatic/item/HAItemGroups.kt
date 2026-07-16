f3f52110dbe4ca2865ee4b45ae4afce332908a5a
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-07-16 19:54:19 +0300

    add paintings to neoforge creative menu

--- neoforge/src/main/kotlin/dev/hybridlabs/aquatic/item/HAItemGroups.kt
+++ neoforge/src/main/kotlin/dev/hybridlabs/aquatic/item/HAItemGroups.kt
@@ -10,13 +10,14 @@ import dev.hybridlabs.aquatic.block.MessageInABottleBlock
 import dev.hybridlabs.aquatic.block.entity.MessageInABottleBlockEntity
 import dev.hybridlabs.aquatic.platform.registration.RegistryObject
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Holder
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
 import net.minecraft.network.chat.Component
-import net.minecraft.world.item.CreativeModeTab
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
-import net.minecraft.world.item.SpawnEggItem
+import net.minecraft.world.entity.decoration.PaintingVariant
+import net.minecraft.world.item.*
 import net.minecraft.world.level.block.Blocks
+import kotlin.jvm.optionals.getOrNull
 
 object HAItemGroups {
     val BLOCKS = register(
@@ -24,7 +25,7 @@ object HAItemGroups {
         CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)
             .title(Component.translatable("itemGroup.${Constants.MOD_ID}.blocks"))
             .icon { ItemStack(HAItems.ANEMONE.get()) }
-            .displayItems { _, entries ->
+            .displayItems { itemDisplayParameters, entries ->
                 // message in a bottle variants
                 MessageInABottleBlock.Variant.entries.forEach { variant ->
                     val blockEntity = MessageInABottleBlockEntity(
@@ -303,6 +304,16 @@ object HAItemGroups {
                     if (id.namespace != Constants.MOD_ID) {
                         return@forEach
                     }
+                }
+
+                itemDisplayParameters.holders().lookup(Registries.PAINTING_VARIANT).ifPresent { registryLookup ->
+                    CreativeModeTabs.generatePresetPaintings(
+                        entries,
+                        itemDisplayParameters.holders(),
+                        registryLookup,
+                        { holder: Holder<PaintingVariant> -> holder.unwrapKey().getOrNull()?.location()?.namespace == Constants.MOD_ID },
+                        CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS
+                    )
                 }
             }
             .build()
