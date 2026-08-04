e34e6d9e2bca198c68bf6bc5bdd5d05029df0b5c
Kevin Murphy <kevin@eorbit.net>
2026-08-04 09:50:29 -0700

    Fix: use REPLACE_SINGLE_BLOCK for tide pool mussels to get the correct waterlogged state

--- fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/server/worldgen/ConfiguredFeatureProvider.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/data/server/worldgen/ConfiguredFeatureProvider.kt
@@ -4,8 +4,8 @@ package dev.hybridlabs.aquatic.data.server.worldgen
 
 import dev.hybridlabs.aquatic.HybridAquatic
 import dev.hybridlabs.aquatic.block.HABlocks
-import dev.hybridlabs.aquatic.fluid.HAPlatformFluids
 import dev.hybridlabs.aquatic.block.TubeWormBlock
+import dev.hybridlabs.aquatic.fluid.HAPlatformFluids
 import dev.hybridlabs.aquatic.tag.HABlockTags
 import dev.hybridlabs.aquatic.world.gen.feature.*
 import dev.hybridlabs.aquatic.world.gen.feature.kelp.BullKelpFeatureConfig
@@ -43,6 +43,7 @@ import net.minecraft.world.level.levelgen.placement.CaveSurface
 import net.minecraft.world.level.levelgen.placement.CountPlacement
 import net.minecraft.world.level.levelgen.placement.HeightmapPlacement
 import net.minecraft.world.level.levelgen.placement.PlacementModifier
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockStateMatchTest
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest
 import net.minecraft.world.level.levelgen.synth.NormalNoise
 import java.util.concurrent.CompletableFuture
@@ -317,11 +318,13 @@ class ConfiguredFeatureProvider(
                 Feature.DISK,
                 DiskConfiguration(
                     RuleBasedBlockStateProvider.simple(HABlocks.AERATED_SAND.get()),
-                    BlockPredicate.matchesBlocks(listOf<Block>(
-                        HABlocks.GRASSY_SAND.get(),
-                        HABlocks.SHORESTONE.get(),
-                        HABlocks.CORALSTONE.get(),
-                        Blocks.SAND)
+                    BlockPredicate.matchesBlocks(
+                        listOf<Block>(
+                            HABlocks.GRASSY_SAND.get(),
+                            HABlocks.SHORESTONE.get(),
+                            HABlocks.CORALSTONE.get(),
+                            Blocks.SAND
+                        )
                     ),
                     UniformInt.of(1, 3),
                     1
@@ -494,11 +497,19 @@ class ConfiguredFeatureProvider(
                 RandomPatchConfiguration(
                     16, 4, 4,
                     PlacementUtils.filtered(
-                        Feature.SIMPLE_BLOCK,
-                        SimpleBlockConfiguration(
-                            BlockStateProvider.simple(
-                                HABlocks.WILD_MUSSELS.get().defaultBlockState()
-                                    .setValue(WATERLOGGED, false)
+                        Feature.REPLACE_SINGLE_BLOCK,
+                        ReplaceBlockConfiguration(
+                            listOf(
+                                OreConfiguration.target(
+                                    BlockStateMatchTest(Blocks.WATER.defaultBlockState()),
+                                    HABlocks.WILD_MUSSELS.get().defaultBlockState()
+                                        .setValue(WATERLOGGED, true)
+                                ),
+                                OreConfiguration.target(
+                                    BlockStateMatchTest(Blocks.AIR.defaultBlockState()),
+                                    HABlocks.WILD_MUSSELS.get().defaultBlockState()
+                                        .setValue(WATERLOGGED, false)
+                                )
                             )
                         ),
                         BlockPredicate.hasSturdyFace(Vec3i(0, -1, 0), Direction.UP)
