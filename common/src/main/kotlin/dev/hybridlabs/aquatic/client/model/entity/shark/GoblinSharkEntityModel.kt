62935a45fcbcca56da85acd8c7d26272bbf52a2e
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-08-07 13:07:12 +0300

    Made goblin sharks more accurate

--- common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/shark/GoblinSharkEntityModel.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/client/model/entity/shark/GoblinSharkEntityModel.kt
@@ -1,5 +1,25 @@
 package dev.hybridlabs.aquatic.client.model.entity.shark
 
 import dev.hybridlabs.aquatic.entity.shark.GoblinSharkEntity
+import net.minecraft.resources.ResourceLocation
 
-class GoblinSharkEntityModel : HASharkEntityModel<GoblinSharkEntity>("goblin_shark")
+class GoblinSharkEntityModel : HASharkEntityModel<GoblinSharkEntity>("goblin_shark") {
+
+    override fun getTextureResource(animatable: GoblinSharkEntity): ResourceLocation =
+        if (animatable.moistness < 590) DEPRESSURIZED_TEXTURE else GOBLIN_SHARK_TEXTURE
+
+    override fun getModelResource(animatable: GoblinSharkEntity): ResourceLocation =
+        if (animatable.moistness < 590) DEPRESSURIZED_MODEL else GOBLIN_SHARK_MODEL
+
+    companion object {
+        private val GOBLIN_SHARK_TEXTURE =
+            ResourceLocation("hybrid_aquatic", "textures/entity/shark/goblin_shark/goblin_shark.png")
+        private val DEPRESSURIZED_TEXTURE =
+            ResourceLocation("hybrid_aquatic", "textures/entity/shark/goblin_shark/goblin_shark_depressurized.png")
+
+        private val GOBLIN_SHARK_MODEL =
+            ResourceLocation("hybrid_aquatic", "geo/shark/goblin_shark/goblin_shark.geo.json")
+        private val DEPRESSURIZED_MODEL =
+            ResourceLocation("hybrid_aquatic", "geo/shark/goblin_shark/goblin_shark_depressurized.geo.json")
+    }
+}
