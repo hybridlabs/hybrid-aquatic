8aa2067cef363659bf331715bc89051c1fb57c5b
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-08-25 18:41:47 +0300

    Revert "Fix: Add trinket renderer for plushies in hat slot"
    This reverts commit 0402e9faaf796ce2ba4c31b5ceef24abb8e66559.

--- fabric/src/main/kotlin/dev/hybridlabs/aquatic/client/render/item/HATrinketRendererRegistry.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/aquatic/client/render/item/HATrinketRendererRegistry.kt
@@ -2,25 +2,18 @@ package dev.hybridlabs.aquatic.client.render.item
 
 import dev.emi.trinkets.api.client.TrinketRendererRegistry
 import dev.hybridlabs.aquatic.item.HAItems
-import net.minecraft.client.Minecraft
 import net.minecraft.client.model.HumanoidModel
 import net.minecraft.client.player.AbstractClientPlayer
 import net.minecraft.client.renderer.RenderType
-import net.minecraft.client.renderer.blockentity.SkullBlockRenderer
 import net.minecraft.client.renderer.entity.ItemRenderer
 import net.minecraft.client.renderer.texture.OverlayTexture
-import net.minecraft.util.Mth
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
-import net.minecraft.world.level.block.AbstractSkullBlock
-import org.joml.Quaternionf
 import software.bernie.geckolib.animatable.GeoItem
 import software.bernie.geckolib.animatable.client.RenderProvider
 
 object HATrinketRendererRegistry {
-    val models = SkullBlockRenderer.createSkullRenderers(Minecraft.getInstance().entityModels)!!
 
     init {
         registerTrinketRenderer(HAItems.EEL_SCARF.get(), EquipmentSlot.CHEST)
@@ -33,14 +26,6 @@ object HATrinketRendererRegistry {
         registerTrinketRenderer(HAItems.BROWN_HATXOLOTL.get(), EquipmentSlot.HEAD)
         registerTrinketRenderer(HAItems.CYAN_HATXOLOTL.get(), EquipmentSlot.HEAD)
         registerTrinketRenderer(HAItems.BLUE_HATXOLOTL.get(), EquipmentSlot.HEAD)
-        registerPlushieRenderer(HAItems.BASKING_SHARK_PLUSHIE.get() as BlockItem)
-        registerPlushieRenderer(HAItems.BULL_SHARK_PLUSHIE.get() as BlockItem)
-        registerPlushieRenderer(HAItems.FRILLED_SHARK_PLUSHIE.get() as BlockItem)
-        registerPlushieRenderer(HAItems.GREAT_WHITE_SHARK_PLUSHIE.get() as BlockItem)
-        registerPlushieRenderer(HAItems.HAMMERHEAD_SHARK_PLUSHIE.get() as BlockItem)
-        registerPlushieRenderer(HAItems.THRESHER_SHARK_PLUSHIE.get() as BlockItem)
-        registerPlushieRenderer(HAItems.TIGER_SHARK_PLUSHIE.get() as BlockItem)
-        registerPlushieRenderer(HAItems.WHALE_SHARK_PLUSHIE.get() as BlockItem)
     }
 
     private fun registerTrinketRenderer(item: Item, equipmentSlot: EquipmentSlot) {
@@ -57,24 +42,6 @@ object HATrinketRendererRegistry {
                     poseStack, vertexConsumer, light, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f
                 )
             }
-        }
-    }
-
-    private fun registerPlushieRenderer(item: BlockItem) {
-        TrinketRendererRegistry.registerRenderer(item) { itemStack, slotReference, contextModel, poseStack, bufferSource, light, entity, limbAngle, limbDistance, tickDelta, animationProgress, headYaw, headPitch ->
-            val skull = item.block as AbstractSkullBlock
-            val type = skull.type
-            poseStack.pushPose()
-            poseStack.mulPose(Quaternionf().rotationZYX(0F, headYaw * Mth.DEG_TO_RAD, headPitch * Mth.DEG_TO_RAD))
-            poseStack.scale(1.1875f, -1.1875f, -1.1875f)
-            poseStack.translate(-0.5, 0.425, -0.5)
-            val mouthAnimation: Float = entity.walkAnimation.position(animationProgress)
-            val renderType = SkullBlockRenderer.getRenderType(type, null)
-            SkullBlockRenderer.renderSkull(
-                null, 180F, mouthAnimation, poseStack, bufferSource, light,
-                models[type], renderType
-            )
-            poseStack.popPose()
         }
     }
 }
