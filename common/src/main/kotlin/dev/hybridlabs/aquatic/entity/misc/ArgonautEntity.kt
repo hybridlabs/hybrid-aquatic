0ed8ee994b64e8f20e522e20422dd213389e9e79
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-09-10 00:45:52 +0300

    Testing argonaut fuel fix

--- common/src/main/kotlin/dev/hybridlabs/aquatic/entity/misc/ArgonautEntity.kt
+++ common/src/main/kotlin/dev/hybridlabs/aquatic/entity/misc/ArgonautEntity.kt
@@ -1,17 +1,23 @@
 package dev.hybridlabs.aquatic.entity.misc
 
+import com.google.common.collect.Maps
 import com.mojang.serialization.Codec
 import dev.hybridlabs.aquatic.item.HAItems
 import dev.hybridlabs.aquatic.world.inventory.ArgonautMenu
+import net.minecraft.SharedConstants
+import net.minecraft.Util
 import net.minecraft.core.Direction
 import net.minecraft.core.NonNullList
 import net.minecraft.core.component.DataComponents
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.protocol.game.ServerboundPaddleBoatPacket
 import net.minecraft.network.syncher.EntityDataAccessor
 import net.minecraft.network.syncher.EntityDataSerializers
 import net.minecraft.network.syncher.SynchedEntityData
 import net.minecraft.resources.ResourceKey
+import net.minecraft.tags.ItemTags
+import net.minecraft.tags.TagKey
 import net.minecraft.util.ByIdMap
 import net.minecraft.util.Mth
 import net.minecraft.util.StringRepresentable
@@ -25,14 +31,12 @@ import net.minecraft.world.entity.player.Player
 import net.minecraft.world.entity.vehicle.ContainerEntity
 import net.minecraft.world.inventory.AbstractContainerMenu
 import net.minecraft.world.inventory.ContainerData
-import net.minecraft.world.item.DyeColor
-import net.minecraft.world.item.DyeItem
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
+import net.minecraft.world.item.*
 import net.minecraft.world.item.component.CustomData
 import net.minecraft.world.level.GameRules
+import net.minecraft.world.level.ItemLike
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.gameevent.GameEvent
 import net.minecraft.world.level.storage.loot.LootTable
 import net.minecraft.world.phys.Vec2
@@ -164,7 +168,100 @@ open class ArgonautEntity(
 
     open fun getBurnDuration(fuel: ItemStack): Int {
         return if (fuel.isEmpty) 0
-        else AbstractFurnaceBlockEntity.getFuel().getOrDefault(fuel.item, 0) * 2
+        else getFuel().getOrDefault(fuel.item, 0)!! * 2
+    }
+
+    fun getFuel(): MutableMap<Item?, Int?> {
+        val map: MutableMap<Item?, Int?> = Maps.newLinkedHashMap()
+        add(map, Items.LAVA_BUCKET, 20000)
+        add(map, Blocks.COAL_BLOCK, 16000)
+        add(map, Items.BLAZE_ROD, 2400)
+        add(map, Items.COAL, 1600)
+        add(map, Items.CHARCOAL, 1600)
+        add(map, ItemTags.LOGS, 300)
+        add(map, ItemTags.BAMBOO_BLOCKS, 300)
+        add(map, ItemTags.PLANKS, 300)
+        add(map, Blocks.BAMBOO_MOSAIC, 300)
+        add(map, ItemTags.WOODEN_STAIRS, 300)
+        add(map, Blocks.BAMBOO_MOSAIC_STAIRS, 300)
+        add(map, ItemTags.WOODEN_SLABS, 150)
+        add(map, Blocks.BAMBOO_MOSAIC_SLAB, 150)
+        add(map, ItemTags.WOODEN_TRAPDOORS, 300)
+        add(map, ItemTags.WOODEN_PRESSURE_PLATES, 300)
+        add(map, ItemTags.WOODEN_FENCES, 300)
+        add(map, ItemTags.FENCE_GATES, 300)
+        add(map, Blocks.NOTE_BLOCK, 300)
+        add(map, Blocks.BOOKSHELF, 300)
+        add(map, Blocks.CHISELED_BOOKSHELF, 300)
+        add(map, Blocks.LECTERN, 300)
+        add(map, Blocks.JUKEBOX, 300)
+        add(map, Blocks.CHEST, 300)
+        add(map, Blocks.TRAPPED_CHEST, 300)
+        add(map, Blocks.CRAFTING_TABLE, 300)
+        add(map, Blocks.DAYLIGHT_DETECTOR, 300)
+        add(map, ItemTags.BANNERS, 300)
+        add(map, Items.BOW, 300)
+        add(map, Items.FISHING_ROD, 300)
+        add(map, Blocks.LADDER, 300)
+        add(map, ItemTags.SIGNS, 200)
+        add(map, ItemTags.HANGING_SIGNS, 800)
+        add(map, Items.WOODEN_SHOVEL, 200)
+        add(map, Items.WOODEN_SWORD, 200)
+        add(map, Items.WOODEN_HOE, 200)
+        add(map, Items.WOODEN_AXE, 200)
+        add(map, Items.WOODEN_PICKAXE, 200)
+        add(map, ItemTags.WOODEN_DOORS, 200)
+        add(map, ItemTags.BOATS, 1200)
+        add(map, ItemTags.WOOL, 100)
+        add(map, ItemTags.WOODEN_BUTTONS, 100)
+        add(map, Items.STICK, 100)
+        add(map, ItemTags.SAPLINGS, 100)
+        add(map, Items.BOWL, 100)
+        add(map, ItemTags.WOOL_CARPETS, 67)
+        add(map, Blocks.DRIED_KELP_BLOCK, 4001)
+        add(map, Items.CROSSBOW, 300)
+        add(map, Blocks.BAMBOO, 50)
+        add(map, Blocks.DEAD_BUSH, 100)
+        add(map, Blocks.SCAFFOLDING, 50)
+        add(map, Blocks.LOOM, 300)
+        add(map, Blocks.BARREL, 300)
+        add(map, Blocks.CARTOGRAPHY_TABLE, 300)
+        add(map, Blocks.FLETCHING_TABLE, 300)
+        add(map, Blocks.SMITHING_TABLE, 300)
+        add(map, Blocks.COMPOSTER, 300)
+        add(map, Blocks.AZALEA, 100)
+        add(map, Blocks.FLOWERING_AZALEA, 100)
+        add(map, Blocks.MANGROVE_ROOTS, 300)
+        return map
+    }
+
+    private fun isNeverAFurnaceFuel(item: Item): Boolean {
+        return item.builtInRegistryHolder().`is`(ItemTags.NON_FLAMMABLE_WOOD)
+    }
+
+    private fun add(map: MutableMap<Item?, Int?>, itemTag: TagKey<Item?>, burnTime: Int) {
+        for (holder in BuiltInRegistries.ITEM.getTagOrEmpty(itemTag)) {
+            if (!isNeverAFurnaceFuel(holder.value() as Item)) {
+                map[holder.value() as Item] = burnTime
+            }
+        }
+    }
+
+    private fun add(map: MutableMap<Item?, Int?>, p_item: ItemLike, burnTime: Int) {
+        val item = p_item.asItem()
+        if (isNeverAFurnaceFuel(item)) {
+            if (SharedConstants.IS_RUNNING_IN_IDE) {
+                throw Util.pauseInIde(
+                    IllegalStateException(
+                        "A developer tried to explicitly make fire resistant item " + item.getName(
+                            null as ItemStack
+                        ).string + " a furnace fuel. That will not work!"
+                    )
+                ) as IllegalStateException
+            }
+        } else {
+            map[item] = burnTime
+        }
     }
 
     fun isLit(): Boolean {
@@ -478,7 +575,7 @@ open class ArgonautEntity(
 
     override fun getPassengerAttachmentPoint(entity: Entity, dimensions: EntityDimensions, partialTick: Float): Vec3 {
         return Vec3(0.0, 0.65, 0.0)
-            .yRot(-this.yRot * Mth.DEG_TO_RAD);
+            .yRot(-this.yRot * Mth.DEG_TO_RAD)
     }
 
     protected fun clampRotation(entityToUpdate: Entity) {
@@ -558,7 +655,7 @@ open class ArgonautEntity(
     }
 
     override fun positionRider(passenger: Entity, callback: MoveFunction) {
-        super.positionRider(passenger, callback);
+        super.positionRider(passenger, callback)
         if (passenger is LivingEntity)
             passenger.yBodyRot = this.yRot
     }
